7310870e87ff5e6873d1615a399174e366c40782
Max Base <MaxBaseCode@Gmail.Com>
2024-02-26 12:54:18 +0330

    ok

--- b.sh
+++ b.sh
@@ -4,8 +4,9 @@
 rm s
 # gcc sallam.c -o s -fsanitize=address
 # gcc sallam.c -o s -fsanitize=leak -llsan -Walloc-zero
-# gcc -g -fsanitize=undefined,address -Walloca sallam.c -o s -lefence
-gcc -g -fsanitize=address -Walloca sallam.c -o s -lefence
+# gcc -g -fsanitize=address -Walloca sallam.c -o s -lefence
+gcc -g -fsanitize=undefined,address -Walloca sallam.c -o s -lefence
+gcc -g -Walloca sallam.c -o s
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then

e921fb59e24c866f35e682b0dce8c1927603ed9f
Max Base <MaxBaseCode@Gmail.Com>
2024-02-28 04:55:08 +0330

    Looking for bugs

--- b.sh
+++ b.sh
@@ -5,13 +5,14 @@ rm s
 # gcc sallam.c -o s -fsanitize=address
 # gcc sallam.c -o s -fsanitize=leak -llsan -Walloc-zero
 # gcc -g -fsanitize=address -Walloca sallam.c -o s -lefence
-gcc -g -fsanitize=undefined,address -Walloca sallam.c -o s -lefence
+gcc -g -fsanitize=undefined,address -Walloca -o s sallam.c -lefence
 # gcc -g -Walloca sallam.c -o s
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
     # If compilation was successful, execute the program with input.sallam
-    ./s input.sallam
+    # ./s input.sallam
+    ASAN_OPTIONS=detect_leaks=1 ./s input.sallam
 else
     # If compilation failed, display an error message
     echo "Compilation failed!"

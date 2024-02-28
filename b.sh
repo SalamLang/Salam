3b492158d40c4508c308141a8305227ba81d5c30
Max Base <MaxBaseCode@Gmail.Com>
2024-02-29 00:17:52 +0330

    delete some unused logs - fix bug of english numbers

--- b.sh
+++ b.sh
@@ -8,9 +8,9 @@ rm s
 # gcc sallam.c -o s -fsanitize=address
 # gcc sallam.c -o s -fsanitize=leak -llsan -Walloc-zero
 # gcc -g -fsanitize=address -Walloca sallam.c -o s -lefence
-gcc -g -fsanitize=undefined,address -Walloca -o s sallam.c -lefence
+# gcc -g -fsanitize=undefined,address -Walloca -o s sallam.c -lefence
 # gcc -g -Walloca sallam.c -o s -fsanitize=undefined,address
-# gcc -g -ggdb -o s sallam.c
+gcc -g -ggdb -o s sallam.c
 # gcc -g -fsanitize=undefined,address -Walloca -o s sallam.c -lefence
 
 # Check if compilation was successful

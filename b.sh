b3945bcaee9fba28c06b4bdeadc0fb9251e415d0
Max Base <MaxBaseCode@Gmail.Com>
2024-02-29 19:02:08 +0330

    Supporting function arguments

--- b.sh
+++ b.sh
@@ -20,8 +20,8 @@ gcc -g -ggdb -o $OUTPUT_FILE $INPUT_FILE
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    # ./$OUTPUT_FILE $EXAMPLE_FILE
-    LSAN_OPTIONS=verbosity=1:log_threads=1 ./$OUTPUT_FILE $EXAMPLE_FILE
+    ./$OUTPUT_FILE $EXAMPLE_FILE
+    # LSAN_OPTIONS=verbosity=1:log_threads=1 ./$OUTPUT_FILE $EXAMPLE_FILE
     # ASAN_OPTIONS=detect_leaks=1 ./$OUTPUT_FILE $EXAMPLE_FILE
     # LSAN_OPTIONS=verbosity=1:log_threads=1:detect_leaks=1 ./$OUTPUT_FILE $EXAMPLE_FILE
 else

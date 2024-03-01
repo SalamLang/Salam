8585ee3aa8042f7f5277ff93cf1c71139c659be5
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-03-01 18:20:49 +0330

    write factorial exmaple, update bash to get input file from argv

--- b.sh
+++ b.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 # Variables
-INPUT_FILE=salam.c
-OUTPUT_FILE=s
-EXAMPLE_FILE=input.salam
+INPUT_FILE="salam.c"
+OUTPUT_FILE="s"
+EXAMPLE_FILE="input.salam"
+
+# Check if an argument is provided
+if [ "$#" -eq 1 ]; then
+    EXAMPLE_FILE="$1"
+fi
 
 # Clear screen
 cls
 clear
 
 # Delete file
-if [ -e $OUTPUT_FILE ]; then
-    rm $OUTPUT_FILE
+if [ -e "$OUTPUT_FILE" ]; then
+    rm "$OUTPUT_FILE"
 fi
 
 # Compile
-gcc -g -ggdb -o $OUTPUT_FILE $INPUT_FILE
-gcc -g -fsanitize=undefined,address -Walloca -o s $INPUT_FILE -lefence
+gcc -g -ggdb -o "$OUTPUT_FILE" "$INPUT_FILE"
+gcc -g -fsanitize=undefined,address -Walloca -o "$OUTPUT_FILE" "$INPUT_FILE" -lefence
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    ./$OUTPUT_FILE $EXAMPLE_FILE
-    # LSAN_OPTIONS=verbosity=1:log_threads=1 ./$OUTPUT_FILE $EXAMPLE_FILE
-    # ASAN_OPTIONS=detect_leaks=1 ./$OUTPUT_FILE $EXAMPLE_FILE
-    # LSAN_OPTIONS=verbosity=1:log_threads=1:detect_leaks=1 ./$OUTPUT_FILE $EXAMPLE_FILE
+    ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+    # LSAN_OPTIONS=verbosity=1:log_threads=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+    # ASAN_OPTIONS=detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+    # LSAN_OPTIONS=verbosity=1:log_threads=1:detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
 else
     echo "Compilation failed!"
 fi

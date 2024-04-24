a2da2706bd148ca3bb4ffacd6348c7cc2a472a32
John Bampton <jbampton@users.noreply.github.com>
2024-04-24 11:34:30 +1000

    Shell format (#94)
    * ~/go/bin/shfmt -l -w b.sh
    
    https://github.com/mvdan/sh?tab=readme-ov-file#shfmt

--- b.sh
+++ b.sh
@@ -7,7 +7,7 @@ EXAMPLE_FILE="input.salam"
 
 # Check if an argument is provided
 if [ "$#" -eq 1 ]; then
-    EXAMPLE_FILE="$1"
+	EXAMPLE_FILE="$1"
 fi
 
 # Clear screen
@@ -16,7 +16,7 @@ clear
 
 # Delete file
 if [ -e "$OUTPUT_FILE" ]; then
-    rm "$OUTPUT_FILE"
+	rm "$OUTPUT_FILE"
 fi
 
 # Compile
@@ -28,10 +28,10 @@ emcc salam.c -o salam.js -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=1 -s NO_EXIT_R
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
-    # LSAN_OPTIONS=verbosity=1:log_threads=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
-    # ASAN_OPTIONS=detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
-    # LSAN_OPTIONS=verbosity=1:log_threads=1:detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+	./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+	# LSAN_OPTIONS=verbosity=1:log_threads=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+	# ASAN_OPTIONS=detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
+	# LSAN_OPTIONS=verbosity=1:log_threads=1:detect_leaks=1 ./"$OUTPUT_FILE" "$EXAMPLE_FILE"
 else
-    echo "Compilation failed!"
+	echo "Compilation failed!"
 fi

08a98a618bfd4fb0186345d2c74eb2b7a589621a
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-08-12 02:23:25 +0330

    Lexer phase

--- src/main.c
+++ src/main.c
@@ -23,42 +23,13 @@ void doargs(int argc, char** argv)
     char* content = file_reads(path, &size);
 
     lexer_t* lexer = lexer_create(path, content);
-    printf("%s\n---------------------------------\n", lexer->source);
     lexer_lex(lexer);
 
-    printf("Tokens: %zu\n", lexer->tokens->size);
-    for (size_t i = 0; i < lexer->tokens->size; i++) {
-        token_t* token = lexer->tokens->data[i];
-        token->print(token);
-    }
+    lexer_debug(lexer);
 
-    // array_t* arr = array_create(sizeof(char*), 10);
-    // for (int i = 0; i < 10; i++) {
-    //     char* str = memory_allocate(10);
-    //     snprintf(str, 10, "Hello %d", i);
-    //     array_push(arr, &str);
-    // }
-    // printf("Array: ");
-    // for (size_t i = 0; i < arr->size; i++) {
-    //     char* str = *(char**)array_get(arr, i);
-    //     printf("%s ", str);
-    // }
-    
+    lexer_destroy(lexer);
 
-    // array_t* arr = array_create(sizeof(int), 10);
-    // for (int i = 0; i < 10; i++) {
-    //     array_push(arr, &i);
-    // }
-    // printf("Array: ");
-    // for (size_t i = 0; i < arr->size; i++) {
-    //     printf("%d ", *(int*)array_get(arr, i));
-    // }
-    // printf("\n");
-
-    printf("done?\n");
-    // lexer_destroy(lexer);
-
-    // memory_free(content);
+    memory_free(content);
 }
 
 int main(int argc, char** argv)

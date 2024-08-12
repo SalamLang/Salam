510ca31b12b6d08cc92e50cfd834a39da4ee0b36
Max Base <MaxBaseCode@gmail.com>
2024-08-12 16:52:03 +0330

    Working on AST

--- src/ast.h
+++ src/ast.h
@@ -1,6 +1,9 @@
 #ifndef _AST_H_
 #define _AST_H_
 
+#include <stdio.h>
+#include <string.h>
+
 #include "base.h"
 #include "memory.h"
 #include "array.h"
@@ -16,72 +19,100 @@ typedef enum {
     AST_NODE_TYPE_LAYOUT_BLOCK,
     AST_NODE_TYPE_LAYOUT_NODE,
     AST_NODE_TYPE_ERROR,
-} ast_node_type_t;
-
-struct ast_node_t;
-
+} ast_type_t;
 
 typedef enum {
     AST_NODE_BLOCK_TYPE_LAYOUT,
     AST_NODE_BLOCK_TYPE_FUNCTION,
     AST_NODE_BLOCK_TYPE_ERROR,
-} ast_node_block_type_t;
-
-typedef struct {
-    ast_node_block_type_t type;
-    array_node_t* children;
-} ast_node_block_t;
-
-typedef struct ast_node_import_t {
-    array_t* path;
-} ast_node_import_t;
-
-typedef struct ast_node_function_t {
-    char* name;
-    array_t* parameters;
-    ast_node_block_t* block;
-} ast_node_function_t;
-
-typedef struct ast_node_layout_block_t {
-    hashmap_array_t* attributes;
-    hashmap_array_t* styles;
-    array_node_layout_t* children;
-} ast_node_layout_block_t;
-
-typedef struct ast_node_layout_t {
-    ast_node_layout_block_t* block;
-} ast_node_layout_t;
+} ast_block_type_t;
 
 typedef enum {
     AST_NODE_LAYOUT_NODE_TYPE_PARAGRAPH,
     AST_NODE_LAYOUT_NODE_TYPE_BUTTON,
     AST_NODE_LAYOUT_NODE_TYPE_FORM,
     AST_NODE_LAYOUT_NODE_TYPE_DIV,
-} ast_node_layout_node_type_t;
+} ast_layout_node_type_t;
 
-typedef struct ast_node_layout_node_t {
+struct ast_t;
+
+typedef struct {
+    ast_block_type_t type;
+    array_node_t* children;
+    ast_type_t parent_type;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_block_t;
+
+typedef struct ast_import_t {
+    array_t* path;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_import_t;
+
+typedef struct ast_function_t {
+    char* name;
+    array_t* parameters;
+    ast_block_t* block;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_function_t;
+
+typedef struct ast_layout_block_t {
+    ast_type_t parent_type;
+    struct hashmap_t* attributes;
+    struct hashmap_t* styles;
+    array_node_layout_t* children;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_layout_block_t;
+
+typedef struct ast_layout_attribute_t {
+    char* key;
+    array_t values;
+    bool isStyle;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_layout_attribute_t;
+
+typedef struct ast_layout_t {
+    ast_layout_block_t* block;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_layout_t;
+
+typedef struct ast_layout_node_t {
     char* tag;
-    ast_node_layout_node_type_t type;
-    ast_node_layout_block_t* block;
-} ast_node_layout_node_t;
+    ast_layout_node_type_t type;
+    ast_layout_block_t* block;
+
+    void (*free)(void* node);
+    void (*print)(void* node);
+} ast_layout_node_t;
 
 typedef union {
-    ast_node_block_t* block;
-    ast_node_import_t* import;
-    ast_node_function_t* function;
-    ast_node_layout_t* layout;
-    ast_node_layout_block_t* layout_block;
-    ast_node_layout_node_t* layout_node;
-} ast_node_union_t;
+    ast_block_t* block;
+    ast_import_t* import;
+    ast_function_t* function;
+    ast_layout_t* layout;
+    ast_layout_block_t* layout_block;
+    ast_layout_node_t* layout_node;
+} ast_union_t;
 
-typedef struct ast_node_t {
-    ast_node_type_t type;
+typedef struct ast_t {
+    ast_type_t type;
     location_t location;
 
     void (*free)(void* node);
     void (*print)(void* node);
 
-    ast_node_union_t data;
+    ast_union_t data;
 } ast_node_t;
 
 typedef struct {
@@ -92,32 +123,71 @@ typedef struct {
  * 
  * @function ast_node_create
  * @brief Create a new AST node
- * @param {ast_node_type_t} type - Type of the AST node
+ * @param {ast_type_t} type - Type of the AST node
  * @param {location_t} location - Location of the AST node
  * @returns {ast_node_t*} - Pointer to the created AST node
  * 
  */
-ast_node_t* ast_node_create(ast_node_type_t type, location_t location);
+ast_node_t* ast_node_create(ast_type_t type, location_t location);
 
 /**
  * 
- * @function ast_node_print
+ * @function ast_layout_node_free
+ * @brief Free the AST node layout attribute
+ * @params {ast_layout_node_t*} value - AST layout node
+ * @returns {void}
+ * 
+ */
+void ast_layout_node_free(ast_layout_node_t* value);
+
+/**
+ * 
+ * @function ast_layout_node_free
+ * @brief Free the AST node layout attribute
+ * @params {ast_layout_node_t*} value - AST layout node
+ * @returns {void}
+ * 
+ */
+void ast_layout_node_free(ast_layout_node_t* value);
+
+/**
+ * 
+ * @function ast_layout_attribute_create
+ * @brief Create a new AST node layout attribute
+ * @returns {ast_layout_attribute_t*} - Pointer to the created AST node layout attribute
+ * 
+ */
+ast_layout_t* ast_layout_create();
+
+/**
+ * 
+ * @function ast_layout_attribute_create
+ * @brief Create a new AST node layout attribute
+ * @params {ast_layout_t*} value - AST layout node
+ * @returns {void}
+ * 
+ */
+void ast_layout_free(ast_layout_t* value);
+
+/**
+ * 
+ * @function ast_print
  * @brief Print the AST node
  * @param {ast_node_t*} node - AST node
  * @returns {void}
  * 
  */
-void ast_node_print(ast_node_t* node);
+void ast_print(ast_node_t* node);
 
 /**
  * 
  * @function ast_node_free
  * @brief Free the AST node
- * @param {ast_node_t*} node - AST node
+ * @param {ast_node_t*} value - AST node
  * @returns {void}
  * 
  */
-void ast_node_free(ast_node_t* node);
+void ast_node_free(ast_node_t* value);
 
 /**
  * 
@@ -147,5 +217,34 @@ void ast_debug(ast_t* ast);
  * 
  */
 void ast_free(ast_t* ast);
+
+/**
+ * 
+ * @function ast_layout_attribute_create
+ * @brief Create a new AST node layout attribute
+ * @returns {ast_layout_block_t*} - Pointer to the created AST node layout block
+ * 
+ */
+ast_layout_block_t* ast_layout_block_create();
+
+/**
+ * 
+ * @function ast_layout_block_free
+ * @brief Free the AST node layout block
+ * @params {ast_layout_block_t*} value - AST layout block
+ * @returns {void}
+ * 
+ */
+void ast_layout_block_free(ast_layout_block_t* value);
+
+/**
+ * 
+ * @function ast_layout_attribute_create
+ * @brief Create a new AST node layout attribute
+ * @params {ast_layout_node_type_t} type - Type of the layout node
+ * @returns {ast_layout_node_t*} - Pointer to the created AST node layout attribute
+ * 
+ */
+ast_layout_node_t* ast_layout_node_create(ast_layout_node_type_t type);
 
 #endif

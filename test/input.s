5072ce1db2dde289dee58157ccf689ccb52e7751
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-08-16 01:59:52 +0330

    Delete colon after attrs

--- test/input.s
+++ test/input.s
@@ -13,35 +13,35 @@ fn main {
 }
 
 layout {
-	dir: "ltr"
-	title: "my site name"
-	description: "desc"
-	author: "Max Base"
+	dir "ltr"
+	title "my site name"
+	description "desc"
+	author "Max Base"
 	
-	background: red
-	color: white
-	margin-top: 40
+	background red
+	color white
+	margin-top 40
 
-	// content: "In the name of god!2222"
+	// content "In the name of god!2222"
 
 	box {
-		content: "Hey, mate!"
-		display: flex
-		flex-direction: column
-		// flex-direction: row
+		content "Hey, mate!"
+		display flex
+		flex-direction column
+		// flex-direction row
 
 		span {
-			background: green
-			color: white
-			content: "Hey, mate!"
+			background green
+			color white
+			content "Hey, mate!"
 		}
 
 		span {
-			background: orange
-			color: black
-			content: "Hey, mate!"
+			background orange
+			color black
+			content "Hey, mate!"
 		}
 	}
 	
-	content: "In the name of god!"
+	content "In the name of god!"
 }

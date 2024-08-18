255182b330c03ffa0890016fb39683238704961a
Max Base <MaxBaseCode@gmail.com>
2024-08-18 04:27:51 +0330

    Fix problem of sub tags

--- src/ast_layout_type.h
+++ src/ast_layout_type.h
@@ -1,9 +1,11 @@
+// ADD_LAYOUT_TYPE(TYPE, NAME, NAME_LOWER, GENERATED_NAME, ENDUSER_NAME)
+
 // Paragraph Types
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_PARAGRAPH, "PARAGRAPH", "paragraph", "p", "paragraph")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_PARAGRAPH_RAW, "PARAGRAPH_RAW", "paragraph_raw", "", "")
 
 // Form Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_BUTTON, "BUTTON", "button", "button", "button")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_BUTTON, "BUTTON", "button", "button", "دکمه")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_INPUT, "INPUT", "input", "input", "input")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TEXTAREA, "TEXTAREA", "textarea", "textarea", "textarea")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LABEL, "LABEL", "label", "label", "label")
@@ -11,38 +13,38 @@ ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SELECT, "SELECT", "select", "select", "select")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_OPTION, "OPTION", "option", "option", "option")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_FIELDSET, "FIELDSET", "fieldset", "fieldset", "fieldset")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LEGEND, "LEGEND", "legend", "legend", "legend")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_FORM, "FORM", "form", "form", "form")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_FORM, "FORM", "form", "form", "فرم")
 
 // Text Elements
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SPAN, "SPAN", "span", "span", "span")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_STRONG, "STRONG", "strong", "strong", "strong")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_STRONG, "STRONG", "strong", "strong", "پرقدرت")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_EM, "EM", "em", "em", "em")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ITALIC, "ITALIC", "italic", "i", "italic")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_BOLD, "BOLD", "bold", "b", "bold")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ITALIC, "ITALIC", "italic", "i", "کج")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_BOLD, "BOLD", "bold", "b", "درشت")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_UNDERLINE, "UNDERLINE", "underline", "u", "underline")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SMALL, "SMALL", "small", "small", "small")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SMALL, "SMALL", "small", "small", "کوچک")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_BIG, "BIG", "big", "big", "big")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SUB, "SUB", "sub", "sub", "sub")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SUP, "SUP", "sup", "sup", "sup")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_S, "S", "s", "s", "s")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_CENTER, "CENTER", "center", "center", "center")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_CENTER, "CENTER", "center", "center", "مرکز")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_MARK, "MARK", "mark", "mark", "mark")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_Q, "Q", "q", "q", "q")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_CITE, "CITE", "cite", "cite", "cite")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_DFN, "DFN", "dfn", "dfn", "dfn")
 
 // Sectioning Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_HEADER, "HEADER", "header", "header", "header")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_FOOTER, "FOOTER", "footer", "footer", "footer")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_NAV, "NAV", "nav", "nav", "nav")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_HEADER, "HEADER", "header", "header", "هدر")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_FOOTER, "FOOTER", "footer", "footer", "فوتر")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_NAV, "NAV", "nav", "nav", "منو")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_MAIN, "MAIN", "main", "main", "main")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SECTION, "SECTION", "section", "section", "section")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ARTICLE, "ARTICLE", "article", "article", "article")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SECTION, "SECTION", "section", "section", "سکشن")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ARTICLE, "ARTICLE", "article", "article", "مقاله")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ASIDE, "ASIDE", "aside", "aside", "aside")
 
 // Multimedia Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_VIDEO, "VIDEO", "video", "video", "video")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_AUDIO, "AUDIO", "audio", "audio", "audio")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_VIDEO, "VIDEO", "video", "video", "فیلم")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_AUDIO, "AUDIO", "audio", "audio", "اهنگ")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_IMG, "IMG", "img", "img", "img")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_IFRAME, "IFRAME", "iframe", "iframe", "iframe")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_CANVAS, "CANVAS", "canvas", "canvas", "canvas")
@@ -56,17 +58,17 @@ ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_MENUITEM, "MENUITEM", "menuitem", "menuitem", "m
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_COMMAND, "COMMAND", "command", "command", "command")
 
 // List Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_UL, "UL", "ul", "ul", "ul")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_OL, "OL", "ol", "ol", "ol")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LI, "LI", "li", "li", "li")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_UL, "UL", "ul", "ul", "لیست")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_OL, "OL", "ol", "ol", "آیتم")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LI, "LI", "li", "li", "ایتم")
 
 // Table Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE, "TABLE", "table", "table", "table")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE, "TABLE", "table", "table", "جدول")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_HEADER, "TABLE_HEADER", "table_header", "thead", "thead")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_BODY, "TABLE_BODY", "table_body", "tbody", "tbody")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_FOOTER, "TABLE_FOOTER", "table_footer", "tfoot", "tfoot")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_TR, "TABLE_TR", "table_tr", "tr", "tr")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_TD, "TABLE_TD", "table_td", "td", "td")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_TR, "TABLE_TR", "table_tr", "tr", "ردیف")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_TD, "TABLE_TD", "table_td", "td", "ستون")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_TABLE_TH, "TABLE_TH", "table_th", "th", "th")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_CAPTION, "CAPTION", "caption", "caption", "caption")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_COL, "COL", "col", "col", "col")
@@ -90,9 +92,9 @@ ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_SCRIPT, "SCRIPT", "script", "script", "script")
 ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_STYLE, "STYLE", "style", "style", "style")
 
 // Special Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_DIV, "DIV", "div", "div", "div")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LINK, "LINK", "link", "a", "a")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_DIV, "DIV", "div", "div", "جعبه")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_LINK, "LINK", "link", "a", "لینک")
 
 // Utility Elements
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_NONE, "NONE", "none", "layout", "layout")
-ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ERROR, "ERROR", "error", "error", "error")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_NONE, "NONE", "none", "layout", "")
+ADD_LAYOUT_TYPE(AST_LAYOUT_TYPE_ERROR, "ERROR", "error", "error", "")

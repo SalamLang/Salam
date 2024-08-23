6e283c02a9e6d1fa0238dbc8f1cf53dcc865fe8f
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-08-23 18:09:47 +0330

    create parser layout file

--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,21 @@
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
+#include <stdbool.h>
+
 #include "base.h"
 #include "ast.h"
 #include "lexer.h"
 #include "validator.h"
+
+#define PARSER_NEXT lexer->token_index++
+#define PARSER_PREV lexer->token_index--
+
+#define PARSER_CURRENT ((token_t*)lexer->tokens->data[lexer->token_index])
+#define PARSER_CURRENT_NEXT ((token_t*)lexer->tokens->data[lexer->token_index + 1])
+#define PARSER_CURRENT_PREV ((token_t*)lexer->tokens->data[lexer->token_index + 1])
+
+#include "parser_layout.h"
 
 /**
  *
@@ -63,28 +74,6 @@ void parser_parse_block(lexer_t* lexer, ast_block_t* block);
 
 /**
  *
- * @function parser_parse_layout_block
- * @brief Parse the block
- * @params {ast_layout_block_t*} block - AST layout block
- * @params {lexer_t*} lexer - Lexer
- * @params {ast_type_t} block_parent_type - Block parent type
- * @returns {void}
- *
- */
-void parser_parse_layout_block(ast_layout_block_t* block, lexer_t* lexer);
-
-/**
- *
- * @function parser_parse_layout
- * @brief Parse the layout
- * @params {lexer_t*} lexer - Lexer
- * @returns {ast_node_t*} - AST node
- *
- */
-ast_node_t* parser_parse_layout(lexer_t* lexer);
-
-/**
- *
  * @function parser_parse_node
  * @brief Parse the node
  * @params {lexer_t*} lexer - Lexer
@@ -102,56 +91,6 @@ ast_node_t* parser_parse_node(lexer_t* lexer);
  *
  */
 ast_t* parser_parse(lexer_t* lexer);
-
-/**
- *
- * @function parser_layout
- * @brief Parse the layout
- * @params {lexer_t*} lexer - Lexer
- * @returns {ast_node_t*} - AST node
- *
- */
-ast_node_t* parser_layout(lexer_t* lexer);
-
-/**
- *
- * @function parser_parse_layout_block_children
- * @brief Parse the block children
- * @params {ast_layout_block_t*} block - AST layout block node
- * @params {lexer_t*} lexer - Lexer
- * @params {char*} name - Name of the attribute
- * @params {token_t*} last_name - Last token
- * @returns {void}
- *
- */
-void parser_parse_layout_block_children(ast_layout_block_t* block, lexer_t* lexer, char* name, token_t* last_name);
-
-/**
- *
- * @function parser_parse_layout_block_attribute
- * @brief Parse the block attribute
- * @params {bool} onlyStyle - Only style
- * @params {ast_layout_block_t*} block - AST layout block node
- * @params {hashmap_t*} normal - Normal hashmap
- * @params {lexer_t*} lexer - Lexer
- * @params {char*} name - Name of the attribute
- * @params {token_t*} last_name - Last token
- * @returns {void}
- *
- */
-void parser_parse_layout_block_attribute(bool onlyStyle, ast_layout_block_t* block, hashmap_t* normal, lexer_t* lexer, char* name, token_t* last_name);
-
-/**
- *
- * @function parser_parse_layout_node
- * @brief Parsing layout node
- * @params {lexer_t*} lexer - Lexer
- * @params {char*} name - Name of the node
- * @params {token_t*} last_name - Last token
- * @returns {ast_layout_node_t*} - AST layout node
- *
- */
-ast_layout_node_t* parser_parse_layout_node(lexer_t* lexer, char* name, token_t* last_name);
 
 /**
  *
@@ -225,16 +164,6 @@ array_value_t* parser_parse_expressions_maybe(lexer_t* lexer);
 
 /**
  *
- * @function parser_parse_layout_value
- * @brief Parse the layout value
- * @params {lexer_t*} lexer - Lexer
- * @returns {ast_value_t*} - AST Layout attribute value
- *
- */
-ast_value_t* parser_parse_layout_value(lexer_t* lexer);
-
-/**
- *
  * @function is_begin_block_token
  * @brief Check if the token is a begin block
  * @params {token_t*} token - Token
@@ -282,18 +211,6 @@ bool match_next_open_block(lexer_t* lexer);
  */
 bool match_next_close_block(lexer_t* lexer);
 
-/**
- *
- * @function parser_parse_layout_block_style_state
- * @brief Parse the block style state
- * @params {ast_layout_block_t*} block - AST layout block node
- * @params {lexer_t*} lexer - Lexer
- * @params {char*} name - Name of the attribute
- * @params {token_t*} last_name - Last token
- * @returns {void}
- *
- */
-void parser_parse_layout_block_style_state(ast_layout_block_t* block, lexer_t* lexer, char* name, token_t* last_name);
 
 /**
  *
@@ -317,23 +234,11 @@ bool match_close_block(lexer_t* lexer);
 
 /**
  *
- * @function parser_parse_layout_name
- * @brief Parse the layout name
- * @params {lexer_t*} lexer - Lexer
- * @params {token_t**} last_name - Last token
- * @returns {string_t*} - String
+ * @function expect_close_block
+ * @brief Expect an open block
+ * @returns {void}
  *
  */
-string_t* parser_parse_layout_name(lexer_t* lexer, token_t** last_name);
-
-/**
- *
- * @function parser_parse_layout_values
- * @brief Parse the layout values
- * @params {lexer_t*} lexer - Lexer
- * @returns {array_value_t*} - Array of AST values
- *
- */
-array_value_t* parser_parse_layout_values(lexer_t* lexer);
+void expect_close_block(lexer_t* lexer);
 
 #endif
---
+++ src/parser_layout.h
@@ -0,0 +1,129 @@
+#ifndef _PARSER_LAYOUT_H_
+#define _PARSER_LAYOUT_H_
+
+#include <stdbool.h>
+
+#include "base.h"
+#include "ast.h"
+#include "lexer.h"
+#include "validator.h"
+#include "parser_layout.h"
+
+/**
+ *
+ * @function parser_parse_layout_block
+ * @brief Parse the block
+ * @params {ast_layout_block_t*} block - AST layout block
+ * @params {lexer_t*} lexer - Lexer
+ * @params {ast_type_t} block_parent_type - Block parent type
+ * @returns {void}
+ *
+ */
+void parser_parse_layout_block(ast_layout_block_t* block, lexer_t* lexer);
+
+/**
+ *
+ * @function parser_parse_layout
+ * @brief Parse the layout
+ * @params {lexer_t*} lexer - Lexer
+ * @returns {ast_node_t*} - AST node
+ *
+ */
+ast_node_t* parser_parse_layout(lexer_t* lexer);
+
+/**
+ *
+ * @function parser_layout
+ * @brief Parse the layout
+ * @params {lexer_t*} lexer - Lexer
+ * @returns {ast_node_t*} - AST node
+ *
+ */
+ast_node_t* parser_layout(lexer_t* lexer);
+
+/**
+ *
+ * @function parser_parse_layout_block_children
+ * @brief Parse the block children
+ * @params {ast_layout_block_t*} block - AST layout block node
+ * @params {lexer_t*} lexer - Lexer
+ * @params {char*} name - Name of the attribute
+ * @params {token_t*} last_name - Last token
+ * @returns {void}
+ *
+ */
+void parser_parse_layout_block_children(ast_layout_block_t* block, lexer_t* lexer, char* name, token_t* last_name);
+
+/**
+ *
+ * @function parser_parse_layout_block_attribute
+ * @brief Parse the block attribute
+ * @params {bool} onlyStyle - Only style
+ * @params {ast_layout_block_t*} block - AST layout block node
+ * @params {hashmap_t*} normal - Normal hashmap
+ * @params {lexer_t*} lexer - Lexer
+ * @params {char*} name - Name of the attribute
+ * @params {token_t*} last_name - Last token
+ * @returns {void}
+ *
+ */
+void parser_parse_layout_block_attribute(bool onlyStyle, ast_layout_block_t* block, hashmap_t* normal, lexer_t* lexer, char* name, token_t* last_name);
+
+/**
+ *
+ * @function parser_parse_layout_node
+ * @brief Parsing layout node
+ * @params {lexer_t*} lexer - Lexer
+ * @params {char*} name - Name of the node
+ * @params {token_t*} last_name - Last token
+ * @returns {ast_layout_node_t*} - AST layout node
+ *
+ */
+ast_layout_node_t* parser_parse_layout_node(lexer_t* lexer, char* name, token_t* last_name);
+
+/**
+ *
+ * @function parser_parse_layout_value
+ * @brief Parse the layout value
+ * @params {lexer_t*} lexer - Lexer
+ * @returns {ast_value_t*} - AST Layout attribute value
+ *
+ */
+ast_value_t* parser_parse_layout_value(lexer_t* lexer);
+
+/**
+ *
+ * @function parser_parse_layout_block_style_state
+ * @brief Parse the block style state
+ * @params {ast_layout_block_t*} block - AST layout block node
+ * @params {lexer_t*} lexer - Lexer
+ * @params {char*} name - Name of the attribute
+ * @params {token_t*} last_name - Last token
+ * @returns {void}
+ *
+ */
+void parser_parse_layout_block_style_state(ast_layout_block_t* block, lexer_t* lexer, char* name, token_t* last_name);
+
+
+/**
+ *
+ * @function parser_parse_layout_name
+ * @brief Parse the layout name
+ * @params {lexer_t*} lexer - Lexer
+ * @params {token_t**} last_name - Last token
+ * @returns {string_t*} - String
+ *
+ */
+string_t* parser_parse_layout_name(lexer_t* lexer, token_t** last_name);
+
+/**
+ *
+ * @function parser_parse_layout_values
+ * @brief Parse the layout values
+ * @params {lexer_t*} lexer - Lexer
+ * @returns {array_value_t*} - Array of AST values
+ *
+ */
+array_value_t* parser_parse_layout_values(lexer_t* lexer);
+
+#endif

a97a05f0dcbc809bcae71bc060efcb19f8b5e061
Max Base <MaxBaseCode@gmail.com>
2024-08-23 17:25:06 +0330

    add font-display for font block

--- src/validator.c
+++ src/validator.c
@@ -446,6 +446,13 @@ bool token_belongs_to_ast_layout_node(ast_layout_attribute_type_t attribute_key_
 					return false;
 				}
 			}
+			else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_STYLE_FONT_DISPLAY) {
+				if (validate_style_value_string(NULL, NULL, attribute, ast_layout_allowed_style_list_font_display, NULL) == false) {
+					error_validator(2, "Invalid value for attribute '%s' in '%s' element at line %zu column %zu!", attribute->key, ast_layout_node_type_to_enduser_name(attribute->parent_node_type), attribute->value_location.start_line, attribute->value_location.start_column);
+
+					return false;
+				}
+			}
 			else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_FONT_UNICODE_RANGE) {
 				// TODO: do more check layer
 			}

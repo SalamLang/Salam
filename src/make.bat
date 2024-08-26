63c7e6efe427fecdd26be6c89226b0169c5466bb
Max Base <MaxBaseCode@gmail.com>
2024-08-26 16:58:41 +0330

    Update gitignore to ignore wino files which is object file for windows os

--- src/make.bat
+++ src/make.bat
@@ -1,22 +1,22 @@
-gcc  -c log.c -o log.o
-gcc  -c file.c -o file.o
-gcc  -c memory.c -o memory.o
-gcc  -c array.c -o array.o
-gcc  -c parser.c -o parser.o
-gcc  -c parser_layout.c -o parser_layout.o
-gcc  -c generator.c -o generator.o
-gcc  -c generator_layout.c -o generator_layout.o
-gcc  -c generator_layout_style.c -o generator_layout_style.o
-gcc  -c generator_identifier.c -o generator_identifier.o
-gcc  -c string.c -o string.o
-gcc  -c validator.c -o validator.o
-gcc  -c hashmap.c -o hashmap.o
-gcc  -c hashmap_custom.c -o hashmap_custom.o
-gcc  -c array_custom.c -o array_custom.o
-gcc  -c lexer.c -o lexer.o
-gcc  -c ast.c -o ast.o
-gcc  -c ast_layout.c -o ast_layout.o
-gcc  -c ast_layout_style.c -o ast_layout_style.o
-gcc  -c main.c -o main.o
-gcc  -o main log.o file.o memory.o array.o parser.o parser_layout.o generator.o generator_layout.o generator_layout_style.o generator_identifier.o string.o validator.o hashmap.o hashmap_custom.o array_custom.o lexer.o ast.o ast_layout.o ast_layout_style.o main.o
+gcc  -c log.c -o log.wino
+gcc  -c file.c -o file.wino
+gcc  -c memory.c -o memory.wino
+gcc  -c array.c -o array.wino
+gcc  -c parser.c -o parser.wino
+gcc  -c parser_layout.c -o parser_layout.wino
+gcc  -c generator.c -o generator.wino
+gcc  -c generator_layout.c -o generator_layout.wino
+gcc  -c generator_layout_style.c -o generator_layout_style.wino
+gcc  -c generator_identifier.c -o generator_identifier.wino
+gcc  -c string.c -o string.wino
+gcc  -c validator.c -o validator.wino
+gcc  -c hashmap.c -o hashmap.wino
+gcc  -c hashmap_custom.c -o hashmap_custom.wino
+gcc  -c array_custom.c -o array_custom.wino
+gcc  -c lexer.c -o lexer.wino
+gcc  -c ast.c -o ast.wino
+gcc  -c ast_layout.c -o ast_layout.wino
+gcc  -c ast_layout_style.c -o ast_layout_style.wino
+gcc  -c main.c -o main.wino
+gcc  -o main log.wino file.wino memory.wino array.wino parser.wino parser_layout.wino generator.wino generator_layout.wino generator_layout_style.wino generator_identifier.wino string.wino validator.wino hashmap.wino hashmap_custom.wino array_custom.wino lexer.wino ast.wino ast_layout.wino ast_layout_style.wino main.wino
 main ../example/test6.salam

8f3d0a692c609659b59f1d1da7918186efcd7c50
Max Base <MaxBaseCode@gmail.com>
2024-09-03 18:26:20 +0330

    delete log ast_destroy

--- src/base.h
+++ src/base.h
@@ -22,8 +22,8 @@
 #define BRIGHT_CYAN "\033[96m"
 #define BRIGHT_WHITE "\033[97m"
 
-#define DEBUG_ME printf(GRAY "DEBUG: %s:%d in %s\n" RESET, __FILE__, __LINE__, __func__)
-// #define DEBUG_ME
+// #define DEBUG_ME printf(GRAY "DEBUG: %s:%d in %s\n" RESET, __FILE__, __LINE__, __func__)
+#define DEBUG_ME
 
 #define STYLE_STYLE_LINKING '.'
 

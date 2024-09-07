2fcc1e7e6b9e3a7ea84daf1e89a6ba0fc1cfcdb9
Max Base <MaxBaseCode@gmail.com>
2024-09-07 15:45:29 +0330

    checking array in mac

--- src/array.h
+++ src/array.h
@@ -5,11 +5,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include  <string.h>
+#include <string.h>
 
-typedef struct array_t
-
-{
+typedef struct array_t {
     void **data;
     size_t length;
     size_t capacity;

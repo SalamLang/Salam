289777c3ff6e1ca0c95acdd7347a9c851b5522b2
Max Base <MaxBaseCode@gmail.com>
2024-09-07 15:53:42 +0330

    rename lines

--- src/build-windows.bat
+++ src/build-windows.bat
@@ -4,7 +4,7 @@ REM Set the output executable name
 set output=salam
 
 REM List of source files
-set sources=log.c file.c memory.c array.c parser.c parser_layout.c generator.c generator_layout.c generator_layout_style.c generator_identifier.c string.c validator.c hashmap.c hashmap_custom.c array_custom.c lexer.c ast.c ast_layout.c ast_layout_style.c main.c
+set sources=log.c file.c memory.c array.c parser.c parser_layout.c generator.c generator_layout.c generator_layout_style.c generator_identifier.c string_buffer.c validator.c hashmap.c hashmap_custom.c array_custom.c lexer.c ast.c ast_layout.c ast_layout_style.c main.c
 
 REM Compile each source file into an object file
 for %%f in (%sources%) do (

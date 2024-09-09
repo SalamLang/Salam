9bd3eedc54353a04bd8c920858d44a8c1589a5b6
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-09-09 05:18:26 +0330

    up

--- src/generator_salam.h
+++ src/generator_salam.h
@@ -3,6 +3,7 @@
 
 #include "ast.h"
 #include "generator.h"
+#include "generator_salam.h"
 #include "string_buffer.h"
 
 /**
@@ -14,5 +15,36 @@
  *
  */
 string_t* generator_salam(ast_t* ast);
+
+/**
+ *
+ * @function generator_salam_layout
+ * @brief Generate the Salam code for the layout
+ * @params {ast_layout_t*} layout - Layout
+ * @returns {void}
+ *
+ */
+void generator_salam_layout(string_t* salam, ast_layout_t* layout);
+
+/**
+ *
+ * @function generator_salam_layout_block
+ * @brief Generate the Salam code for the layout block
+ * @params {ast_layout_block_t*} block - Layout block
+ * @returns {void}
+ *
+ */
+void generator_salam_layout_block(string_t* salam, ast_layout_block_t* block);
+
+/**
+ *
+ * @function generator_salam_ident
+ * @brief Generate the Salam code for the indentation
+ * @params {string_t*} buffer - Buffer
+ * @params {size_t} ident - Identation
+ * @returns {void}
+ *
+ */
+void generator_salam_ident(string_t* buffer, size_t ident);
 
 #endif

7bfe8271e0e92f1ca9c6adff52d5beffeda8c2e8
Max Base <MaxBaseCode@gmail.com>
2024-09-09 05:50:42 +0330

    change EXPORTED_FUNCTIONS

--- src/build-webassembly.sh
+++ src/build-webassembly.sh
@@ -37,7 +37,7 @@ emcc "${sources[@]}" -o ${OUTPUT_BASE}.html \
     -s ALLOW_MEMORY_GROWTH=1 \
 	-s EXIT_RUNTIME=1 \
     -s NO_EXIT_RUNTIME=1 \
-    -s "EXPORTED_FUNCTIONS=['_main']"
+    # -s "EXPORTED_FUNCTIONS=['_main']"
 	# -s ASSERTIONS=0 \
 	# -s VERBOSE=1 \
 

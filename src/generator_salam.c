c5691968667002548a866df1dffffd87ef0b958b
Max Base <MaxBaseCode@gmail.com>
2024-09-11 11:59:37 +0330

    Continue working on generate salam code

--- src/generator_salam.c
+++ src/generator_salam.c
@@ -2,6 +2,8 @@
 
 #define IDENT(SIZE) generator_salam_ident(salam, SIZE);
 
+size_t generator_salam_ident_size = 0;
+
 /**
  *
  * @function generator_salam
@@ -47,35 +49,125 @@ void generator_salam_layout_block(string_t* salam, ast_layout_block_t* block) {
     char* block_node_name =
         ast_layout_node_type_to_enduser_name(block->parent_node_type);
 
-    IDENT(0);
+    IDENT(generator_salam_ident_size);
     string_append_str(salam, block_node_name);
     string_append_str(salam, ":\n");
 
-    IDENT(0);
+    generator_salam_ident_size++;
+
+    generator_salam_layout_attributes(salam, block->attributes);
+
+    generator_salam_layout_styles(salam, block->styles);
+
+    generator_salam_layout_states(salam, block->states);
+
+    generator_salam_ident_size--;
+
+    IDENT(generator_salam_ident_size);
     string_append_str(salam, "تمام\n");
+}
+
+/**
+ *
+ * @function generator_salam_layout_attribute
+ * @brief Generate the Salam code for the attribute
+ * @params {string_t*} salam - Buffer
+ * @params {ast_layout_attribute_t*} attribute - Attribute
+ * @returns {void}
+ *
+ */
+void generator_salam_layout_attribute(string_t* salam,
+                                      ast_layout_attribute_t* attribute) {
+    IDENT(generator_salam_ident_size);
+    char* attribute_name = ast_layout_attribute_type_to_name(attribute->type);
+    char* attribute_value = array_value_stringify(attribute->values, ", ");
+
+    string_append_str(salam, attribute_name);
+    string_append_str(salam, ": ");
+    string_append_str(salam, attribute_value);
+    string_append_str(salam, "\n");
+}
+
+/**
+ *
+ * @function generator_salam_layout_attributes
+ * @brief Generate the Salam code for the attributes
+ * @params {string_t*} salam - Buffer
+ * @params {hashmap_t*} attributes - Attributes
+ * @returns {void}
+ *
+ */
+void generator_salam_layout_attributes(string_t* salam, hashmap_t* attributes) {
+    if (attributes != NULL) {
+        size_t attribute_capacity = attributes->capacity;
+
+        for (size_t i = 0; i < attribute_capacity; i++) {
+            hashmap_entry_t* entry = attributes->data[i];
+
+            while (entry) {
+                ast_layout_attribute_t* attribute =
+                    cast(ast_layout_attribute_t*, entry->value);
+
+                if (attribute->isStyle == true ||
+                    attribute->isContent == true) {
+                } else {
+                    generator_salam_layout_attribute(salam, attribute);
+                }
+
+                entry = cast(hashmap_entry_t*, entry->next);
+            }
+        }
+    }
+}
+
+/**
+ *
+ * @function generator_salam_layout_styles
+ * @brief Generate the Salam code for the styles
+ * @params {string_t*} salam - Buffer
+ * @params {ast_layout_style_state_t*} styles - Styles
+ * @returns {void}
+ *
+ */
+void generator_salam_layout_styles(string_t* salam,
+                                   ast_layout_style_state_t* styles) {
+    // TODO
+}
+
+/**
+ *
+ * @function generator_salam_layout_states
+ * @brief Generate the Salam code for the states
+ * @params {string_t*} salam - Buffer
+ * @params {hashmap_t*} states - States
+ * @returns {void}
+ *
+ */
+void generator_salam_layout_states(string_t* salam, hashmap_t* states) {
+    // TODO
 }
 
 /**
  *
  * @function generator_salam_ident
  * @brief Generate the Salam code for the indentation
- * @params {string_t*} buffer - Buffer
+ * @params {string_t*} salam - Buffer
  * @params {size_t} ident - Indentation
  * @returns {void}
  *
  */
-void generator_salam_ident(string_t* buffer, size_t ident) {
+void generator_salam_ident(string_t* salam, size_t ident) {
     DEBUG_ME;
     if (ident == 0) {
         return;
     }
 
     for (size_t i = 0; i < ident; i++) {
-        // string_append_str(buffer, "  ");
-        // string_append_str(buffer, "    ");
-        // string_append_str(buffer, "      ");
-        // string_append_str(buffer, "        ");
-        // string_append_str(buffer, "\t\t");
-        string_append_char(buffer, '\t');
+        // string_append_str(salam, "  ");
+        // string_append_str(salam, "    ");
+        // string_append_str(salam, "      ");
+        // string_append_str(salam, "        ");
+        // string_append_str(salam, "\t\t");
+        string_append_char(salam, '\t');
     }
 }

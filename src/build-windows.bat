d58b002e4b1e39c13b02018caaf7f741ac8f7dc1
Max Base <MaxBaseCode@gmail.com>
2024-11-04 13:43:51 +0330

    create out dir in build win

--- src/build-windows.bat
+++ src/build-windows.bat
@@ -7,6 +7,12 @@ set output=salam
 REM List of source files
 set sources=log.c file.c memory.c downloader.c array.c parser.c parser_layout.c generator.c generator_layout.c generator_salam.c generator_layout_style.c generator_identifier.c string_buffer.c validator.c hashmap.c hashmap_custom.c array_custom.c lexer.c ast.c ast_layout.c ast_layout_style.c main.c
 
+REM Ensure the output directory exists
+if not exist "..\out" (
+	echo Creating output directory...
+	mkdir ..\out
+)
+
 REM Compile each source file into an object file
 for %%f in (%sources%) do (
 	echo Compiling %%f...

cffc655042f87ef9dd555b78f1daa37cb17d5d19
Max Base <MaxBaseCode@gmail.com>
2024-11-04 21:32:17 +0330

    Add valid scripts

--- config/layout/type.validate.py
+++ config/layout/type.validate.py
@@ -17,10 +17,16 @@ for doc in docs:
 		if "id" not in item:
 			print(item)
 			error("id is required and missed in an item")
+		
+		if "is_mother" in item:
+			if item["is_mother"] != True and item["is_mother"] != False:
+				print(item)
+				error("is_mother is invalid in an item")
 
 		if "text" in item:
 			for lang in LANGUAGES:
 				if lang not in item["text"]:
+					print(item)
 					error("text is required for " + lang + " language")
 
 print(FILE + ": Validation is successful")

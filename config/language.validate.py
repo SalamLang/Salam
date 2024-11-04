b9c9987ee39cecd1ebb1782b8048e7d22c45eb8e
Max Base <MaxBaseCode@gmail.com>
2024-11-04 20:34:43 +0330

    Add more validate scripts, fix some issues in layout/type.yaml

---
+++ config/language.validate.py
@@ -0,0 +1,26 @@
+import yaml
+
+FILE = "language.yaml"
+LANGUAGES = ["en", "fa"]
+
+file = open(FILE, "r", encoding="utf-8")
+docs = yaml.safe_load_all(file)
+
+
+def error(msg):
+    print("Error: " + msg)
+    exit(1)
+
+
+for doc in docs:
+    for item in doc["items"]:
+        if "id" not in item:
+            error("id is required and missed in an item")
+
+        if "name" not in item:
+            error("name is required and missed in an item")
+
+        if "local_name" not in item:
+            error("local_name is required and missed in an item")
+
+print(FILE + ": Validation is successful")

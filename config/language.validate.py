cf233126cd198dbf1d3f0c7f4bd257370eba4dd2
John Bampton <jbampton@gmail.com>
2024-11-05 15:10:59 +1000

    Fix pre-commit

--- config/language.validate.py
+++ config/language.validate.py
@@ -8,22 +8,22 @@ docs = yaml.safe_load_all(file)
 
 
 def error(msg):
-	print("Error: " + msg)
-	exit(1)
+    print("Error: " + msg)
+    exit(1)
 
 
 for doc in docs:
-	for item in doc["items"]:
-		if "id" not in item:
-			print(item)
-			error("id is required and missed in an item")
+    for item in doc["items"]:
+        if "id" not in item:
+            print(item)
+            error("id is required and missed in an item")
 
-		if "name" not in item:
-			print(item)
-			error("name is required and missed in an item")
+        if "name" not in item:
+            print(item)
+            error("name is required and missed in an item")
 
-		if "local_name" not in item:
-			print(item)
-			error("local_name is required and missed in an item")
+        if "local_name" not in item:
+            print(item)
+            error("local_name is required and missed in an item")
 
 print(FILE + ": Validation is successful")

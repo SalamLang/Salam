1f4c3d8e51e19e7d50bdcb9215b5bc0e996f004e
Max Base <MaxBaseCode@gmail.com>
2024-11-17 15:07:36 +0330

    Add validate test script for global_value and state yaml files in style dir

---
+++ config/layout/attribute/style/state.validate.py
@@ -0,0 +1,32 @@
+import yaml
+
+FILE = "global_value.yaml"
+LANGUAGES = ["en", "fa"]
+ALL_ATTRS = ["id", "generate_name", "text"]
+
+file = open(FILE, "r", encoding="utf-8")
+docs = yaml.safe_load_all(file)
+
+
+def error(msg):
+	print("Error: " + msg)
+	exit(1)
+
+
+for doc in docs:
+	for item in doc["items"]:
+		if "id" not in item:
+			print(item)
+			error("id is required and missed in an item")
+		
+		if "generate_name" not in item:
+			print(item)
+			error("generate_name is required")
+
+		if "text" in item:
+			for lang in LANGUAGES:
+				if lang not in item["text"]:
+					print(item)
+					error("text is required for " + lang + " language")
+
+print(FILE + ": Validation is successful")

87981c38e38f94f83bbf23964daff845a63df6d4
Max Base <MaxBaseCode@gmail.com>
2024-11-18 17:11:07 +0330

    continue working on sync script

--- config/sync.py
+++ config/sync.py
@@ -12,81 +12,52 @@ def printify_type(item):
 		f"\"{item['id'].replace('AST_TYPE_', '').lower()}\""
 		f")"
 	)
- 
+
 def printify_layout_type(item):
-	# if item['text'][SELECTED_LANGUAGE] is string so use it, otherwise foreach on it and use REPEAT for second and the next one (not first)
-	# If 'generate_name' is missing, use ADD_LAYOUT_TYPE_HIDE
-	return (
-		f"ADD_LAYOUT_TYPE_HIDE({item['id']}, "
-		f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-		f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '').lower()}\", "
-		f"\"{item['text'][SELECTED_LANGUAGE]}\", "
-		f"{str(item.get('is_mother', False)).lower()})"
-		
-		if 'generate_name' not in item else (
-			# Use ADD_LAYOUT_TYPE_REPEAT if 'repeat' is true
-			f"ADD_LAYOUT_TYPE_REPEAT({item['id']}, "
+	def command(value):
+		return (
+			f"({item['id']}, "
 			f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-			f"\"{item['generate_name']}\", "
-			f"\"{item['text']['en']}\", "
-			f"\"{item['text']['fa']}\", "
+			f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '').lower()}\", "
+			f"\"{item.get('generate_name', item['id'].replace('AST_LAYOUT_TYPE_', '').lower())}\", "
+			f"\"{value}\", "
 			f"{str(item.get('is_mother', False)).lower()})"
-			
-			if item.get('repeat', False) else (
-				# Default case for ADD_LAYOUT_TYPE
-				f"ADD_LAYOUT_TYPE({item['id']}, "
-				f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-				f"\"{item['generate_name']}\", "
-				f"\"{item['text']['en']}\", "
-				f"\"{item['text']['fa']}\", "
-				f"{str(item.get('is_mother', False)).lower()})"
-			)
 		)
-	)
+	
+	if 'generate_name' not in item:
+		if type(item['text']) is str:
+			return "ADD_LAYOUT_TYPE_HIDE" + command(item['text'])
+		else:
+			result = ""
+			for (index, value) in item['text'][SELECTED_LANGUAGE]:
+				if index == 0:
+					result += "ADD_LAYOUT_TYPE_HIDE" + command(value)
+				else:
+					result += "ADD_LAYOUT_TYPE_REPEAT" + command(value)
+			return result
+	else:
+		if type(item['text']) is str:
+			return "ADD_LAYOUT_TYPE" + command(item['text'])
+		else:
+			result = ""
+			for (index, value) in item['text'][SELECTED_LANGUAGE]:
+				if index == 0:
+					result += "ADD_LAYOUT_TYPE" + command(value)
+				else:
+					result += "ADD_LAYOUT_TYPE_REPEAT" + command(value)
+			return result
+
 FILES = [
-    {
-        "input": "type.yaml",
-        "output": "ast_type.h",
-        "printify": lambda item: (
-            f"AST_TYPE({item['id']}, "
-            f"\"{item['id'].replace('AST_TYPE_', '')}\", "
-            f"\"{item['id'].replace('AST_TYPE_', '').lower()}\""
-            f")"
-        ),
-    },
-    {
-        "input": "layout/type.yaml",
-        "output": "ast_layout_type.h",
-        "printify": lambda item: (
-            # if item['text'][SELECTED_LANGUAGE] is string so use it, otherwise foreach on it and use REPEAT for second and the next one (not first)
-            # If 'generate_name' is missing, use ADD_LAYOUT_TYPE_HIDE
-            f"ADD_LAYOUT_TYPE_HIDE({item['id']}, "
-            f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-            f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '').lower()}\", "
-            f"\"{item['text'][SELECTED_LANGUAGE]}\", "
-            f"{str(item.get('is_mother', False)).lower()})"
-            
-            if 'generate_name' not in item else (
-                # Use ADD_LAYOUT_TYPE_REPEAT if 'repeat' is true
-                f"ADD_LAYOUT_TYPE_REPEAT({item['id']}, "
-                f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-                f"\"{item['generate_name']}\", "
-                f"\"{item['text']['en']}\", "
-                f"\"{item['text']['fa']}\", "
-                f"{str(item.get('is_mother', False)).lower()})"
-                
-                if item.get('repeat', False) else (
-                    # Default case for ADD_LAYOUT_TYPE
-                    f"ADD_LAYOUT_TYPE({item['id']}, "
-                    f"\"{item['id'].replace('AST_LAYOUT_TYPE_', '')}\", "
-                    f"\"{item['generate_name']}\", "
-                    f"\"{item['text']['en']}\", "
-                    f"\"{item['text']['fa']}\", "
-                    f"{str(item.get('is_mother', False)).lower()})"
-                )
-            )
-        ),
-    },
+	{
+		"input": "type.yaml",
+		"output": "ast_type.h",
+		"printify": printify_type,
+	},
+	{
+		"input": "layout/type.yaml",
+		"output": "ast_layout_type.h",
+		"printify": printify_layout_type,
+	},
 ]
 
 

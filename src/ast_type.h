45aff03a88c20c1731ea6dba437ce09352a893b1
Max Base <MaxBaseCode@gmail.com>
2024-11-18 17:56:18 +0330

    add blockyaml sync

--- src/ast_type.h
+++ src/ast_type.h
@@ -1,21 +1,21 @@
 // ----------- BEGIN AUTO GENERATED ----------- //
-ADD_TYPE(AST_TYPE_BLOCK, "AST_TYPE_BLOCK", "ast_type_block")
+ADD_TYPE(AST_TYPE_BLOCK, "BLOCK", "block")
 
-ADD_TYPE(AST_TYPE_IF, "AST_TYPE_IF", "ast_type_if")
+ADD_TYPE(AST_TYPE_IF, "IF", "if")
 
-ADD_TYPE(AST_TYPE_ELSE_IF, "AST_TYPE_ELSE_IF", "ast_type_else_if")
+ADD_TYPE(AST_TYPE_ELSE_IF, "ELSE_IF", "else_if")
 
-ADD_TYPE(AST_TYPE_RETURN, "AST_TYPE_RETURN", "ast_type_return")
+ADD_TYPE(AST_TYPE_RETURN, "RETURN", "return")
 
-ADD_TYPE(AST_TYPE_IMPORT, "AST_TYPE_IMPORT", "ast_type_import")
+ADD_TYPE(AST_TYPE_IMPORT, "IMPORT", "import")
 
-ADD_TYPE(AST_TYPE_PRINT, "AST_TYPE_PRINT", "ast_type_print")
+ADD_TYPE(AST_TYPE_PRINT, "PRINT", "print")
 
-ADD_TYPE(AST_TYPE_FUNCTION, "AST_TYPE_FUNCTION", "ast_type_function")
+ADD_TYPE(AST_TYPE_FUNCTION, "FUNCTION", "function")
 
-ADD_TYPE(AST_TYPE_LAYOUT, "AST_TYPE_LAYOUT", "ast_type_layout")
+ADD_TYPE(AST_TYPE_LAYOUT, "LAYOUT", "layout")
 
-ADD_TYPE(AST_TYPE_ERROR, "AST_TYPE_ERROR", "ast_type_error")
+ADD_TYPE(AST_TYPE_ERROR, "ERROR", "error")
 
 // ----------- END AUTO GENERATED ----------- //
 

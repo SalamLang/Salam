eee6ccd11f0647b77e64994921bb23b7db284970
Max Base <MaxBaseCode@gmail.com>
2024-11-18 11:56:04 +0330

    Clean html and testing webasm

--- build-webassembly.sh
+++ build-webassembly.sh
@@ -38,6 +38,7 @@ emcc "${sources[@]}" -o ${OUTPUT_BASE}.html \
 	-s ALLOW_MEMORY_GROWTH=1 \
 	-s EXIT_RUNTIME=1 \
 	-s VERBOSE=1 \
+	-s "EXPORTED_RUNTIME_METHODS=['callMain','cwrap']" \
 	# -s NO_EXIT_RUNTIME=1 \
 	# -s "EXPORTED_FUNCTIONS=['_main']"
 	# -s ASSERTIONS=0 \

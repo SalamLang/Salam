aa2b3cb9d2aad06ea5abfe1586d41ef075c984b2
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-11-18 17:38:26 +0330

    Update yaml

--- src/ast_type.h
+++ src/ast_type.h
@@ -1,21 +1,30 @@
 // ----------- BEGIN AUTO GENERATED ----------- //
 ADD_TYPE(AST_TYPE_BLOCK, "AST_TYPE_BLOCK", "ast_type_block")
 
+
 ADD_TYPE(AST_TYPE_IF, "AST_TYPE_IF", "ast_type_if")
+
 
 ADD_TYPE(AST_TYPE_ELSE_IF, "AST_TYPE_ELSE_IF", "ast_type_else_if")
 
+
 ADD_TYPE(AST_TYPE_RETURN, "AST_TYPE_RETURN", "ast_type_return")
+
 
 ADD_TYPE(AST_TYPE_IMPORT, "AST_TYPE_IMPORT", "ast_type_import")
 
+
 ADD_TYPE(AST_TYPE_PRINT, "AST_TYPE_PRINT", "ast_type_print")
+
 
 ADD_TYPE(AST_TYPE_FUNCTION, "AST_TYPE_FUNCTION", "ast_type_function")
 
+
 ADD_TYPE(AST_TYPE_LAYOUT, "AST_TYPE_LAYOUT", "ast_type_layout")
 
+
 ADD_TYPE(AST_TYPE_ERROR, "AST_TYPE_ERROR", "ast_type_error")
+
 
 // ----------- END AUTO GENERATED ----------- //
 

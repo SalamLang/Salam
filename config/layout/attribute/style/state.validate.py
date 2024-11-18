8702cd35bb2f6b4b15f90538bdf930e25796ca9e
John Bampton <jbampton@gmail.com>
2024-11-18 11:00:10 +1000

    Run `pre-commit run --all-files`
    - chmod +x build-linux.sh
    - chmod +x build-webassembly.sh
    
    Run clang-format and black

--- config/layout/attribute/style/state.validate.py
+++ config/layout/attribute/style/state.validate.py
@@ -9,24 +9,24 @@ docs = yaml.safe_load_all(file)
 
 
 def error(msg):
-	print("Error: " + msg)
-	exit(1)
+    print("Error: " + msg)
+    exit(1)
 
 
 for doc in docs:
-	for item in doc["items"]:
-		if "id" not in item:
-			print(item)
-			error("id is required and missed in an item")
-		
-		if "generate_name" not in item:
-			print(item)
-			error("generate_name is required")
+    for item in doc["items"]:
+        if "id" not in item:
+            print(item)
+            error("id is required and missed in an item")
 
-		if "text" in item:
-			for lang in LANGUAGES:
-				if lang not in item["text"]:
-					print(item)
-					error("text is required for " + lang + " language")
+        if "generate_name" not in item:
+            print(item)
+            error("generate_name is required")
+
+        if "text" in item:
+            for lang in LANGUAGES:
+                if lang not in item["text"]:
+                    print(item)
+                    error("text is required for " + lang + " language")
 
 print(FILE + ": Validation is successful")

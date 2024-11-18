4030deffda8ebe03c7b9d66f6c61cba347bc1c00
Max Base <MaxBaseCode@gmail.com>
2024-11-18 13:30:17 +0330

    update bash

--- build-webassembly.sh
+++ build-webassembly.sh
@@ -41,9 +41,7 @@ emcc "${sources[@]}" -o ${OUTPUT_BASE}.html \
 	-s "VERBOSE=1" \
 	-s "EXPORTED_RUNTIME_METHODS=['callMain']" \
 	-s "ASSERTIONS=2" \
-	# -s "NO_EXIT_RUNTIME=1" \
-	# -s "EXPORTED_FUNCTIONS=['_main']"
-	# -s "ASSERTIONS=0" \
+	-s "EXPORTED_FUNCTIONS=['_main']"
 
 if [ $? -eq 0 ]; then
 	echo "Compilation successful. Output files:"

d86e418c18ab68b66f170ebc2d64c25711d34bcd
Max Base <MaxBaseCode@gmail.com>
2024-11-19 13:30:47 +0330

    Add official comments in files

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,26 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Salam Programming Language
+ * ---------------------------------------------------------------------------
+ * 
+ * Welcome to the Salam Programming Language! Salam is a modern, efficient, 
+ * and developer-friendly language created to inspire and empower programmers 
+ * of all ages. With its unique features and Persian roots, Salam aims to make 
+ * programming more accessible and enjoyable for everyone.
+ * 
+ * This file is part of the core implementation of Salam, including its runtime 
+ * and compiler components. By contributing to or using this codebase, you are 
+ * part of a growing community dedicated to innovation and inclusivity in 
+ * programming.
+ * 
+ * Explore Salam further:
+ * - Website: https://www.salamlang.ir/
+ * - GitHub: https://github.com/SalamLang/Salam
+ * 
+ * Thank you for being part of this journey!
+ * ---------------------------------------------------------------------------
+ */
+
 #include "parser.h"
 
 /**

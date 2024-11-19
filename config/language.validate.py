378e053ee83ace7f7f34690dd0611034bf30f7c6
Max Base <MaxBaseCode@gmail.com>
2024-11-19 09:41:42 +0330

    Add if to check it's main script

--- config/language.validate.py
+++ config/language.validate.py
@@ -12,18 +12,19 @@ def error(msg):
     exit(1)
 
 
-for doc in docs:
-    for item in doc["items"]:
-        if "id" not in item:
-            print(item)
-            error("id is required and missed in an item")
+if __name__ == "__main__":
+	for doc in docs:
+		for item in doc["items"]:
+			if "id" not in item:
+				print(item)
+				error("id is required and missed in an item")
 
-        if "name" not in item:
-            print(item)
-            error("name is required and missed in an item")
+			if "name" not in item:
+				print(item)
+				error("name is required and missed in an item")
 
-        if "local_name" not in item:
-            print(item)
-            error("local_name is required and missed in an item")
+			if "local_name" not in item:
+				print(item)
+				error("local_name is required and missed in an item")
 
-print(FILE + ": Validation is successful")
+	print(FILE + ": Validation is successful")
--- config/layout/attribute/style/global_value.validate.py
+++ config/layout/attribute/style/global_value.validate.py
@@ -13,16 +13,17 @@ def error(msg):
     exit(1)
 
 
-for doc in docs:
-    for item in doc["items"]:
-        if "id" not in item:
-            print(item)
-            error("id is required and missed in an item")
+if __name__ == "__main__":
+	for doc in docs:
+		for item in doc["items"]:
+			if "id" not in item:
+				print(item)
+				error("id is required and missed in an item")
 
-        if "text" in item:
-            for lang in LANGUAGES:
-                if lang not in item["text"]:
-                    print(item)
-                    error("text is required for " + lang + " language")
+			if "text" in item:
+				for lang in LANGUAGES:
+					if lang not in item["text"]:
+						print(item)
+						error("text is required for " + lang + " language")
 
-print(FILE + ": Validation is successful")
+	print(FILE + ": Validation is successful")

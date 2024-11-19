9d52512ac5ce35878d65fd5f86008525f2a13d8a
Max Base <MaxBaseCode@gmail.com>
2024-11-19 09:41:42 +0330

    Add if to check it's main script

--- config/layout/type.validate.py
+++ config/layout/type.validate.py
@@ -12,21 +12,22 @@ def error(msg):
     exit(1)
 
 
-for doc in docs:
-    for item in doc["items"]:
-        if "id" not in item:
-            print(item)
-            error("id is required and missed in an item")
+if __name__ == "__main__":
+	for doc in docs:
+		for item in doc["items"]:
+			if "id" not in item:
+				print(item)
+				error("id is required and missed in an item")
 
-        if "is_mother" in item:
-            if item["is_mother"] != True and item["is_mother"] != False:
-                print(item)
-                error("is_mother is invalid in an item")
+			if "is_mother" in item:
+				if item["is_mother"] != True and item["is_mother"] != False:
+					print(item)
+					error("is_mother is invalid in an item")
 
-        if "text" in item:
-            for lang in LANGUAGES:
-                if lang not in item["text"]:
-                    print(item)
-                    error("text is required for " + lang + " language")
+			if "text" in item:
+				for lang in LANGUAGES:
+					if lang not in item["text"]:
+						print(item)
+						error("text is required for " + lang + " language")
 
-print(FILE + ": Validation is successful")
+	print(FILE + ": Validation is successful")

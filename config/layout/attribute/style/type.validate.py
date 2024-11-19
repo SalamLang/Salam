716d2e413645dae156853bac90be096debc55e32
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2024-11-19 16:59:59 +0330

    Run pre-commit, fix problem of values: in layour/attr/style/type yaml file

--- config/layout/attribute/style/type.validate.py
+++ config/layout/attribute/style/type.validate.py
@@ -13,35 +13,35 @@ def error(msg):
 
 
 if __name__ == "__main__":
-	for doc in docs:
-		for item in doc["items"]:
-			if "id" not in item:
-				print(item)
-				error("id is required and missed in an item")
+    for doc in docs:
+        for item in doc["items"]:
+            if "id" not in item:
+                print(item)
+                error("id is required and missed in an item")
 
-			if "type" not in item:
-				print(item)
-				error("type is required")
+            if "type" not in item:
+                print(item)
+                error("type is required")
 
-			if "generate_name" in item:
-				if "reserved_values" not in item:
-					print(item)
-					error("reserved_values is required")
-				if "text" not in item:
-					print(item)
-					error("text is required")
-			else:
-				if "reserved_values" in item:
-					print(item)
-					error("Why reserved_values exists for a non-generate_name item?")
-				if "text" in item:
-					print(item)
-					error("Why text exists for a non-generate_name item?")
-	
-			if "text" in item:
-				for lang in LANGUAGES:
-					if lang not in item["text"]:
-						print(item)
-						error("text is required for " + lang + " language")
+            if "generate_name" in item:
+                if "reserved_values" not in item:
+                    print(item)
+                    error("reserved_values is required")
+                if "text" not in item:
+                    print(item)
+                    error("text is required")
+            else:
+                if "reserved_values" in item:
+                    print(item)
+                    error("Why reserved_values exists for a non-generate_name item?")
+                if "text" in item:
+                    print(item)
+                    error("Why text exists for a non-generate_name item?")
 
-	print(FILE + ": Validation is successful")
+            if "text" in item:
+                for lang in LANGUAGES:
+                    if lang not in item["text"]:
+                        print(item)
+                        error("text is required for " + lang + " language")
+
+    print(FILE + ": Validation is successful")

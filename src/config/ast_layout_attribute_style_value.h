bd6eba3369cb4bb73f492d2c5b64494dfef4a12d
Max Base <MaxBaseCode@gmail.com>
2024-11-26 22:35:51 +0330

    Fix bug - add valid_layout_attributes var

--- src/config/ast_layout_attribute_style_value.h
+++ src/config/ast_layout_attribute_style_value.h
@@ -1467,3 +1467,16 @@ const ast_layout_attribute_style_pair_t ast_layout_allowed_style_list_zoom[] = {
 
 // ----------- END AUTO GENERATED ----------- //
 
+/**
+ *
+ * @var valid_layout_attributes
+ * @brief Valid layout attributes
+ * @type {ast_layout_attribute_type_t[]}
+ */
+ast_layout_attribute_type_t valid_layout_attributes[] = {
+    AST_LAYOUT_ATTRIBUTE_TYPE_TITLE,    AST_LAYOUT_ATTRIBUTE_TYPE_DESCRIPTION,
+    AST_LAYOUT_ATTRIBUTE_TYPE_AUTHOR,   AST_LAYOUT_ATTRIBUTE_TYPE_KEYWORDS,
+    AST_LAYOUT_ATTRIBUTE_TYPE_ICON,     AST_LAYOUT_ATTRIBUTE_TYPE_CHARSET,
+    AST_LAYOUT_ATTRIBUTE_TYPE_DIR,      AST_LAYOUT_ATTRIBUTE_TYPE_LANG,
+    AST_LAYOUT_ATTRIBUTE_TYPE_VIEWPORT, AST_LAYOUT_ATTRIBUTE_TYPE_REFRESH,
+};

97d4afb13224ab6cf95aa9a7ef97cca0ade38545
Max Base <MaxBaseCode@gmail.com>
2024-11-26 13:13:34 +0330

    Run pre-commit and change input.salam

--- src/lexer.c
+++ src/lexer.c
@@ -1171,31 +1171,34 @@ void lexer_lex(lexer_t *lexer) {
                                 LEXER_NEXT_COLUMN;
                             }
                         } else if (LEXER_CURRENT == '*') {
-							LEXER_NEXT;
-							LEXER_NEXT_COLUMN;
+                            LEXER_NEXT;
+                            LEXER_NEXT_COLUMN;
 
-							while (1) {
-								if (LEXER_CURRENT == '\0') {
-									error_lexer(2, "Unterminated multiline comment at line %zu, column %zu",
-												lexer->line, lexer->column);
-									break;
-								}
+                            while (1) {
+                                if (LEXER_CURRENT == '\0') {
+                                    error_lexer(
+                                        2,
+                                        "Unterminated multiline comment at "
+                                        "line %zu, column %zu",
+                                        lexer->line, lexer->column);
+                                    break;
+                                }
 
-								if (LEXER_CURRENT == '*') {
-									LEXER_NEXT;
-									LEXER_NEXT_COLUMN;
+                                if (LEXER_CURRENT == '*') {
+                                    LEXER_NEXT;
+                                    LEXER_NEXT_COLUMN;
 
-									if (LEXER_CURRENT == '/') {
-										LEXER_NEXT;
-										LEXER_NEXT_COLUMN;
-										break;
-									}
-								} else {
-									LEXER_NEXT;
-									LEXER_NEXT_COLUMN;
-								}
-							}
-						} else {
+                                    if (LEXER_CURRENT == '/') {
+                                        LEXER_NEXT;
+                                        LEXER_NEXT_COLUMN;
+                                        break;
+                                    }
+                                } else {
+                                    LEXER_NEXT;
+                                    LEXER_NEXT_COLUMN;
+                                }
+                            }
+                        } else {
                             token_type_t type = token_char_type(c);
                             token_t *token = token_create(
                                 type, (location_t){lexer->index, 1, lexer->line,

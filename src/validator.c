2c5c271d46b88ec0e56d5493215a6ee53aa38a04
Max Base <MaxBaseCode@gmail.com>
2024-12-07 13:45:21 +0330

    delete some code from validator

--- src/validator.c
+++ src/validator.c
@@ -164,7 +164,7 @@ bool token_belongs_to_ast_layout_node(
     ast_layout_attribute_type_t attribute_key_type,
     ast_layout_attribute_t *attribute) {
     DEBUG_ME;
-    // value attribute for input tag
+
     if ((attribute->parent_node_type == AST_LAYOUT_TYPE_INPUT) &&
         (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT)) {
         attribute->final_key = string_strdup("value");
@@ -172,25 +172,25 @@ bool token_belongs_to_ast_layout_node(
     } else if (is_layout_node_a_single_tag(attribute->parent_node_type) &&
                attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT) {
         return false;
-    } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_LABEL &&
-               attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_FOR) {
-        return true;
-    } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT) {
-        return true;
-    } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_DIV &&
-               attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT) {
-        return true;
-    } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_INCLUDE &&
-               (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_SRC ||
-                attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_REPEAT)) {
-        attribute->ignoreMe = true;
-        return true;
+    // } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_LABEL &&
+    //            attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_FOR) {
+    //     return true;
+    // } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT) {
+    //     return true;
+    // } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_DIV &&
+    //            attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_CONTENT) {
+    //     return true;
+    // } else if (attribute->parent_node_type == AST_LAYOUT_TYPE_INCLUDE &&
+    //            (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_SRC ||
+    //             attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_REPEAT)) {
+    //     attribute->ignoreMe = true;
+    //     return true;
     } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_REPEAT) {
         // TODO: add validation to only accepts positive integer
         attribute->ignoreMe = true;
         return true;
-    } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_GROUP) {
-        return true;
+    // } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_GROUP) {
+    //     return true;
     } else if (is_style_attribute(attribute_key_type)) {
         attribute->isStyle = true;
 
@@ -199,357 +199,7 @@ bool token_belongs_to_ast_layout_node(
         return true;
     }
 
-    // LAYOUT
-    if (attribute->parent_node_type == AST_LAYOUT_TYPE_NONE) {
-        if (is_attribute_type_in_array(attribute_key_type,
-                                       valid_layout_attributes,
-                                       valid_layout_attributes_length)) {
-            return true;
-        }
-    }
-    // BR, HR
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_BR ||
-             attribute->parent_node_type == AST_LAYOUT_TYPE_HR) {
-        // TODO: Maybe later?
-        // return true;
-        return false;
-    }
-    // RESPONSIVE
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_MEDIA) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MAX_HEIGHT,
-            AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MIN_HEIGHT,
-            AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MAX_WIDTH,
-            AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MIN_WIDTH,
-        };
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            if (attribute_key_type ==
-                    AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MAX_HEIGHT ||
-                attribute_key_type ==
-                    AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MIN_HEIGHT ||
-                attribute_key_type ==
-                    AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MAX_WIDTH ||
-                attribute_key_type ==
-                    AST_LAYOUT_ATTRIBUTE_TYPE_RESPONSIVE_MIN_WIDTH) {
-                if (validate_style_value_size(NULL, NULL, attribute, NULL,
-                                              NULL) == false) {
-                    error_validator(
-                        2,
-                        "Invalid value for condition attribute '%s' in '%s' "
-                        "element at line %zu column %zu!",
-                        attribute->key,
-                        ast_layout_node_type_to_enduser_name(
-                            attribute->parent_node_type),
-                        attribute->value_location.start_line,
-                        attribute->value_location.start_column);
-
-                    return false;
-                }
-
-                return true;
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-    // FONT
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_FONT) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_NAME,
-            AST_LAYOUT_ATTRIBUTE_TYPE_SRC,
-            AST_LAYOUT_ATTRIBUTE_TYPE_FONT_STYLE,
-            AST_LAYOUT_ATTRIBUTE_TYPE_FONT_WEIGHT,
-            AST_LAYOUT_ATTRIBUTE_TYPE_FONT_UNICODE_RANGE,
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            attribute->final_key = string_strdup(
-                generator_code_layout_attribute_name(attribute_key_type));
-
-            // name
-            if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_NAME) {
-                ast_value_t *value = attribute->values->data[0];
-
-                if (value->type->kind == AST_TYPE_KIND_STRING &&
-                    strlen(value->data.string_value) == 0) {
-                    error_validator(2,
-                                    "Invalid value for attribute '%s' in '%s' "
-                                    "element at line %zu column %zu!",
-                                    attribute->key,
-                                    ast_layout_node_type_to_enduser_name(
-                                        attribute->parent_node_type),
-                                    attribute->value_location.start_line,
-                                    attribute->value_location.start_column);
-
-                    return false;
-                }
-
-                if (attribute->final_key != NULL) {
-                    memory_destroy(attribute->final_key);
-                }
-
-                attribute->final_key = string_strdup("font-family");
-
-                return true;
-            }
-            // src
-            else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_SRC) {
-                string_t *buffer = string_create(10);
-
-                for (size_t i = 0; i < attribute->values->length; i++) {
-                    ast_value_t *value = attribute->values->data[i];
-
-                    if (value->type->kind == AST_TYPE_KIND_STRING) {
-                        char *out_extension = NULL;
-
-                        if (has_font_extension(value->data.string_value,
-                                               &out_extension) &&
-                            out_extension != NULL) {
-                            string_append_str(buffer, "url('");
-                            string_append_str(buffer, value->data.string_value);
-                            string_append_char(buffer, '\'');
-                            string_append_char(buffer, ')');
-
-                            string_append_str(buffer, " format('");
-                            string_append_str(buffer, out_extension);
-                            string_append_char(buffer, '\'');
-                            string_append_char(buffer, ')');
-                        } else {
-                            string_append_str(buffer, "local('");
-                            string_append_str(buffer, value->data.string_value);
-                            string_append_str(buffer, "')");
-                        }
-
-                        if (out_extension != NULL) {
-                            memory_destroy(out_extension);
-                        }
-
-                        if (i < attribute->values->length - 1) {
-                            string_append_char(buffer, ',');
-                            string_append_char(buffer, ' ');
-                        }
-                    } else {
-                        error_validator(2,
-                                        "Invalid value for attribute '%s' in "
-                                        "'%s' element at line %zu column %zu!",
-                                        attribute->key,
-                                        ast_layout_node_type_to_enduser_name(
-                                            attribute->parent_node_type),
-                                        attribute->value_location.start_line,
-                                        attribute->value_location.start_column);
-
-                        return false;
-                    }
-                }
-
-                if (attribute->final_value != NULL) {
-                    memory_destroy(attribute->final_value);
-                }
-
-                attribute->final_value = string_strdup(buffer->data);
-
-                string_destroy(buffer);
-
-                return true;
-            } else if (attribute_key_type ==
-                       AST_LAYOUT_ATTRIBUTE_TYPE_FONT_STYLE) {
-                if (validate_style_value_string(
-                        NULL, NULL, attribute,
-                        ast_layout_allowed_style_list_font_style,
-                        NULL) == false) {
-                    error_validator(2,
-                                    "Invalid value for attribute '%s' in '%s' "
-                                    "element at line %zu column %zu!",
-                                    attribute->key,
-                                    ast_layout_node_type_to_enduser_name(
-                                        attribute->parent_node_type),
-                                    attribute->value_location.start_line,
-                                    attribute->value_location.start_column);
-
-                    return false;
-                }
-
-                return true;
-            } else if (attribute_key_type ==
-                       AST_LAYOUT_ATTRIBUTE_TYPE_FONT_WEIGHT) {
-                if (validate_style_value_integer(
-                        NULL, NULL, attribute,
-                        ast_layout_allowed_style_list_font_weight,
-                        NULL) == false) {
-                    error_validator(2,
-                                    "Invalid value for attribute '%s' in '%s' "
-                                    "element at line %zu column %zu!",
-                                    attribute->key,
-                                    ast_layout_node_type_to_enduser_name(
-                                        attribute->parent_node_type),
-                                    attribute->value_location.start_line,
-                                    attribute->value_location.start_column);
-
-                    return false;
-                }
-
-                return true;
-            } else if (attribute_key_type ==
-                       AST_LAYOUT_ATTRIBUTE_STYLE_TYPE_FONT_DISPLAY) {
-                if (validate_style_value_string(
-                        NULL, NULL, attribute,
-                        ast_layout_allowed_style_list_font_display,
-                        NULL) == false) {
-                    error_validator(2,
-                                    "Invalid value for attribute '%s' in '%s' "
-                                    "element at line %zu column %zu!",
-                                    attribute->key,
-                                    ast_layout_node_type_to_enduser_name(
-                                        attribute->parent_node_type),
-                                    attribute->value_location.start_line,
-                                    attribute->value_location.start_column);
-
-                    return false;
-                }
-
-                return true;
-            } else if (attribute_key_type ==
-                       AST_LAYOUT_ATTRIBUTE_TYPE_FONT_UNICODE_RANGE) {
-                // TODO: We need to do more check layer later!
-                return true;
-            }
-
-            return true;
-        }
-    }
-    // TEXTAREA
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_TEXTAREA) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_PLACEHOLDER,
-            AST_LAYOUT_ATTRIBUTE_TYPE_NAME,
-            AST_LAYOUT_ATTRIBUTE_TYPE_VALUE,  // Not needed
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            return true;
-        }
-    }
-    // INPUT
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_INPUT) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_PLACEHOLDER,
-            AST_LAYOUT_ATTRIBUTE_TYPE_NAME,
-            AST_LAYOUT_ATTRIBUTE_TYPE_VALUE,
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            return true;
-        }
-    }
-    // LINK
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_LINK) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_SRC,
-            AST_LAYOUT_ATTRIBUTE_TYPE_TYPE,
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            // rename type/نوع to target
-            if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_TYPE) {
-                if (attribute->final_key != NULL) {
-                    memory_destroy(attribute->final_key);
-                }
-
-                attribute->final_key = string_strdup("target");
-
-                return true;
-                // rename src/منبع to href
-            } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_SRC) {
-                if (attribute->final_key != NULL) {
-                    memory_destroy(attribute->final_key);
-                }
-
-                attribute->final_key = string_strdup("href");
-
-                return true;
-            }
-        }
-    }
-    // SUBTITLE
-    // else if (attribute->parent_node_type == AST_LAYOUT_TYPE_SUBTITLE) {
-    //     ast_layout_attribute_type_t valid_attributes[] = {
-    //         AST_LAYOUT_ATTRIBUTE_TYPE_TYPE,
-    //     };
-
-    //     size_t valid_attributes_length =
-    //         sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-    //     if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-    //                                    valid_attributes_length)) {
-    //         return true;
-    //     }
-    // }
-    // FORM
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_FORM) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_TYPE,
-            AST_LAYOUT_ATTRIBUTE_TYPE_SRC,
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            if (attribute->final_key != NULL) {
-                memory_destroy(attribute->final_key);
-            }
-
-            // rename type/نوع to method
-            if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_TYPE) {
-                attribute->final_key = string_strdup("method");
-                // rename src/منبع to action
-            } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_SRC) {
-                attribute->final_key = string_strdup("action");
-                // rename kind/روش to target
-            } else if (attribute_key_type == AST_LAYOUT_ATTRIBUTE_TYPE_KIND) {
-                attribute->final_key = string_strdup("target");
-            }
-
-            return true;
-        }
-    }
-    // IMG
-    else if (attribute->parent_node_type == AST_LAYOUT_TYPE_IMG) {
-        ast_layout_attribute_type_t valid_attributes[] = {
-            AST_LAYOUT_ATTRIBUTE_TYPE_SRC,
-        };
-
-        size_t valid_attributes_length =
-            sizeof(valid_attributes) / sizeof(valid_attributes[0]);
-
-        if (is_attribute_type_in_array(attribute_key_type, valid_attributes,
-                                       valid_attributes_length)) {
-            return true;
-        }
-    }
+	
 
     return false;
 }

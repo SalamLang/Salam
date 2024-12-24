613b588b21f1b763364b4043a163dffece769540
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2024-12-24 22:53:24 +0100

    update

--- src/generated-config/ast_layout_attribute_value.h
+++ src/generated-config/ast_layout_attribute_value.h
@@ -3,41 +3,34 @@
 
 // ----------- BEGIN AUTO GENERATED ----------- //
 const ast_layout_attribute_pair_t ast_layout_allowed_dir[] = {
-	{"چپ به راست", "ltr"},
-	{"چپ راست", "ltr"},
-	{"راست به چپ", "rtl"},
-	{"راست چپ", "rtl"},
-	{NULL, NULL},
+    {"چپ به راست", "ltr"}, {"چپ راست", "ltr"}, {"راست به چپ", "rtl"},
+    {"راست چپ", "rtl"},    {NULL, NULL},
 };
 
 const ast_layout_attribute_pair_t ast_layout_allowed_form_type[] = {
-	{"گت", "get"},
-	{"پست", "post"},
-	{NULL, NULL},
+    {"گت", "get"},
+    {"پست", "post"},
+    {NULL, NULL},
 };
 
 const ast_layout_attribute_pair_t ast_layout_allowed_form_kind[] = {
-	{"خالی", "_blank"},
-	{"خود", "_self"},
-	{"والد", "_parent"},
-	{"بالا", "_top"},
-	{NULL, NULL},
+    {"خالی", "_blank"}, {"خود", "_self"}, {"والد", "_parent"},
+    {"بالا", "_top"},   {NULL, NULL},
 };
 
 const ast_layout_attribute_pair_t ast_layout_allowed_charset[] = {
-	{"یوتی‌اف", "UTF-8"},
-	{"یوتی‌اف8", "UTF-8"},
-	{"یوتی‌اف 8", "UTF-8"},
-	{NULL, NULL},
+    {"یوتی‌اف", "UTF-8"},
+    {"یوتی‌اف8", "UTF-8"},
+    {"یوتی‌اف 8", "UTF-8"},
+    {NULL, NULL},
 };
 
 const ast_layout_attribute_pair_t ast_layout_allowed_lang[] = {
-	{"انگلیسی", "fa"},
-	{"فارسی", "fa"},
-	{NULL, NULL},
+    {"انگلیسی", "fa"},
+    {"فارسی", "fa"},
+    {NULL, NULL},
 };
 
 // ----------- END AUTO GENERATED ----------- //
 
 #endif
-

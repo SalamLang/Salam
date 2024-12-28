a5ab9a42bc846b8ce3d716240a43ee255daf5d54
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2024-12-28 10:44:03 +0100

    Fix bug of title tag, name, weight inside tag

--- src/generated-config/ast_layout_attribute_style_value.h
+++ src/generated-config/ast_layout_attribute_style_value.h
@@ -1474,3 +1474,5 @@ const ast_layout_attribute_pair_t ast_layout_allowed_style_list_zoom[] = {
 
 #endif
 
+
+
--- src/generated-config/ast_layout_attribute_style_value_extern.h
+++ src/generated-config/ast_layout_attribute_style_value_extern.h
@@ -361,3 +361,5 @@ extern const ast_layout_attribute_pair_t ast_layout_allowed_style_list_zoom[];
 
 // ----------- END AUTO GENERATED ----------- //
 
+
+

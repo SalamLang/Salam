e4afc7d95b595b998c8b2b4a9ac5f6f37627977b
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2024-12-28 12:07:09 +0100

    move update commands to a seperate functions, add func signs to the header of main.h

--- src/main.h
+++ src/main.h
@@ -21,4 +21,72 @@
 #include "validator.h"
 #include "validator_style.h"
 
+/**
+ *
+ * @function lint
+ * @brief Linting the given content and parameters
+ * @params {bool} isCode - Whether the content is code or file
+ * @params {const char*} path - Path of the file
+ * @params {char*} content - Content of the file
+ * @params {char*} build_file - Build file
+ * @returns {void}
+ *
+ */
+void lint(bool isCode, const char *path, char *content, char *build_file);
+
+/**
+ *
+ * @function run
+ * @brief Running the compiler with the given content and parameters
+ * @params {bool} isCode - Whether the content is code or file
+ * @params {const char*} path - Path of the file
+ * @params {char*} content - Content of the file
+ * @params {char*} build_dir - Build directory
+ * @returns {void}
+ *
+ */
+void run(bool isCode, const char *path, char *content, char *build_dir);
+
+/**
+ *
+ * @function help
+ * @brief Display help message
+ * @params {char*} app - Application name
+ * @returns {void}
+ *
+ */
+void help(char *app);
+
+/**
+ * 
+ * @function update
+ * @brief Update and download new version
+ * @params {void}
+ * @returns {void}
+ * 
+ */
+void update();
+
+/**
+ *
+ * @function doargs
+ * @brief Handle command line arguments
+ * @params {int} argc - Number of arguments
+ * @params {char**} argv - Array of arguments
+ * @returns {void}
+ *
+ */
+void doargs(int argc, char **argv);
+
+/**
+ *
+ * @function main
+ * @brief Main entry point
+ * @params {int} argc - Number of arguments
+ * @params {char**} argv - Array of arguments
+ * @returns {int}
+ *
+ */
+int main(int argc, char **argv);
+
 #endif

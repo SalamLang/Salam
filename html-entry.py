edab3773a5e47c495571c972112b54558886e37a
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-08 09:57:19 +0100

    complete script

--- html-entry.py
+++ html-entry.py
@@ -1,5 +1,22 @@
+import os
+import shlex
 import requests
 from bs4 import BeautifulSoup
+
+repo = "SalamLang/Salam"
+
+def create_github_issue(repo, title, body, labels=None):
+    escaped_body = shlex.quote(body).replace("'", '"').replace("\n", "<br>")
+    
+    if labels:
+        labels_option = ' '.join([f'--label "{label}"' for label in labels])
+    else:
+        labels_option = ""
+
+    command = f'gh issue create --repo {repo} --title "{title}" --body {escaped_body} {labels_option}'
+    
+    print(command)
+    os.system(command)
 
 url = "https://www.w3schools.com/tags/default.asp"
 response = requests.get(url)
@@ -27,6 +44,20 @@ if response.status_code == 200:
                 "brief": brief
             })
 
-    print(result)
+    for item in result:
+        title = f"Implement support for `{item['name']}` tag (HTML)"
+        body = (
+            f"### Description\n\n"
+            f"Tag: `{item['name']}`\n\n"
+            f"**Brief:** {item['brief']}\n\n"
+            f"[Learn more about `{item['name']}`]({item['link']})\n\n"
+            f"Implement this tag in the Salam programming language for generating equivalent HTML."
+        )
+
+        labels = ["html", "enhancement", "good first issue", "yaml"]
+        
+        create_github_issue(repo, title, body, labels)
+        # break
+        print(f"Issue created for {item['name']}")
 else:
     print(f"Failed to fetch the page. Status code: {response.status_code}")

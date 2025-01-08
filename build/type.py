44fc829013e96e0d900c1f068cac72d133f48b1c
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-08 22:57:18 +0100

    complete a

--- build/type.py
+++ build/type.py
@@ -1,6 +1,8 @@
 class Type:
     """Type of an attribute."""
+    Any = "AST_TYPE_ANY"
+    AnyTodo = "AST_TYPE_ANY"
     String = "AST_TYPE_STRING"
+    Url = "AST_TYPE_URL"
     Number = "AST_TYPE_NUMBER"
     Boolean = "AST_TYPE_BOOLEAN"
-    Any = "AST_TYPE_ANY"

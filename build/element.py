85b309a6e3acfe89fa5eb3aaf32c8731844db4ba
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-08 22:24:41 +0100

    start working on build structure

---
+++ build/element.py
@@ -0,0 +1,44 @@
+from text import Text
+from attribute import Attribute
+
+class Element:
+    def __init__(self):
+        self.text = Text()
+        self.attributes = []
+        self.is_mother = False
+
+    def is_mother(self, is_mother):
+        """Set the mother status for the main element."""
+        self.is_mother = is_mother
+
+    def set_generate_name(self, generate_name):
+        """Set the generate name for the main element."""
+        self.generate_name = generate_name
+        self.id = "AST_LAYOUT_TYPE_" + generate_name.upper()
+
+    def set_text(self, language_code, text):
+        """Set text for the main element."""
+        self.text.add_translation(language_code, text)
+
+    def add_attribute(self, attribute_generate_name):
+        """Add an attribute with its own text translations."""
+        attribute = Attribute(attribute_generate_name)
+        self.attributes.append(attribute)
+
+        return attribute
+
+    def to_dict(self):
+        """Convert the element to a dictionary, including all attributes."""
+        for attr in self.attributes:
+            attr.validate()
+
+        return {
+            "id": self.id,
+            "is_mother": self.is_mother,
+            "generate_name": self.generate_name,
+            "text": self.text.to_dict(),
+            "attributes": [
+                attr.to_dict() for attr in self.attributes
+            ],
+        }
+

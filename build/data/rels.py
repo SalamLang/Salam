da9c86a61d066aaa623cb994639e9181775cc329
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 11:18:33 +0100

    Add more tags: area, address, abbr

---
+++ build/data/rels.py
@@ -0,0 +1,18 @@
+from lang import Lang
+from value import Value
+
+rels = [
+    Value().set_generate_name("alternate").add_text(Lang.EN, "alternate").add_text(Lang.FA, "متناوب"),
+    Value().set_generate_name("author").add_text(Lang.EN, "author").add_text(Lang.FA, "نویسنده"),
+    Value().set_generate_name("bookmark").add_text(Lang.EN, "bookmark").add_text(Lang.FA, "بوکمارک"),
+    Value().set_generate_name("external").add_text(Lang.EN, "external").add_text(Lang.FA, "خارجی"),
+    Value().set_generate_name("help").add_text(Lang.EN, "help").add_text(Lang.FA, "کمک"),
+    Value().set_generate_name("license").add_text(Lang.EN, "license").add_text(Lang.FA, "مجوز"),
+    Value().set_generate_name("next").add_text(Lang.EN, "next").add_text(Lang.FA, "بعدی"),
+    Value().set_generate_name("nofollow").add_text(Lang.EN, "nofollow").add_text(Lang.FA, "بدون دنبال"),
+    Value().set_generate_name("noreferrer").add_text(Lang.EN, "noreferrer").add_text(Lang.FA, "بدون ارجاع"),
+    Value().set_generate_name("noopener").add_text(Lang.EN, "noopener").add_text(Lang.FA, "بدون بازکردن"),
+    Value().set_generate_name("prev").add_text(Lang.EN, "prev").add_text(Lang.FA, "قبلی"),
+    Value().set_generate_name("search").add_text(Lang.EN, "search").add_text(Lang.FA, "جستجو"),
+    Value().set_generate_name("tag").add_text(Lang.EN, "tag").add_text(Lang.FA, "برچسب")
+]
---
+++ build/tags/abbr.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import json
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir))
+sys.path.append(os.path.join(parent_dir, "data"))
+
+from lang import Lang
+from type import Type
+from element import Element
+
+if __name__ == "__main__":
+    element = Element()
+    element.set_is_mother(True)
+    element.set_generate_name("abbr")
+    element.set_text(Lang.EN, "abbreviation")
+    element.set_text(Lang.FA, "مخفف")
+
+    element.add_attribute() \
+        .set_generate_name("title") \
+        .set_type(Type.AnyNoEmpty) \
+        .add_text(Lang.EN, "title") \
+        .add_text(Lang.FA, "عنوان") \
+
+    element_dict = element.to_dict()
+
+    j = json.dumps(element_dict, indent=4, ensure_ascii=False)
+    print(j)
+    open(Path(__file__).stem + ".json", "w", encoding="utf-8").write(j)

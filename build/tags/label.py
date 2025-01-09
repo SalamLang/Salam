bc6e7297836bb6004640e72f709195aa4e5f77f9
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 13:37:19 +0100

    create file for crossorigins, add kbd, label, legend, link, input

--- build/tags/label.py
+++ build/tags/label.py
@@ -7,22 +7,28 @@ parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
 sys.path.append(os.path.join(parent_dir, "data"))
 
-from type import Type
 from lang import Lang
+from type import Type
 from element import Element
 
 if __name__ == "__main__":
     element = Element()
     element.set_is_mother(True)
-    element.set_generate_name("li")
-    element.set_text(Lang.EN, "li")
-    element.set_text(Lang.FA, "ایتم لیست")
+    element.set_generate_name("label")
+    element.set_text(Lang.EN, "label")
+    element.set_text(Lang.FA, "لیبل")
 
     element.add_attribute() \
-        .set_generate_name("value") \
-        .set_type(Type.Number) \
-        .add_text(Lang.EN, "value") \
-        .add_text(Lang.FA, "مقدار") \
+        .set_generate_name("for") \
+        .set_type(Type.ID) \
+        .add_text(Lang.EN, "for") \
+        .add_text(Lang.FA, "برای") \
+
+    element.add_attribute() \
+        .set_generate_name("form") \
+        .set_type(Type.ID) \
+        .add_text(Lang.EN, "form") \
+        .add_text(Lang.FA, "فرم") \
 
     element_dict = element.to_dict()
 

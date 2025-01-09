7512fafd150ef505aba82c31feca6dfe894fc1d1
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 14:21:49 +0100

    add noscript, ol, optgroup, option, output, ul

---
+++ build/tags/output.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import json
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir))
+sys.path.append(os.path.join(parent_dir, "data"))
+
+from type import Type
+from lang import Lang
+from element import Element
+
+if __name__ == "__main__":
+    element = Element()
+    element.set_is_mother(True)
+    element.set_generate_name("output")
+    element.set_text(Lang.EN, "output")
+    element.set_text(Lang.FA, "خروجی")
+
+    element.add_attribute() \
+        .set_generate_name("for") \
+        .set_type(Type.IDs) \
+        .add_text(Lang.EN, "for") \
+        .add_text(Lang.FA, "برای") \
+
+    element.add_attribute() \
+        .set_generate_name("form") \
+        .set_type(Type.ID) \
+        .add_text(Lang.EN, "form") \
+        .add_text(Lang.FA, "فرم") \
+
+    element.add_attribute() \
+        .set_generate_name("name") \
+        .set_type(Type.Number) \
+        .add_text(Lang.EN, "name") \
+        .add_text(Lang.FA, "نام") \
+
+    element_dict = element.to_dict()
+
+    j = json.dumps(element_dict, indent=4, ensure_ascii=False)
+    print(j)
+    open(Path(__file__).stem + ".json", "w", encoding="utf-8").write(j)

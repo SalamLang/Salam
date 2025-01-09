4887a4958633efef559dafb246fdb00f21da5f77
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 19:06:41 +0100

    delete extraline before return command in py, update button and targets

--- build/data/targets.py
+++ build/data/targets.py
@@ -5,5 +5,5 @@ targets = [
     Value().set_generate_name("_blank").add_text(Lang.EN, "blank").add_text(Lang.FA, "جدید"),
     Value().set_generate_name("_self").add_text(Lang.EN, "self").add_text(Lang.FA, "خود"),
     Value().set_generate_name("_parent").add_text(Lang.EN, "parent").add_text(Lang.FA, "والدین"),
-    Value().set_generate_name("_top").add_text(Lang.EN, "top").add_text(Lang.FA, "بالا")
+    Value().set_generate_name("_top").add_text(Lang.EN, "top").add_text(Lang.FA, "بالا"),
 ]

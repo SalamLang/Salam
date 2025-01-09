eb38faaaf75ed18c1534017d94df5324688d9d1e
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 11:57:14 +0100

    add button, etc

---
+++ build/tags/button.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import json
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir))
+sys.path.append(os.path.join(parent_dir, "data"))
+
+import targets
+import methods
+import mediatypes
+from type import Type
+from lang import Lang
+from value import Value
+from element import Element
+
+if __name__ == "__main__":
+    element = Element()
+    element.set_is_mother(True)
+    element.set_generate_name("button")
+    element.set_text(Lang.EN, "button")
+    element.set_text(Lang.FA, "دکمه")
+
+    element.add_attribute() \
+        .set_generate_name("autofocus") \
+        .set_type(Type.Boolean) \
+        .add_text(Lang.EN, "focus") \
+        .add_text(Lang.FA, "فوکوس") \
+
+    element.add_attribute() \
+        .set_generate_name("disabled") \
+        .set_type(Type.Boolean) \
+        .add_text(Lang.EN, "disabled") \
+        .add_text(Lang.FA, "غیرفعال") \
+
+    element.add_attribute() \
+        .set_generate_name("form") \
+        .set_type(Type.ID) \
+        .add_text(Lang.EN, "form") \
+        .add_text(Lang.FA, "فرم") \
+
+    element.add_attribute() \
+        .set_generate_name("formaction") \
+        .set_type(Type.Url) \
+        .add_text(Lang.EN, "source") \
+        .add_text(Lang.FA, "منبع") \
+
+    element.add_attribute() \
+        .set_generate_name("formenctype") \
+        .set_type(Type.String) \
+        .add_text(Lang.EN, "formenctype") \
+        .add_text(Lang.FA, "نوع فرم") \
+        .add_reserve_values(mediatypes.mediatypes) \
+
+    element.add_attribute() \
+        .set_generate_name("formmethod") \
+        .set_type(Type.Boolean) \
+        .add_text(Lang.EN, "method") \
+        .add_text(Lang.FA, "شیوه") \
+        .add_reserve_values(methods.methods)
+
+    # TODO
+    element.add_attribute() \
+        .set_generate_name("formnovalidate") \
+        .set_type(Type.Boolean) \
+        .add_text(Lang.EN, "formnovalidate") \
+        .add_text(Lang.FA, "formnovalidate") \
+        .add_reserve_values(targets.targets)
+
+    # TODO
+    element.add_attribute() \
+        .set_generate_name("formtarget") \
+        .set_type(Type.String) \
+        .add_text(Lang.EN, "formtarget") \
+        .add_text(Lang.FA, "formtarget") \
+        .add_reserve_values(targets.targets)
+
+    # TODO
+    element.add_attribute() \
+        .set_generate_name("popovertarget") \
+        .set_type(Type.ID) \
+        .add_text(Lang.EN, "popovertarget") \
+        .add_text(Lang.FA, "popovertarget") \
+
+    # TODO
+    element.add_attribute() \
+        .set_generate_name("popovertargetaction") \
+        .set_type(Type.AnyNoEmpty) \
+        .add_text(Lang.EN, "popovertargetaction") \
+        .add_text(Lang.FA, "popovertargetaction") \
+        .add_reserve_value(Value().set_generate_name("hide").add_text(Lang.EN, "hide").add_text(Lang.FA, "مخفی")) \
+        .add_reserve_value(Value().set_generate_name("show").add_text(Lang.EN, "show").add_text(Lang.FA, "نمایش")) \
+        .add_reserve_value(Value().set_generate_name("toggle").add_text(Lang.EN, "toggle").add_text(Lang.FA, "متغیر بودن")) \
+
+    element.add_attribute() \
+        .set_generate_name("name") \
+        .set_type(Type.AnyNoEmpty) \
+        .add_text(Lang.EN, "name") \
+        .add_text(Lang.FA, "نام") \
+
+    element.add_attribute() \
+        .set_generate_name("type") \
+        .set_type(Type.AnyNoEmpty) \
+        .add_text(Lang.EN, "type") \
+        .add_text(Lang.FA, "نوع") \
+        .add_reserve_value(Value().set_generate_name("button").add_text(Lang.EN, "button").add_text(Lang.FA, "دکمه")) \
+        .add_reserve_value(Value().set_generate_name("reset").add_text(Lang.EN, "reset").add_text(Lang.FA, "ریست")) \
+        .add_reserve_value(Value().set_generate_name("submit").add_text(Lang.EN, "submit").add_text(Lang.FA, "ارسال")) \
+
+
+    element.add_attribute() \
+        .set_generate_name("value") \
+        .set_type(Type.AnyNoEmpty) \
+        .add_text(Lang.EN, "content") \
+        .add_text(Lang.FA, "محتوا") \
+        .set_is_required(True) \
+
+    element_dict = element.to_dict()
+
+    j = json.dumps(element_dict, indent=4, ensure_ascii=False)
+    print(j)
+    open(Path(__file__).stem + ".json", "w", encoding="utf-8").write(j)

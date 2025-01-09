2bfe8ad69a262b03ce104a9b1b182aca1a1543e8
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-09 20:55:59 +0100

    update all tags file to save object string in a temp var to improve performance

--- build/build.py
+++ build/build.py
@@ -5,11 +5,11 @@ def to_string(element_dict):
     return json.dumps(element_dict, indent=4, ensure_ascii=False)
 
 
-def print(element_dict):
-    print(to_string(element_dict))
+def print(element_str):
+    print(element_str)
 
 
-def save(element_dict):
+def save(element_str):
     open(Path(__file__).stem + ".json", "w", encoding="utf-8").write(
-        to_string(element_dict) + "\n"
+        element_str + "\n"
     )
--- build/tags/run_all.sh
+++ build/tags/run_all.sh
@@ -2,7 +2,8 @@
 
 for script in *.py; do
     echo "Running $script..."
-    python3 "$script" || python "$script" || echo "Error running $script"
+    # python3 "$script" || python "$script" || echo "Error running $script"
+    python3 "$script" || echo "Error running $script"
 done
 
 echo "All scripts executed."

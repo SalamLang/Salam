3a9416669dd02fc43d599b5b903c4076f447bb21
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-09 13:37:19 +0100

    create file for crossorigins, add kbd, label, legend, link, input

---
+++ build/data/crossorigins.py
@@ -0,0 +1,7 @@
+from lang import Lang
+from value import Value
+
+crossorigins = [
+    Value().set_generate_name("anonymous").add_text(Lang.EN, "anonymous").add_text(Lang.FA, "ناشناس"),
+    Value().set_generate_name("use-credentials").add_text(Lang.EN, "use credentials").add_text(Lang.FA, "یوز کرید"),
+]

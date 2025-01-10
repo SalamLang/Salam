95dfbe5a7eb598561cfd642f7d447a797e68de49
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-10 19:08:22 +0100

    working on values

--- build/values.py
+++ build/values.py
@@ -13,6 +13,13 @@ class Values:
         Returns:
             A list of Value objects representing all combinations.
         """
+        def generate_ordered_strings(data):
+            result = {}
+            for lang, text_groups in data.items():
+                combinations = itertools.product(*text_groups)
+                result[lang] = [' '.join(' '.join(item) for item in combo) for combo in combinations]
+
+            return result
 
         combinations = itertools.product(*value_groups)
 
@@ -22,16 +29,21 @@ class Values:
 
             for perm in permutations:
                 combined_name = []
+                combined_text = {} # key are language codes, values are array of text
                 for value in perm:
                     combined_name.append(value.generate_name)
+                    for lang, text in value.text.translations.items():
+                        if lang not in combined_text:
+                            combined_text[lang] = []
+                        combined_text[lang].append(text)
 
-                print(combined_name)
-
-                print(combo)
                 combined_value = Value()
                 combined_value.set_generate_name(" ".join(combined_name))
 
-                print(combined_value.generate_name)
+                output = generate_ordered_strings(combined_text)
+                for lang, strings in output.items():
+                    value.add_text(lang, strings)
+
                 result.append(combined_value)
 
         return result

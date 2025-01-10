8cd444d725913b1124afcfa0e7da05e1a662be5d
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-10 19:50:54 +0100

    Complete align-items

--- build/styles/align-items.py
+++ build/styles/align-items.py
@@ -5,12 +5,16 @@ from pathlib import Path
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
 sys.path.append(os.path.join(parent_dir, "data", "styles"))
+sys.path.append(os.path.join(parent_dir, "data", "styles", "values"))
 
-from prebuild import Prebuild
-
+import self_positions
+import overflow_positions
+import baseline_positions
 from lang import Lang
 from type import Type
 from value import Value
+from values import Values
+from prebuild import Prebuild
 from property import Property
 
 if __name__ == "__main__":
@@ -24,6 +28,12 @@ if __name__ == "__main__":
         Value().set_generate_name("stretch").add_text(Lang.EN, "stretch").add_text(Lang.FA, "کشیده"),
         Value().set_generate_name("center").add_text(Lang.EN, "center").add_text(Lang.FA, "وسط"),
     ])
+    property.add_reserve_values(baseline_positions.baseline_positions)
+    property.add_reserve_values(self_positions.self_positions)
+    property.add_reserve_values(Values.And(
+        overflow_positions.overflow_positions,
+        self_positions.self_positions,
+    ))
 
     property_dict = property.to_dict()
 

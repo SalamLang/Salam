48fa2c07e001c75a4b03aa0cb2cf3b333d8f3e82
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-11 13:14:59 +0100

    Fix mistake in single_animation_play_states, add more

---
+++ build/data/styles/values/cubic_bezier_easing_functions.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(parent_dir))
+
+from lang import Lang
+from value import Value
+from prebuild import Prebuild
+
+cubic_bezier_easing_functions = [
+    Value().set_generate_name("ease").add_text(Lang.EN, "ease").add_text(Lang.FA, "آسان"),
+    Value().set_generate_name("ease-in").add_text(Lang.EN, "ease-in").add_text(Lang.FA, "آسان-ورودی"),
+    Value().set_generate_name("ease-out").add_text(Lang.EN, "ease-out").add_text(Lang.FA, "آسان-خروجی"),
+    Value().set_generate_name("ease-in-out").add_text(Lang.EN, "ease-in-out").add_text(Lang.FA, "آسان-ورودی-خروجی"),
+    Value().set_generate_name("cubic-bezier").add_text(Lang.EN, "cubic-bezier").add_text(Lang.F, "بیزیر-مکعب").set_is_function(True),
+]
+
+if __name__ == "__main__":
+    strings = Prebuild.to_string(
+        [value.to_dict() for value in cubic_bezier_easing_functions]
+    )
+    Prebuild.print(strings)
+    Prebuild.save(strings, __file__)
+
+# https://developer.mozilla.org/en-US/docs/Web/CSS/animation-timing-function
+# <cubic-bezier-easing-function> = 
+#   ease              |
+#   ease-in           |
+#   ease-out          |
+#   ease-in-out       |
+#   <cubic-bezier()>  
+
+# <cubic-bezier()> = 
+#   cubic-bezier( [ <number [0,1]> , <number> ]#{2} )  
---
+++ build/data/styles/values/easing_functions.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(parent_dir))
+
+import step_easing_functions
+import linear_easing_functions
+import cubic_bezier_easing_functions
+from prebuild import Prebuild
+
+easing_functions = linear_easing_functions.linear_easing_functions
+easing_functions += cubic_bezier_easing_functions.cubic_bezier_easing_functions
+easing_functions += step_easing_functions.step_easing_functions
+
+if __name__ == "__main__":
+    strings = Prebuild.to_string(
+        [value.to_dict() for value in easing_functions]
+    )
+    Prebuild.print(strings)
+    Prebuild.save(strings, __file__)
+
+# https://developer.mozilla.org/en-US/docs/Web/CSS/animation-timing-function
+# <easing-function> = 
+#   <linear-easing-function>        |
+#   <cubic-bezier-easing-function>  |
+#   <step-easing-function>          

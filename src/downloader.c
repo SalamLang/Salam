c44294b2709b77b1454f93b4ce1572bf2ea3343e
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-13 23:02:09 +0530

    update dowloader.c

--- src/downloader.c
+++ src/downloader.c
@@ -23,6 +23,18 @@
 
 #include "downloader.h"
 
+/**
+ *
+ * @function parse_url
+ * @brief Parse URL
+ * @params {const char*} url
+ * @params {char*} hostname
+ * @params {char*} path
+ * @returns {void}
+ *
+ */
+
+
 void parse_url(const char *url, char *hostname, char *path) {
     DEBUG_ME;
     const char *start;
@@ -50,6 +62,18 @@ void parse_url(const char *url, char *hostname, char *path) {
         path[1] = '\0';
     }
 }
+
+/**
+ *
+ * @function download
+ * @brief Download a file from a HTTP URL
+ * @params {FILE*} fp - File pointer
+ * @params {const char*} port - Port
+ * @params {const char*} hostname - Hostname
+ * @params {const char*} path - Path
+ * @returns {bool} - True if the download was successful, false otherwise
+ *
+ */
 
 bool download(FILE *fp, const char *port, const char *hostname,
               const char *path) {

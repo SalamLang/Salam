61ceeffafdd2d695c936365e37b2f737cf8f1212
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-24 22:29:37 +0330

    start working on parser

--- src/parser/cli/run.ts
+++ src/parser/cli/run.ts
@@ -1,11 +1,17 @@
 import { LanguageMap } from "./language/language";
-import { Lexer } from "./../lex/lexer";
-import { lex } from "./../lex/lex";
+import { Lexer } from "./../../lexer/lex/lexer";
+import { lex } from "./../../lexer/lex/lex";
+import { Parser } from "./../parse/parser";
+import { parse } from "./../parse/parse";
 
 export function processCommandRun(fileName: string | undefined, absoluteDirPath: string | undefined, source: string, selectedLanguage: LanguageMap): number {
     const lexer = new Lexer(source, selectedLanguage, fileName, absoluteDirPath);
     lex(lexer);
-    lexer.print();
+    // lexer.print();
+
+    const parser = new Parser(lexer);
+    parse(parser);
+    parser.print();
 
     return 0;
 };
---
+++ src/parser/parse/layout/layout.ts
@@ -0,0 +1,5 @@
+import { Parser } from './../parser';
+
+export function parserParseLayout(parser: Parser): void {
+    console.log("Parse layout...");
+};

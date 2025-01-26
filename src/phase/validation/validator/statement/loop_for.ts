f950cc00d6b8c0e81317ce1700085367eda4a70a
Max Base <maxbasecode@gmail.com>
2025-01-26 04:29:40 +0330

    Adding more files for staement validators

---
+++ src/phase/validation/validator/statement/loop_for.ts
@@ -0,0 +1,5 @@
+import { Validation } from "./../validation";
+import { AstNode } from "./../../../parser/parse/ast/node";
+
+export function validateLoopFor(validator: Validation, node: AstNode) {
+};

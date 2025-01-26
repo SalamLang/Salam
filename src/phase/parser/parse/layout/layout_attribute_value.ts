58eec2fb9e21f49d162cfd2b82aaec066f282723
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-26 04:23:18 +0330

    fix bug of validate and add clean and readable error messages

--- src/phase/parser/parse/layout/layout_attribute_value.ts
+++ src/phase/parser/parse/layout/layout_attribute_value.ts
@@ -1,7 +1,7 @@
 import { Parser } from '../parser';
 import { AstExpression } from '../ast/expression/expression';
 
-export function parserParseLayoutAttributeValue(parser: Parser, element_name: string, key: string): AstExpression | undefined {
+export function parserParseLayoutAttributeValue(parser: Parser, element_enduser_name: string, key: string): AstExpression | undefined {
     if (parser.currentToken.isValue && parser.currentToken.data !== undefined) {
         const ast: AstExpression = new AstExpression(parser.currentToken.data.getValueString());
         parser.next();
--- src/phase/validation/validator/layout/element_attribute_value.ts
+++ src/phase/validation/validator/layout/element_attribute_value.ts
@@ -1,111 +1,111 @@
 import { Validation } from "../validation";
 import { RuntimeElementAttributeType } from './../../../../runtime/element_attribute_type';
 import { RuntimeElementAttribute } from './../../../../runtime/element_attribute';
-import { RuntimeElementAttributeValue } from './../../../../runtime/element_attribute_value';
 import { AstLayoutAttribute } from './../../../parser/parse/ast/layout/attribute';
 
-export function isUrl(value: string) {
-    try {
-        new URL(value);
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
-
-// Check if attributes values are valid for attribute with reserved values
-export function validateLayoutElementAttributeReservedValue(validator: Validation, node: AstLayoutAttribute, runtimeElementAttribute: RuntimeElementAttribute) {
-    if (runtimeElementAttribute.reservedValues.length > 0) {
-        const found = runtimeElementAttribute.reservedValues.find((value: RuntimeElementAttributeValue) => value.getText(validator.ast.language.id)?.includes(node.enduser_name));
-        if (!found) {
-            validator.pushError("Attribute value '" + node.value + "' is not a valid value for attribute '" + runtimeElementAttribute.getText(validator.ast.language.id) + "'");
-        }
-    }
-}
 export function validateLayoutElementAttributeValue(validator: Validation, node: AstLayoutAttribute, runtimeElementAttribute: RuntimeElementAttribute): string | undefined {
     const attr_name = runtimeElementAttribute.getText(validator.ast.language.id);
-    const error = "Attribute value '" + node.value + "' is not a valid "+ runtimeElementAttribute.type +" for attribute '" + attr_name + "'";
+    const value = node.value.getString();
 
-    if (runtimeElementAttribute.type !== RuntimeElementAttributeType.String && node.value.getString() === "") {
-        return "Attribute value '" + node.value + "' is not a valid "+ runtimeElementAttribute.type +" for attribute '" + attr_name + "' as it should not be empty";
+    const error = "Attribute value '" + value + "' is not a valid "+ runtimeElementAttribute.type +" for attribute '" + attr_name + "' in element '" + node.element_enduser_name + "'";
+
+    if (runtimeElementAttribute.type !== RuntimeElementAttributeType.String && value === "") {
+        return "Attribute value '" + value + "' is not a valid "+ runtimeElementAttribute.type +" for attribute '" + attr_name + "' as it should not be empty in element '" + node.element_enduser_name + "'";
     }
 
     switch (runtimeElementAttribute.type) {
-        case RuntimeElementAttributeType.Url:
-            if (!isUrl(node.value.getString())) {
+        case RuntimeElementAttributeType.Url: {
+            try {
+                new URL(value);
+                return undefined;
+            } catch (e) {
                 return error;
             }
-            break;
+        }
 
-        case RuntimeElementAttributeType.Urls:
+        case RuntimeElementAttributeType.Urls: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.String:
+        case RuntimeElementAttributeType.String: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Strings:
+        case RuntimeElementAttributeType.Strings: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.AnyNoEmpty:
+        case RuntimeElementAttributeType.AnyNoEmpty: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Sizes:
+        case RuntimeElementAttributeType.Sizes: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Size:
+        case RuntimeElementAttributeType.Size: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.PositiveInt:
+        case RuntimeElementAttributeType.PositiveInt: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.IDs:
+        case RuntimeElementAttributeType.IDs: {
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.ID:
+        case RuntimeElementAttributeType.ID: {
             // TODO: make sure ID name defined somewhere
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.CSSMedia:
+        case RuntimeElementAttributeType.CSSMedia: {
             // TODO: make sure it is a valid CSS media
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Coords:
-            const coords = node.value.getString().split(",");
+        case RuntimeElementAttributeType.Coords: {
+            const coords = value.split(",");
             if (coords.length !== 2) {
                 return error;
             } else if (isNaN(Number(coords[0])) || isNaN(Number(coords[1]))) {
                 return error;
             }
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Color:
-            if (node.value.getString().startsWith("#") === true) {
-                if (node.value.getString().length !== 7 || node.value.getString().length !== 4 || !/^#[0-9A-F]{6}$/i.test(node.value.getString())) {
+        case RuntimeElementAttributeType.Color: {
+            if (value.startsWith("#") === true) {
+                if ((value.length !== 7 && value.length !== 4) || !/^#[0-9A-F]{6}$/i.test(value)) {
                     return error;
                 }
             }
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Colors:
+        case RuntimeElementAttributeType.Colors: {
             // TODO: make sure it is array of valid colors
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Number:
-            if (isNaN(Number(node.value.getString()))) {
+        case RuntimeElementAttributeType.Number: {
+            if (isNaN(Number(value))) {
                 return error;
             }
             return undefined;
+        }
 
-        case RuntimeElementAttributeType.Boolean:
-            if (node.value.getString() !== "true" && node.value.getString() !== "false") {
+        case RuntimeElementAttributeType.Boolean: {
+            if (value !== "true" && value !== "false") {
                 return error;
             }
             return undefined;
+        }
 
         case RuntimeElementAttributeType.Date: {
             // Example: 2021-01-01
             const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-            const value = node.value.getString();
             if (!datePattern.test(value) || isNaN(Date.parse(value))) {
                 return error;
             }
@@ -115,7 +115,6 @@ export function validateLayoutElementAttributeValue(validator: Validation, node:
         case RuntimeElementAttributeType.Time: {
             // Example: 12:00
             const timePattern = /^([01]?[0-9]|2[0-3]):([0-5][0-9])$/;
-            const value = node.value.getString();
             if (!timePattern.test(value) || isNaN(Date.parse(`1970-01-01T${value}:00Z`))) {
                 return error;
             }
@@ -129,7 +128,6 @@ export function validateLayoutElementAttributeValue(validator: Validation, node:
         case RuntimeElementAttributeType.DateTime: {
             // Example: 2021-01-01T12:00:00
             const dateTimePattern = /^\d{4}-\d{2}-\d{2}T([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/;
-            const value = node.value.getString();
             if (!dateTimePattern.test(value) || isNaN(Date.parse(value))) {
                 return error;
             }
@@ -137,7 +135,7 @@ export function validateLayoutElementAttributeValue(validator: Validation, node:
         }
       
         case RuntimeElementAttributeType.NumberOr2Numbers: {
-            const parts = node.value.getString().split(" ");
+            const parts = value.split(" ");
             if (parts.length > 2) {
                 return error;
             }

eeae67f607640c5dacc014fb0b61108364c936ca
mammaadDeveloper <developermammaad@gmail.com>
2025-01-27 00:26:51 +0330

    feat(tags, ts): Add TypeScript code tags part 1. abbr , address , area , article .

---
+++ src/runtime/element/abbr.ts
@@ -0,0 +1,20 @@
+import {LanguageID} from '../../common/language/language';
+import {RuntimeElement} from '../element';
+import {RuntimeElementAttribute} from '../element_attribute';
+import {RuntimeElementAttributeType} from '../element_attribute_type';
+
+export class RuntimeElementAbbr extends RuntimeElement {
+  constructor() {
+    super(true, 'abbr');
+    this.addText(LanguageID.LanguageEnglish, 'abbreviation');
+    this.addText(LanguageID.LanguagePersian, 'مخفف');
+    this.addAttribute(
+      new RuntimeElementAttribute(
+        RuntimeElementAttributeType.StringNoEmpty,
+        'title'
+      )
+        .addText(LanguageID.LanguagePersian, 'عنوان')
+        .addText(LanguageID.LanguageEnglish, 'title')
+    );
+  }
+}

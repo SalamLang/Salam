96307d3ec003d77c86aecb5a502d0f4c875c05b6
Max Base <maxbasecode@gmail.com>
2025-01-27 03:16:01 +0330

    clean structure for multi-languages

--- src/common/message/lexer/lexer.ts
+++ src/common/message/lexer/lexer.ts
@@ -1,6 +1,9 @@
-import { MessageKeys, MessageStruct } from './../message';
+import { faMessages } from './fa';
+import { enMessages } from './en';
+import { MultiLanguageMessages } from './../message';
+import { LanguageID } from './../../language/language';
 
-export const lexerMessages: MessageStruct = {
-    [MessageKeys.LEXER_INVALID_UNEXPECTED_CHAR]: "Unexpected character '{0}'",
-    [MessageKeys.LEXER_UNTERMINATED_MULTI_LINE_COMMENT_BLOCK]: "Unterminated multi-line comment block",
+export const lexerMessages: MultiLanguageMessages = {
+    [LanguageID.LanguageEnglish]: enMessages,
+    [LanguageID.LanguagePersian]: faMessages
 };

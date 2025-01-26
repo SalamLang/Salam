4f450d615b787083ec0ed5814951894fdd1ba4f0
mammaadDeveloper <developermammaad@gmail.com>
2025-01-27 02:00:53 +0330

    feat(tags, ts): Adding TypeScript code to tags, part three.

---
+++ src/runtime/element/del.ts
@@ -0,0 +1,10 @@
+import { LanguageID } from "../../common/language/language";
+import { RuntimeElement } from "../element";
+
+export class RuntimeElementDel extends RuntimeElement {
+    constructor() {
+        super(true, 'del');
+        this.addText(LanguageID.LanguageEnglish, 'del');
+        this.addText(LanguageID.LanguagePersian, 'خط خورده');
+    }
+}

f226b992ed8ccd1c3550691276601c3bb0488d50
Max Base <maxbasecode@gmail.com>
2025-01-27 02:38:20 +0330

    Fix bug of reserved values to be accepted and handled correctly, change attribute types

--- src/runtime/element/custom/layout.ts
+++ src/runtime/element/custom/layout.ts
@@ -2,6 +2,7 @@ import { RuntimeElement } from './../../element';
 import { LanguageID } from './../../../common/language/language';
 import { RuntimeElementAttribute } from './../../element_attribute';
 import { RuntimeElementAttributeType } from './../../element_attribute_type';
+import { RuntimeElementAttributeValue } from '../../element_attribute_value';
 
 export class RuntimeElementLayout extends RuntimeElement {
     constructor() {
@@ -9,9 +10,28 @@ export class RuntimeElementLayout extends RuntimeElement {
         this.addText(LanguageID.LanguageEnglish, "layout");
         this.addText(LanguageID.LanguagePersian, "صفحه");
         this.addAttribute(
-            new RuntimeElementAttribute(RuntimeElementAttributeType.StringNoEmpty, "download")
-            .addText(LanguageID.LanguagePersian, "دانلود")
-            .addText(LanguageID.LanguageEnglish, "download")
+            new RuntimeElementAttribute(RuntimeElementAttributeType.AnyNoEmpty, "title")
+            .addText(LanguageID.LanguagePersian, "عنوان")
+            .addText(LanguageID.LanguageEnglish, "title")
+        );
+        this.addAttribute(
+            new RuntimeElementAttribute(RuntimeElementAttributeType.AnyNoEmpty, "author")
+            .addText(LanguageID.LanguagePersian, "نویسنده")
+            .addText(LanguageID.LanguageEnglish, "author")
+        );
+        this.addAttribute(
+            new RuntimeElementAttribute(RuntimeElementAttributeType.AnyNoEmpty, "lang")
+            .addText(LanguageID.LanguagePersian, "زبان")
+            .addText(LanguageID.LanguageEnglish, "language")
+        );
+        this.addAttribute(
+            new RuntimeElementAttribute(RuntimeElementAttributeType.StringNoEmpty, "dir")
+            .addText(LanguageID.LanguagePersian, "جهت")
+            .addText(LanguageID.LanguageEnglish, "direction")
+            .addReservedValues([
+                new RuntimeElementAttributeValue("ltr").addText(LanguageID.LanguagePersian, "چپ به راست").addText(LanguageID.LanguageEnglish, "left to right"),
+                new RuntimeElementAttributeValue("rtl").addText(LanguageID.LanguagePersian, "راست به چپ").addText(LanguageID.LanguageEnglish, "right to left")
+            ])
         );
     }
 };
--- src/runtime/element_attribute_type.ts
+++ src/runtime/element_attribute_type.ts
@@ -1,5 +1,7 @@
 export enum RuntimeElementAttributeType {
-  String = 'STRING',
+  Any = 'Any',
+  AnyNoEmpty = 'AnyNoEmpty',
+  String = 'String',
   StringNoEmpty = 'StringNoEmpty',
   Strings = 'Strings',
 
@@ -34,5 +36,4 @@ export enum RuntimeElementAttributeType {
   DateTime = 'DateTime',
 
   NumberOr2Numbers = 'NumberOr2Numbers', // aspect-ratio
-  AnyNoEmpty = 'AnyNoEmpty',
-}
+};

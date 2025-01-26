54ac7200430f4ccb73a621539eee6d7a49177884
Max Base <maxbasecode@gmail.com>
2025-01-27 00:57:11 +0330

    Complete repeat value and test it on elements

--- src/phase/generator/generation/generate.ts
+++ src/phase/generator/generation/generate.ts
@@ -2,7 +2,9 @@ import { Generator } from './generator';
 import { generateLayout } from './layout/layout';
 
 export function generate(generator: Generator): void {
+    generator.source = '';
+
     if (generator.ast.layout) {
-        generateLayout(generator, generator.ast.layout);
+        generator.source += generateLayout(generator, generator.ast.layout);
     }
 };

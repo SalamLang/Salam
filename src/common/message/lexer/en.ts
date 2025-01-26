3b7e0f1bbf2d1683d46fe497653fb52aab5347a6
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-27 03:16:01 +0330

    clean structure for multi-languages

---
+++ src/common/message/lexer/en.ts
@@ -0,0 +1,6 @@
+import { MessageKeys, MessageStruct } from './../message';
+
+export const enMessages: MessageStruct = {
+    [MessageKeys.LEXER_INVALID_UNEXPECTED_CHAR]: "Unexpected character '{0}'",
+    [MessageKeys.LEXER_UNTERMINATED_MULTI_LINE_COMMENT_BLOCK]: "Unterminated multi-line comment block",
+};
--- src/common/message/message.ts
+++ src/common/message/message.ts
@@ -1,14 +1,20 @@
+import { LanguageID } from './../../common/language/language';
+
 export enum MessageKeys {
-    LEXER_INVALID_UNEXPECTED_CHAR,
-    LEXER_UNTERMINATED_MULTI_LINE_COMMENT_BLOCK,
-};
+    LEXER_INVALID_UNEXPECTED_CHAR = "LEXER_INVALID_UNEXPECTED_CHAR",
+    LEXER_UNTERMINATED_MULTI_LINE_COMMENT_BLOCK = "LEXER_UNTERMINATED_MULTI_LINE_COMMENT_BLOCK",
+}
 
 export type MessageStruct = {
     [K in MessageKeys]: string;
 };
 
+export type MultiLanguageMessages = {
+    [L in LanguageID]: MessageStruct;
+};
+
 export function messageRenderer(message: string, ...args: string[]): string {
     return message.replace(/{(\d+)}/g, (match, number) => {
-        return typeof args[number] !== 'undefined' ? args[number] : match;
+        return typeof args[number] !== "undefined" ? args[number] : match;
     });
-};
+}

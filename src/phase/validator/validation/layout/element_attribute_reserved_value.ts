1c3e1f45cd0a62007bc24f8be67db4c0e0c9ae9d
Max Base <maxbasecode@gmail.com>
2025-01-26 15:03:53 +0330

    create element_attribute_reserved_value file

---
+++ src/phase/validator/validation/layout/element_attribute_reserved_value.ts
@@ -0,0 +1,20 @@
+import { Validator } from "../validator";
+import { RuntimeElementAttribute } from '../../../../runtime/element_attribute';
+import { AstLayoutAttribute } from '../../../parser/parse/ast/layout/attribute';
+import { RuntimeElementAttributeValue } from '../../../../runtime/element_attribute_value';
+
+export function validateLayoutElementAttributeReservedValue(validator: Validator, node: AstLayoutAttribute, runtimeElementAttribute: RuntimeElementAttribute): string | undefined {
+    const error = "Attribute value '" + node.value.getString() + "' is not a valid value for attribute '" + node.enduser_name + "' in element '" + node.element_enduser_name + "'";
+    if (runtimeElementAttribute.reservedValues.length > 0) {
+        const found = runtimeElementAttribute.reservedValues.find((value: RuntimeElementAttributeValue) => value.getText(validator.ast.language.id)?.includes(node.enduser_name));
+        if (!found) {
+            return error;
+        }
+        // Update the generate name of the attribute value
+        node.generate_name = runtimeElementAttribute.generate_name;
+        // Update the generate value of the attribute value
+        node.generate_value = found.generate_name;
+        return undefined;
+    }
+    return error;
+};

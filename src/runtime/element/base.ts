f40014b55971409b0ca791054f43280e9693e941
mammaadDeveloper <developermammaad@gmail.com>
2025-01-27 01:10:16 +0330

    feat(tags, ts): Adding TypeScript code to tags, part two.

---
+++ src/runtime/element/base.ts
@@ -0,0 +1,24 @@
+import {LanguageID} from '../../common/language/language';
+import {RuntimeElement} from '../element';
+import {RuntimeElementAttribute} from '../element_attribute';
+import {RuntimeElementAttributeType} from '../element_attribute_type';
+
+export class RuntimeElementBase extends RuntimeElement {
+  constructor() {
+    super(true, 'base');
+    this.addText(LanguageID.LanguageEnglish, 'base');
+    this.addText(LanguageID.LanguagePersian, 'پایه');
+
+    this.addAttribute(
+      new RuntimeElementAttribute(RuntimeElementAttributeType.Url, 'href')
+        .addText(LanguageID.LanguageEnglish, 'href')
+        .addText(LanguageID.LanguagePersian, 'منبع')
+    );
+
+    this.addAttribute(
+      new RuntimeElementAttribute(RuntimeElementAttributeType.String, 'target')
+        .addText(LanguageID.LanguageEnglish, 'target')
+        .addText(LanguageID.LanguagePersian, 'نوع')
+    );
+  }
+}

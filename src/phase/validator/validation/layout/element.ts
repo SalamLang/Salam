bfccb9c3dd1f2e1ab486b7146124f5c82f13f6a0
Max Base <maxbasecode@gmail.com>
2025-01-26 19:54:37 +0330

    Add validateLayoutElementAttributes and clean it

--- src/phase/validator/validation/layout/element.ts
+++ src/phase/validator/validation/layout/element.ts
@@ -1,7 +1,7 @@
 import { Validator } from "../validator";
 import { validateLayoutBlock } from './block';
 import { RuntimeElement } from '../../../../runtime/element';
-import { validateLayoutElementAttribute } from './element_attribute';
+import { validateLayoutElementAttributes } from './element_attributes';
 import { AstLayoutElement } from "../../../parser/parse/ast/layout/element";
 
 export function validateLayoutElement(validator: Validator, node: AstLayoutElement): void {
@@ -17,24 +17,11 @@ export function validateLayoutElement(validator: Validator, node: AstLayoutEleme
     node.generate_type = runtimeElement.constructor.name;
 
     // Check attributes
-    for (const attribute of node.attributes.items) {
-        validateLayoutElementAttribute(validator, runtimeElement, attribute);
-    }
-
-    validateLayoutBlock(validator, node.block);
+    validateLayoutElementAttributes(validator, node.enduser_name, runtimeElement, node.attributes);
 
     // Check global attributes
-    for (const attribute of node.globalAttributes.items) {
-        validateLayoutElementAttribute(validator, runtimeElement, attribute);
-    }
+    validateLayoutElementAttributes(validator, node.enduser_name, runtimeElement, node.globalAttributes);
 
-    // Check if required attributes are not present
-    for (const runtimeAttribute of runtimeElement.attributes) {
-        if (runtimeAttribute.is_required) {
-            const found = node.attributes.items.find((nodeAttribute) => runtimeAttribute.getText(validator.ast.language.id)?.includes(nodeAttribute.enduser_name));
-            if (!found) {
-                validator.pushError("Attribute '" + runtimeAttribute.getText(validator.ast.language.id) + "' is required for element '" + node.enduser_name + "' but not found");
-            }
-        }
-    }
+    // Check block
+    validateLayoutBlock(validator, node.block);
 };
---
+++ src/phase/validator/validation/layout/element_attributes.ts
@@ -0,0 +1,20 @@
+import { Validator } from "../validator";
+import { RuntimeElement } from '../../../../runtime/element';
+import { validateLayoutElementAttribute } from './element_attribute';
+import { AstLayoutAttributes } from "../../../parser/parse/ast/layout/attributes";
+
+export function validateLayoutElementAttributes(validator: Validator, element_enduser_name: string, runtimeElement: RuntimeElement, attributes: AstLayoutAttributes): void {
+    for (const attribute of attributes.items) {
+        validateLayoutElementAttribute(validator, runtimeElement, attribute);
+    }
+
+    // Check if required attributes are not present
+    for (const runtimeAttribute of runtimeElement.attributes) {
+        if (runtimeAttribute.is_required) {
+            const found = attributes.items.find((nodeAttribute) => runtimeAttribute.getText(validator.ast.language.id)?.includes(nodeAttribute.enduser_name));
+            if (!found) {
+                validator.pushError("Attribute '" + runtimeAttribute.getText(validator.ast.language.id) + "' is required for element '" + element_enduser_name + "' but not found");
+            }
+        }
+    }
+};

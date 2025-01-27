25c7d7d6f943ef6ac2cc570c878edad3f90f340c
Max Base <maxbasecode@gmail.com>
2025-01-27 04:00:02 +0330

    Complete and connect all warnings/errors to message module for validator and generator

---
+++ src/common/message/generator/fa.ts
@@ -0,0 +1,5 @@
+import { GeneratorMessageKeys, GeneratorMessageStruct } from '../message';
+
+export const faMessages: GeneratorMessageStruct = {
+    [GeneratorMessageKeys.GENERATOR_CANNOT_OUTDENT_BELOW_ZERO]: "نمی‌توانید از صفر کمتر برود",
+};

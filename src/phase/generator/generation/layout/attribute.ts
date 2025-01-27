de069a29908098cf46851e04e56975ac57c4733c
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-27 04:30:57 +0330

    Add generateLayoutAttributeOutput and handle url() output type

--- src/phase/generator/generation/layout/attribute.ts
+++ src/phase/generator/generation/layout/attribute.ts
@@ -1,26 +1,24 @@
 import { Generator } from "./../generator";
 import { AstLayoutAttribute } from "./../../../parser/parse/ast/layout/attribute";
+import { AstLayoutAttributeType } from "./../../../parser/parse/ast/layout/attribute_type";
+import { generateLayoutAttributeOutput } from "./attribute_output";
 
 export function generateLayoutAttribute(generator: Generator, attribute: AstLayoutAttribute): string {
     let result: string = "";
+
+    const isStyle: boolean = attribute.kind === AstLayoutAttributeType.Style;
 
     if (attribute.generate_name === undefined) {
         return "";
     }
     
-    result += attribute.generate_name + '=';
+    result += attribute.generate_name + (isStyle ? ':' : '=');
 
     if (attribute.generate_value === undefined) {
         attribute.generate_value = attribute.value.getString();
     }
 
-    if (attribute.generate_value === undefined) {
-        result += `""`;
-    } else if (attribute.generate_value.includes(' ')) {
-        result += `"${attribute.generate_value}"`;
-    } else {
-        result += attribute.generate_value;
-    }
+    result += generateLayoutAttributeOutput(generator, attribute);
 
     return result;
 };

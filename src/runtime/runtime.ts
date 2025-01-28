a912809730b7ae99b3a7050f9eafcf49d82b4e66
Max Base <maxbasecode@gmail.com>
2025-01-28 17:30:10 +0330

    complete

--- src/runtime/runtime.ts
+++ src/runtime/runtime.ts
@@ -38,3 +38,36 @@ import { RuntimeGlobalAttributeRepeat } from './attribute/custom/repeat';
 export const runtimeGlobalAttributes: RuntimeElementAttribute[] = [
     new RuntimeGlobalAttributeRepeat(),
 ];
+
+import { RuntimeElementStyleStateActive } from './style/state/active';
+import { RuntimeElementStyleStateBlank } from './style/state/blank';
+import { RuntimeElementStyleStateChecked } from './style/state/checked';
+import { RuntimeElementStyleStateDisabled } from './style/state/disabled';
+import { RuntimeElementStyleStateEmpty } from './style/state/empty';
+import { RuntimeElementStyleStateEnabled } from './style/state/enabled';
+import { RuntimeElementStyleStateFirstChild } from './style/state/first-child';
+import { RuntimeElementStyleStateFirstOfType } from './style/state/first-of-type';
+import { RuntimeElementStyleStateFocusVisible } from './style/state/focus-visible';
+import { RuntimeElementStyleStateFocusWithin } from './style/state/focus-within';
+import { RuntimeElementStyleStateFocus } from './style/state/focus';
+import { RuntimeElementStyleStateGlobal } from './style/state/global';
+import { RuntimeElementStyleStateHover } from './style/state/hover';
+import { RuntimeElementStyleStateInRange } from './style/state/in-range';
+import { RuntimeElementStyleStateLastChild } from './style/state/last-child';
+import { RuntimeElementStyleStateLastOfType } from './style/state/last-of-type';
+import { RuntimeElementStyleStateLink } from './style/state/link';
+import { RuntimeElementStyleStateNthChild } from './style/state/nth-child';
+import { RuntimeElementStyleStateNthLastChild } from './style/state/nth-last-child';
+import { RuntimeElementStyleStateNthLastOfType } from './style/state/nth-last-of-type';
+import { RuntimeElementStyleStateNthOfType } from './style/state/nth-of-type';
+import { RuntimeElementStyleStateOnlyChild } from './style/state/only-child';
+import { RuntimeElementStyleStateOnlyOfType } from './style/state/only-of-type';
+import { RuntimeElementStyleStateOptional } from './style/state/optional';
+import { RuntimeElementStyleStateRoot } from './style/state/root';
+import { RuntimeElementStyleStateTarget } from './style/state/target';
+import { RuntimeElementStyleStateValid } from './style/state/valid';
+import { RuntimeElementStyleStateVisited } from './style/state/visited';
+
+export const runtimeStyleStates: RuntimeElementStyleState[] = [
+
+];

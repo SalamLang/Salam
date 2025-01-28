5bcdf091ec3b0bdbbf27ac3ddc655501dd587a01
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-28 18:50:07 +0330

    saev

--- src/phase/validator/validation/layout/layout.ts
+++ src/phase/validator/validation/layout/layout.ts
@@ -3,5 +3,6 @@ import { validateLayoutElement } from "./element";
 import { AstLayout } from "./../../../parser/parse/ast/layout/layout";
 
 export function validateLayout(validator: Validator, layout: AstLayout): void {
+    // Validate root element (It is an element, not style state)
     validateLayoutElement(validator, undefined, layout.root);
 };

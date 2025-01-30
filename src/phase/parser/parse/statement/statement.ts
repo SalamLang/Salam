2edc79d35488ecc3d9ff135103583e86e2abb4c5
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-31 01:35:37 +0330

    start working on function, block, statement

---
+++ src/phase/parser/parse/statement/statement.ts
@@ -0,0 +1,6 @@
+import { Parser } from './../parser';
+import { AstBlock } from './../ast/block';
+
+export function parseStatement(parser: Parser): AstBlock | undefined {
+    return undefined;
+};

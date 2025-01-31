aedfab7bdda6bbf82fdc5d9b4b0e2a34b34d0424
Max Base <maxbasecode@gmail.com>
2025-01-31 17:47:11 +0330

    add AstExpressionLiteral

--- src/phase/parser/parse/ast/expression/unary.ts
+++ src/phase/parser/parse/ast/expression/unary.ts
@@ -2,19 +2,19 @@ import { AstExpression } from './expression';
 
 export class AstExpressionUnary extends AstExpression {
     operator: string;
-    operand: AstExpression;
+    right: AstExpression;
 
     constructor(operator: string, operand: AstExpression) {
         super("ExpressionUnary");
         this.operator = operator;
-        this.operand = operand;
+        this.right = operand;
     }
 
     stringify(wantsJson: boolean = true): string | object {
         return {
             type: "ExpressionUnary",
             operator: this.operator,
-            operand: this.operand.stringify(wantsJson),
+            right: this.right.stringify(wantsJson),
         };
     }
 }

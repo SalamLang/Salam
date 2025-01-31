c4fdf06388f145f2ca6210116f73c0f21a62b6f4
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-31 19:41:49 +0330

    Change all any to correct types

--- src/phase/lexer/cli/command/file.ts
+++ src/phase/lexer/cli/command/file.ts
@@ -10,23 +10,27 @@ export function processCommandFile(args: string[], selectedLanguage: LanguageMap
     let source, fileName, absoluteDirPath;
 
     try {
-        if (! filePath) {
+        if (!filePath) {
             console.error(`Error: File path not provided.`);
             return 1;
-        } else if (! fs.existsSync(filePath)) {
+        } else if (!fs.existsSync(filePath)) {
             console.error(`Error: File not found - "${filePath}"`);
             return 1;
         }
-
+    
         fileName = fs.realpathSync(filePath);
         absoluteDirPath = fs.realpathSync(path.dirname(filePath));
-
+    
         source = fs.readFileSync(fileName, 'utf8');
-    } catch (error: any) {
-        if (error.code === 'ENOENT') {
-            console.error(`Error: File not found - "${filePath}"`);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+                console.error(`Error: File not found - "${filePath}"`);
+            } else {
+                console.error(`Error reading file: ${error.message}`);
+            }
         } else {
-            console.error(`Error reading file: ${error.message}`);
+            console.error(`Unknown error occurred.`);
         }
         return 1;
     }

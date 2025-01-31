f6b1a246a8801a5a6ccea0914f8f30c831314821
mammaadDeveloper <developermammaad@gmail.com>
2025-02-01 01:04:32 +0330

    fix(element, runtime): Import all tag classes into the runtime file

--- src/runtime/element/script.ts
+++ src/runtime/element/script.ts
@@ -1,7 +1,7 @@
 import { LanguageID } from "./../../common/language/language";
 import { RuntimeElement } from "./../element";
 
-export class RuntimeElementS extends RuntimeElement {
+export class RuntimeElementScript extends RuntimeElement {
     constructor() {
         super(true, 'script');
         this.addText(LanguageID.LanguageEnglish, 'script');
--- src/runtime/element/style.ts
+++ src/runtime/element/style.ts
@@ -1,10 +1,10 @@
 import { LanguageID } from "./../../common/language/language";
 import { RuntimeElement } from "./../element";
 
-export class RuntimeElementSource extends RuntimeElement {
+export class RuntimeElementStyle extends RuntimeElement {
     constructor() {
-        super(true, 'source');
-        this.addText(LanguageID.LanguageEnglish, 'source');
-        this.addText(LanguageID.LanguagePersian, 'منبع');
+        super(true, 'style');
+        this.addText(LanguageID.LanguageEnglish, 'style');
+        this.addText(LanguageID.LanguagePersian, 'استایل');
     }
 }

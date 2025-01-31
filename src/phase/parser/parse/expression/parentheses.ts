aebd21233c5737e36c3e278767ead2464902c299
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-01-31 15:27:52 +0330

    clean - more small pieces

---
+++ src/phase/parser/parse/expression/parentheses.ts
@@ -0,0 +1,13 @@
+import { Parser } from './../parser';
+import { parseExpression } from './expression';
+import { AstExpression } from './../ast/expression/expression';
+import { TokenOperatorType } from '../../../lexer/tokenizer/type';
+
+export function parseExpressionParentheses(parser: Parser): AstExpression | undefined {
+    if (parser.skip(TokenOperatorType.TOKEN_LEFT_PAREN)) {
+        const expr = parseExpression(parser); // Parse the expression inside parentheses
+        parser.expect(TokenOperatorType.TOKEN_RIGHT_PAREN); // Expect ')'
+        return expr;
+    }
+    return undefined;
+};

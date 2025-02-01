7e20f7ef9c013f07c7252d15943484e17835ebb8
Max Base <maxbasecode@gmail.com>
2025-02-01 11:07:58 +0330

    add generateExpressionOperator

--- src/phase/generator/generation/expression/binary.ts
+++ src/phase/generator/generation/expression/binary.ts
@@ -1,13 +1,11 @@
 import { Generator } from './../generator';
-import { generateExpression } from './expression';
+import { generateExpression, generateExpressionOperator } from './expression';
 import { AstExpressionBinary } from '../../../parser/parse/ast/expression/binary';
+import { TokenOperatorType } from '../../../lexer/tokenizer/type';
 
 export function generateExpressionBinary(generator: Generator, expr: AstExpressionBinary): string {
-    let result: string = "";
-    result += generateExpression(generator, expr.left);
-    result += " ";
-    result += expr.operator;
-    result += " ";
-    result += generateExpression(generator, expr.right);
+    const left: string = generateExpression(generator, expr.left);
+    const right: string = generateExpression(generator, expr.right);
+    const result: string = generateExpressionOperator(generator, left, expr.operator, right);
     return result;
 };
--- src/phase/generator/generation/expression/expression.ts
+++ src/phase/generator/generation/expression/expression.ts
@@ -3,11 +3,53 @@ import { generateExpressionUnary } from './unary';
 import { generateExpressionBinary } from './binary';
 import { generateExpressionLiteral } from './literal';
 import { generateExpressionVariable } from './variable';
+import { TokenOperatorType, TokenType } from '../../../lexer/tokenizer/type';
 import { AstExpression } from '../../../parser/parse/ast/expression/expression';
 import { AstExpressionUnary } from '../../../parser/parse/ast/expression/unary';
 import { AstExpressionBinary } from '../../../parser/parse/ast/expression/binary';
 import { AstExpressionLiteral } from '../../../parser/parse/ast/expression/literal';
 import { AstExpressionVariable } from '../../../parser/parse/ast/expression/variable';
+
+export function generateExpressionOperator(generator: Generator, left: string, op: TokenType, right: string | undefined = undefined): string {
+    switch (op) {
+        case TokenOperatorType.TOKEN_PLUS:
+            if (right === undefined) {
+                return left;
+            }
+            return `${left} + ${right}`;
+        case TokenOperatorType.TOKEN_MINUS:
+            if (right === undefined) {
+                return `-${left}`;
+            }
+            return `${left} - ${right}`;
+        case TokenOperatorType.TOKEN_MULTIPLY:
+            if (right === undefined) {
+                return left;
+            }
+            return `${left} * ${right}`;
+        case TokenOperatorType.TOKEN_DIVIDE:
+            if (right === undefined) {
+                return left;
+            }
+            return `${left} / ${right}`;
+        case TokenOperatorType.TOKEN_MODULO:
+            if (right === undefined) {
+                return left;
+            }
+            return `${left} % ${right}`;
+        case TokenOperatorType.TOKEN_POWER:
+            if (right === undefined) {
+                return left;
+            }
+            return `pow(${left}, ${right})`;
+        case TokenOperatorType.TOKEN_MEMBER:
+            if (right === undefined) {
+                return left;
+            }
+            return `${left}.${right}`;
+    }
+    return "Unknown operator " + op;
+};
 
 export function generateExpression(generator: Generator, expr: AstExpression): string {
     switch (expr.type) {

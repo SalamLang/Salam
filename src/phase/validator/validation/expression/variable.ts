819c8e843028b7c1dfc6caafd74ac61904b97483
Max Base <maxbasecode@gmail.com>
2025-02-02 14:07:25 +0330

    Connect symbol-hash table into blocks

--- src/phase/validator/validation/expression/variable.ts
+++ src/phase/validator/validation/expression/variable.ts
@@ -1,15 +1,23 @@
 import { Validator } from "./../validator";
 import { validateExpression } from "./expression";
-import { AstExpressionVariable } from "../../../parser/parse/ast/expression/variable";
+import { AstBlock } from "../../../parser/parse/ast/block";
 import { AstType } from "../../../parser/parse/ast/expression/type";
+import { AstExpressionVariable } from "../../../parser/parse/ast/expression/variable";
 
-export function validateExpressionVariable(validator: Validator, expr: AstExpressionVariable): void {
-    validateExpression(validator, expr.left);
+export function validateExpressionVariable(validator: Validator, block: AstBlock, expr: AstExpressionVariable): void {
+    validateExpression(validator, block, expr.left);
 
     if (expr.left.value_type === undefined) {
         return;
     }
 
-    // TODO: check if the variable is defined
-    expr.value_type = AstType.createInt();
+    const name: string = expr.left.getString();
+    const symbol_type: AstType | undefined = block.symbol_table.getSymbol(name);
+
+    if (symbol_type === undefined) {
+        validator.pushError(`Variable '${name}' is not defined`);
+        return;
+    }
+
+    expr.value_type = symbol_type;
 };
--- src/phase/validator/validation/function/function_parameters.ts
+++ src/phase/validator/validation/function/function_parameters.ts
@@ -1,9 +1,10 @@
 import { Validator } from "../validator";
+import { AstBlock } from "../../../parser/parse/ast/block";
 import { validateFunctionParameter } from "./function_parameter";
 import { AstFunctionParameter } from "../../../parser/parse/ast/function/function_parameter";
 
-export function validateFunctionParameters(validator: Validator, attributes: AstFunctionParameter[]): void {
+export function validateFunctionParameters(validator: Validator, block: AstBlock, attributes: AstFunctionParameter[]): void {
     for (const attribute of attributes) {
-        validateFunctionParameter(validator, attribute);
+        validateFunctionParameter(validator, block, attribute);
     }
 };

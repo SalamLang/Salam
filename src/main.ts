44555770697c26fb9f42ad2d9df0ab0e35dc54f6
Max Base <maxbasecode@gmail.com>
2025-02-10 13:36:55 +0330

    add salam.bat, create main for salam

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,38 @@ export const phases: string[] = [
     "generator",
 ];
 
-function main(args: string[]): number {
-    let selectedLanguage = languageMapsValues[0];
+export const finalPhase: string = phases[phases.length - 1];
 
-    const result: number = processLanguageFlag(args, selectedLanguage);
-    if (result === 1) {
-        return result;
+function displayHelp() {
+    console.log("You need to choose a phase of the Salam programming language:");
+    phases.forEach(phase => {
+        console.log(`    ${phase}`);
+    });
+    console.log("");
+}
+
+function main(args: string[]): number {
+    if (args.length === 0) {
+        displayHelp();
+        return 1;
     }
 
-    return processCommand("lexer", args, selectedLanguage, result);
-};
+    const selectedLanguage = languageMapsValues[0];
+
+    const languageResult: number = processLanguageFlag(args, selectedLanguage);
+    if (languageResult === 1) {
+        return languageResult;
+    }
+
+    const selectedPhase: string = args[0];
+    if (!phases.includes(selectedPhase)) {
+        return processCommand(finalPhase, args, selectedLanguage, languageResult);
+    }
+
+    const remainingArgs: string[] = args.slice(1);
+    
+    return processCommand(selectedPhase, remainingArgs, selectedLanguage, languageResult);
+}
 
 const args: string[] = process?.argv?.slice(2) || [];
 process.exit(main(args));

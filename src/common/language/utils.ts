532366fcc08048b2eb512518b844a5aa60abc11e
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-02-11 02:38:22 -0800

    Update: getLanguageNameById

--- src/common/language/utils.ts
+++ src/common/language/utils.ts
@@ -4,7 +4,3 @@ import { LanguageID, LanguageMap } from './language';
 export function getLanguageIDByFlag(flag: string): LanguageID | undefined {
     return languageMapsValues.find((lang: LanguageMap) => lang.flag === flag)?.id;
 };
-
-export function getLanguageNameById(id: LanguageID): string | undefined {
-    return languageMapsValues.find((lang: LanguageMap) => lang.id === id)?.name;
-};

94c68c72a383ce0b5ff143f2ee8988e50999275d
Max Base <MaxBaseCode@gmail.com>
2025-02-13 11:32:48 -0800

    Add validator for style element

--- src/test.c
+++ src/test.c
@@ -4,6 +4,7 @@
         <title>Salam Untitled</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="test.png">
     </head>
     <body>
     </body>

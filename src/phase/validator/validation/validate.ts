4158c8c750e09931c8a5c40a170091cbfad1f9d9
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-03-05 16:08:56 +0330

    handling extern in validation layer

--- src/phase/validator/validation/validate.ts
+++ src/phase/validator/validation/validate.ts
@@ -1,12 +1,23 @@
 import { Validator } from "./validator";
+import { validateExtern } from "./extern";
 import { validateLayout } from "./layout/layout";
+import { AstExtern } from "../../parser/parse/ast/extern";
 import { validateFunctionDeclaration } from "./function/function_declaration";
 import { AstFunctionDeclaration } from "./../../parser/parse/ast/function/function_declaration";
 
 export function validate(validator: Validator): void {
+    validator.ast.externs.forEach((node: AstExtern) => {
+        validateExtern(validator, validator.ast.block, node);
+    });
+
     validator.ast.functions.forEach((node: AstFunctionDeclaration) => {
         validateFunctionDeclaration(validator, validator.ast.block, node as AstFunctionDeclaration);
     });
+
+    // const int2str_type: AstType = AstType.createFunction("int2str", "int2str", [new AstFunctionArgument("value", AstType.createInt())], AstType.createString());
+    // int2str_type.setSyetem();
+    // block.symbol_table.addSystemSymbol("int2str", int2str_type);
+
     if (validator.ast.layout !== undefined) {
         validateLayout(validator, validator.ast.block, validator.ast.layout);
     }

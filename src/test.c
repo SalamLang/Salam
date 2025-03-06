c4a202c8adfc934dfc1d464ca616be00aac692b6
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-03-07 00:49:58 +0330

    work on externs

--- src/test.c
+++ src/test.c
@@ -1,16 +1,29 @@
-// Libraries
+// Extern Includes
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <math.h>
-#include <time.h>
-#include <ctype.h>
+
+// Extern Functions
+extern char* int2str(int c);
+
+// Extern Variables
 
 // Sign functions
 int main();
 
 // Functions
 int main() {
+char* name = (char*) malloc(strlen("Max") + 1);
+if (name == NULL) {
+    printf("Memory allocation failed in name\n");
+    exit(1);
+}
+strcpy(name, "Max");
+char* temp_var_1 = name;
+char* temp_var_0 = malloc(sizeof(char) * (4 + strlen2(temp_var_1)) + 1);
+strcpy(temp_var_0, "Hey ");
+strcat(temp_var_0, temp_var_1);
+char* temp_var_2 = malloc(sizeof(char) * (14 + 1) + 1);
+strcpy(temp_var_2, temp_var_0);
+strcat(temp_var_2, "!");
+printf("%s", temp_var_2);
 return (1);
 }

4ddd41e05100fda9003a1b3e4c842a3ebec76e30
Max Base <MaxBaseCode@gmail.com>
2025-03-06 17:34:26 +0330

    working on extern, add extern types

--- src/test.c
+++ src/test.c
@@ -8,58 +8,9 @@
 #include <ctype.h>
 
 // Sign functions
-char* test();
-char* main();
+int main();
 
 // Functions
-char* test() {
-printf("%d", age);
-return ("");
+int main() {
+return (1);
 }
-
-char* main() {
-char* bbb = (char*) malloc(strlen("hey") + 1);
-if (bbb == NULL) {
-    printf("Memory allocation failed in bbb\n");
-    exit(1);
-}
-strcpy(bbb, "hey");
-char* aaa = (char*) malloc(strlen3("...") + strlen4("!!!") + strlen4("??? END") + strlen4(int2str(33)) + 1);
-if (aaa == NULL) {
-    printf("Memory allocation failed in aaa\n");
-    exit(1);
-}
-strcat(aaa, "...");
-strcat(aaa, "!!!");
-strcat(aaa, "??? END");
-strcat(aaa, int2str(33));
-}
-
-<!doctype html>
-<html dir="ltr" lang="en-US">
-    <head>
-        <title>Salam Untitled</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-        .a {color:white;background-color:black;margin-top:20;padding-top:20px}
-        .a:hover {background-color:white}
-        .a:focus {background-color:green}
-        @media screen and (min-width: 400px) {.a:hover {background-color:pink}}
-        @media screen and (min-width: 400px) {.a:focus {background-color:pink}}
-        @media screen and (min-width: 400px) {.a {background-color:pink}}
-        </style>
-    </head>
-    <body>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-        <input class=a>
-    </body>
-</html>

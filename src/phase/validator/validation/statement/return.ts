702920204497f8044782c244d0ee1c021d001749
Max Base <MaxBaseCode@gmail.com>
2025-03-07 02:45:32 +0330

    saev

--- src/phase/validator/validation/statement/return.ts
+++ src/phase/validator/validation/statement/return.ts
@@ -4,5 +4,6 @@ import { validateExpression } from './../expression/expression';
 import { AstStatementReturn } from "../../../parser/parse/ast/statement/return";
 
 export function validateStatementReturn(validator: Validator, block: AstBlock, stmt: AstStatementReturn): void {
+    console.log("validateStatementReturn:", block);
     validateExpression(validator, block, stmt.value);
 };

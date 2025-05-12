d4417806d5189fe1f7159c3523bf36156f048005
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-12 08:09:27 +0330

    lexer: number, ident

--- src/stage/scanner/scanner_scan/scanner_scan.c
+++ src/stage/scanner/scanner_scan/scanner_scan.c
@@ -4,25 +4,56 @@ void scanner_scan(scanner_t* scanner)
 {
     while (scanner->source[scanner->index] != '\0') {
         char ch = scanner->source[scanner->index];
-        // Ignoring invisible characters
-        if (
-            scanner->source[scanner->index] == '\r' ||
-            scanner->source[scanner->index] == '\a' ||
-            scanner->source[scanner->index] == '\b' ||
-            scanner->source[scanner->index] == '\f' ||
-            scanner->source[scanner->index] == ' ' ||
-            scanner->source[scanner->index] == '\t'
-        ) {
-            scanner->column++;
-            scanner->index++;
-            continue;
-        }
-        // Ignoring newline character
-        else if (scanner->source[scanner->index] == '\n') {
-            scanner->column=0;
-            scanner->index++;
-            scanner->line++;
-            continue;
+        switch (ch) {
+            case '\r':
+            case '\a':
+            case '\b':
+            case '\f':
+            case ' ':
+            case '\t': {
+                scanner->column++;
+                scanner->index++;
+                continue;
+            } break;
+            case '\n': {
+                scanner->column=0;
+                scanner->index++;
+                scanner->line++;
+                continue;
+            } break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9': {
+                scanner_scan_number(scanner);
+            }
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's': {
+                scanner_scan_number(scanner);
+            }
         }
 
         token_t* token = token_create(TOKEN_TYPE_UNKNOWN);

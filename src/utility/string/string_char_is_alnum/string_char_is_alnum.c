1ab959938c1725aa56f46b01a47decc9606c91cd
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-13 03:25:17 +0330

    Adding string_char package

---
+++ src/utility/string/string_char_is_alnum/string_char_is_alnum.c
@@ -0,0 +1,15 @@
+#include <utility/string/string_char_is_alnum/string_char_is_alnum.h>
+
+/**
+ *
+ * @function string_char_is_alnum
+ * @brief Check if a character is an alphabet or a digit
+ * @params {char} c - Character
+ * @returns {bool}
+ *
+ */
+bool string_char_is_alnum(char c)
+{
+    DEBUG_ME;
+    return string_char_is_alpha(c) || string_char_is_digit(c);
+}
---
+++ src/utility/string/string_char_is_alpha/string_char_is_alpha.h
@@ -0,0 +1,6 @@
+#include <stdbool.h> // for bool
+
+// base
+#include <base.h>
+
+bool string_char_is_alpha(const char c);

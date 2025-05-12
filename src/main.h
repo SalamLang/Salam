ec1b4c9b59a05b2409564cdce0a8afb6c9820931
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-12 05:42:32 +0330

    start scanner stage

--- src/main.h
+++ src/main.h
@@ -3,3 +3,8 @@
 #include <utility/file/file_reads/file_reads.h>
 #include <utility/memory/memory_free/memory_free.h>
 #include <utility/log/log_fatal/log_fatal.h>
+
+#include <stage/scanner/type.h>
+#include <stage/scanner/scanner_create/scanner_create.h>
+#include <stage/scanner/scanner_log/scanner_log.h>
+#include <stage/scanner/scanner_free/scanner_free.h>

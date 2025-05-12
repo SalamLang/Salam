1e3aa274d8a797fc89633a603cdf872d6c9379fd
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-13 03:25:17 +0330

    Adding string_char package

---
+++ src/utility/string/string_char_is_alnum/string_char_is_alnum.h
@@ -0,0 +1,10 @@
+#include <stdbool.h> // for bool
+
+// base
+#include <base.h>
+
+// string
+#include <utility/string/string_char_is_alpha/string_char_is_alpha.h>
+#include <utility/string/string_char_is_digit/string_char_is_digit.h>
+
+bool string_char_is_alnum(const char c);
---
+++ src/utility/string/string_char_is_digit/string_char_is_digit.h
@@ -0,0 +1,6 @@
+#include <stdbool.h> // for bool
+
+// base
+#include <base.h>
+
+bool string_char_is_digit(char c);

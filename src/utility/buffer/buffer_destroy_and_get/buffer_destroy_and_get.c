9a8c0b46066dd3aa1aaaecc8026e86f27432d017
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-13 02:24:14 +0330

    Continue fixing issues

--- src/utility/buffer/buffer_destroy_and_get/buffer_destroy_and_get.c
+++ src/utility/buffer/buffer_destroy_and_get/buffer_destroy_and_get.c
@@ -11,11 +11,11 @@
 char *buffer_destroy_and_get(buffer_t *str)
 {
     DEBUG_ME;
-    char *res = buffer_strdup(str->data);
+    char *res = string_duplicate(str->data);
     buffer_destroy(str);
 
     if (res == NULL) {
-        error(2, "Failed to allocate memory for buffer data");
+        log_fatal("Failed to allocate memory for buffer data");
 
         return NULL;
     }
--- src/utility/buffer/buffer_lower_str/buffer_lower_str.c
+++ src/utility/buffer/buffer_lower_str/buffer_lower_str.c
@@ -11,9 +11,9 @@
 char *buffer_lower_str(const char *str)
 {
     DEBUG_ME;
-    char *buffer = buffer_strdup(str);
+    char *buffer = string_duplicate(str);
 
-    for (size_t i = 0; i < strlen(buffer); i++) {
+    for (size_t i = 0; i < string_length(buffer); i++) {
         buffer[i] = tolower(buffer[i]);
     }
 

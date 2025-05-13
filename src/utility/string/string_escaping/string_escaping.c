76978a9f3b147e7468d1848a43d0c83bbdcaeab2
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-13 07:18:43 +0330

    Complete logic of string_escaping for now

--- src/utility/string/string_escaping/string_escaping.c
+++ src/utility/string/string_escaping/string_escaping.c
@@ -2,5 +2,34 @@
 
 char* string_escaping(const char* input)
 {
-    return string_replace_all(input, "\"", "\\\\");
+    if (!input) return NULL;
+
+    char* temp = string_replace_all(input, "\\", "\\\\");
+    char* next;
+
+    next = string_replace_all(temp, "\"", "\\\"");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\n", "\\n");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\t", "\\t");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\r", "\\r");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\b", "\\b");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\f", "\\f");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\a", "\\a");
+    free(temp); temp = next;
+
+    next = string_replace_all(temp, "\v", "\\v");
+    free(temp); temp = next;
+
+    return temp;
 }

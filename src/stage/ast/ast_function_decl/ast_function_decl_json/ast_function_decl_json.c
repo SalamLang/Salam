cdafd6feb8e81ba458c8aeee5c5120011c40f997
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-13 11:03:37 +0330

    Continue working on AST

---
+++ src/stage/ast/ast_function_decl/ast_function_decl_json/ast_function_decl_json.c
@@ -0,0 +1,12 @@
+#include <stage/ast/ast_function_decl_json/ast_function_decl_json.h>
+
+char* ast_function_decl_json(ast_function_decl_t* function_decl)
+{
+    buffer_t* temp = buffer_create(24);
+
+    // TODO
+
+    char* result = string_duplicate(temp->data);
+    buffer_destroy(temp);
+    return result;
+}
---
+++ src/stage/ast/ast_program/ast_program_json/ast_program_json.h
@@ -0,0 +1,11 @@
+// base
+#include <base.h>
+
+// buffer
+#include <utility/buffer/buffer_create/buffer_create.h>
+#include <utility/buffer/buffer_destroy/buffer_destroy.h>
+
+// string
+#include <utility/string/string_duplicate/string_duplicate.h>
+
+char* ast_program_json(ast_program_t* program);

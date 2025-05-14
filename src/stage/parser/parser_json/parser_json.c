d0edb2810f0e525a0c7ff82aa4e620d9d8cb447d
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-14 04:42:07 +0330

    Add parser_json, scanner json

---
+++ src/stage/parser/parser_json/parser_json.c
@@ -0,0 +1,12 @@
+#include <stage/parser/parser_json/parser_json.h>
+
+char* parser_json(parser_t* parser)
+{
+    buffer_t* temp = buffer_create();
+
+    // TODO
+
+    char* result = string_duplicate(temp->data);
+    buffer_destroy(temp);
+    return result;
+}
---
+++ src/stage/parser/parser_json/parser_json.h
@@ -0,0 +1,16 @@
+#ifndef _STAGE_PARSER_PARSER_JSON_PARSER_JSON_H_
+#define _STAGE_PARSER_PARSER_JSON_PARSER_JSON_H_
+
+// base
+#include <base.h>
+
+// buffer
+#include <utility/buffer/type.h>
+#include <utility/buffer/buffer_destroy/buffer_destroy.h>
+
+// string
+#include <utility/string/string_duplicate/string_duplicate.h>
+
+char* parser_json(parser_t* parser);
+
+#endif;

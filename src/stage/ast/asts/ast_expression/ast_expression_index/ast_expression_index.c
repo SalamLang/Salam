9d120bb6ef00f57397c102d78b7d2b6ac1c752f5
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-15 04:13:06 +0330

    update

---
+++ src/stage/ast/asts/ast_expression/ast_expression_index/ast_expression_index.c
@@ -0,0 +1,3 @@
+#include "ast_expression_index.h"
+
+// Implementation here
---
+++ src/stage/ast/asts/ast_expression/ast_expression_index/ast_expression_index.h
@@ -0,0 +1,6 @@
+#ifndef AST_EXPRESSION_INDEX_H
+#define AST_EXPRESSION_INDEX_H
+
+// Declarations here
+
+#endif // AST_EXPRESSION_INDEX_H

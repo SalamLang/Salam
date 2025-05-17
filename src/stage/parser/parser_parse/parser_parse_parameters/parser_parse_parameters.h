02361ebbae6a46516ba063957f490fb078346a90
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 09:13:54 +0330

    Continue fixing issues

--- src/stage/parser/parser_parse/parser_parse_parameters/parser_parse_parameters.h
+++ src/stage/parser/parser_parse/parser_parse_parameters/parser_parse_parameters.h
@@ -1,8 +1,14 @@
 #ifndef _STAGE_PARSER_PARSER_PARSE_PARSER_PARSE_PARAMETERS_PARSER_PARSE_PARAMETERS_H_
 #define _STAGE_PARSER_PARSER_PARSE_PARSER_PARSE_PARAMETERS_PARSER_PARSE_PARAMETERS_H_
 
+#include <stddef.h> // for size_t
+
 // base
 #include <base.h>
+
+// array
+#include <utility/array/type.h>
+#include <utility/array/array_create/array_create.h>
 
 // parser
 #include <stage/parser/type.h>
@@ -12,6 +18,7 @@
 
 // ast
 #include <stage/ast/type.h>
+#include <stage/ast/asts/ast_parameter/ast_parameters/ast_parameters_create/ast_parameters_create.h>
 
 ast_t* parser_parse_parameters(parser_t* parser);
 

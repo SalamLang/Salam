f7360db42e3ee1c43d3ced110c957bc047ead4ca
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 08:52:44 +0330

    Rename files/dirs in ast_parameter_item

--- src/stage/ast/ast_destroy/ast_destroy.h
+++ src/stage/ast/ast_destroy/ast_destroy.h
@@ -14,23 +14,23 @@
 #include <stage/ast/asts/ast_function_decl/ast_function_decl_direct_destroy/ast_function_decl_direct_destroy.h>
 #include <stage/ast/asts/ast_block/ast_block_direct_destroy/ast_block_direct_destroy.h>
 #include <stage/ast/asts/ast_program/ast_program_direct_destroy/ast_program_direct_destroy.h>
-#include <stage/ast/asts/ast_parameter/ast_parameter_direct_destroy/ast_parameter_direct_destroy.h>
-#include <stage/ast/asts/ast_parameters/ast_parameters_direct_destroy/ast_parameters_direct_destroy.h>
-#include <stage/ast/asts/ast_attribute/ast_attribute_direct_destroy/ast_attribute_direct_destroy.h>
-#include <stage/ast/asts/ast_attributes/ast_attributes_direct_destroy/ast_attributes_direct_destroy.h>
-#include <stage/ast/asts/ast_argument/ast_argument_direct_destroy/ast_argument_direct_destroy.h>
-#include <stage/ast/asts/ast_arguments/ast_arguments_direct_destroy/ast_arguments_direct_destroy.h>
+#include <stage/ast/asts/ast_parameter/ast_parameter_item/ast_parameter_item_direct_destroy/ast_parameter_item_direct_destroy.h>
+#include <stage/ast/asts/ast_parameter/ast_parameters/ast_parameters_direct_destroy/ast_parameters_direct_destroy.h>
+#include <stage/ast/asts/ast_attribute/ast_attribute_item/ast_attribute_item_direct_destroy/ast_attribute_item_direct_destroy.h>
+#include <stage/ast/asts/ast_attribute/ast_attributes/ast_attributes_direct_destroy/ast_attributes_direct_destroy.h>
+#include <stage/ast/asts/ast_argument/ast_argument_item/ast_argument_direct_destroy/ast_argument_direct_destroy.h>
+#include <stage/ast/asts/ast_argument/ast_arguments/ast_arguments_direct_destroy/ast_arguments_direct_destroy.h>
 
 // #include <stage/ast/asts/ast_variable_decl/ast_variable_decl_destroy/ast_variable_decl_destroy.h>
 // #include <stage/ast/asts/ast_function_decl/ast_function_decl_destroy/ast_function_decl_destroy.h>
 // #include <stage/ast/asts/ast_block/ast_block_destroy/ast_block_destroy.h>
 // #include <stage/ast/asts/ast_program/ast_program_destroy/ast_program_destroy.h>
-// #include <stage/ast/asts/ast_parameter/ast_parameter_destroy/ast_parameter_destroy.h>
-// #include <stage/ast/asts/ast_parameters/ast_parameters_destroy/ast_parameters_destroy.h>
-// #include <stage/ast/asts/ast_attribute/ast_attribute_destroy/ast_attribute_destroy.h>
+// #include <stage/ast/asts/ast_parameter/ast_parameter_item/ast_parameter_item_destroy/ast_parameter_item_destroy.h>
+// #include <stage/ast/asts/ast_parameter/ast_parameters/ast_parameters_destroy/ast_parameters_destroy.h>
+// #include <stage/ast/asts/ast_attribute/ast_attribute_item/ast_attribute_item_destroy/ast_attribute_item_destroy.h>
 // #include <stage/ast/asts/ast_attributes/ast_attributes_destroy/ast_attributes_destroy.h>
 // #include <stage/ast/asts/ast_argument/ast_argument_destroy/ast_argument_destroy.h>
-// #include <stage/ast/asts/ast_arguments/ast_arguments_destroy/ast_arguments_destroy.h>
+// #include <stage/ast/asts/ast_argument/ast_arguments/ast_arguments_destroy/ast_arguments_destroy.h>
 
 void ast_destroy(ast_t* ast);
 

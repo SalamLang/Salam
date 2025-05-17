b0bfa7d6a02a7f5dd52c08fa481023e691b3376d
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 16:08:12 +0330

    Create json, direct_json for ast_statements, etc

---
+++ src/stage/ast/asts/ast_statement/ast_statement_for/ast_statement_for_direct_json/ast_statement_for_direct_json.c
@@ -0,0 +1,67 @@
+#include "ast_statement_for_direct_json.h"
+
+char* ast_statement_for_direct_json(ast_statement_for_t statement_for)
+{
+    buffer_t* temp = buffer_create(24);
+
+    buffer_append_char(temp, '{');
+
+    // initializer
+    buffer_append_str(temp, "\"initializer\": ");
+    if (statement_for->initializer == NULL)
+    {
+        buffer_append_str(temp, "null");
+    }
+    else
+    {
+        char* initializer = ast_json(statement_for->initializer);
+        buffer_append_str(temp, initializer);
+        memory_destroy(initializer);
+    }
+
+    // condition
+    buffer_append_str(temp, "\"condition\": ");
+    if (statement_for->condition == NULL)
+    {
+        buffer_append_str(temp, "null");
+    }
+    else
+    {
+        char* condition = ast_json(statement_for->condition);
+        buffer_append_str(temp, condition);
+        memory_destroy(condition);
+    }
+
+    // increment
+    buffer_append_str(temp, "\"increment\": ");
+    if (statement_for->increment == NULL)
+    {
+        buffer_append_str(temp, "null");
+    }
+    else
+    {
+        char* increment = ast_json(statement_for->increment);
+        buffer_append_str(temp, increment);
+        memory_destroy(increment);
+    }
+
+    // block
+    buffer_append_str(temp, "\"block\": ");
+    if (statement_for->block == NULL)
+    {
+        buffer_append_str(temp, "null");
+    }
+    else
+    {
+        char* block = ast_json(statement_for->block);
+        buffer_append_str(temp, block);
+        memory_destroy(block);
+    }
+
+
+    buffer_append_str(temp, "}");
+
+    char* result = string_duplicate(temp->data);
+    buffer_destroy(temp);
+    return result;
+}

9e3f10b9e3d0867f77ab7711b24f432d8b42951b
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 17:04:14 +0330

    Start working on AST of exprs

---
+++ src/stage/ast/asts/ast_expression/ast_expression_type_name/ast_expression_type_name.c
@@ -0,0 +1,21 @@
+#include "ast_expression_type_name.h"
+
+char* ast_expression_type_name(ast_expression_item_t* expression_item)
+{
+    switch (expression_item->type)
+    {
+        case AST_EXPRESSION_TYPE_LITERAL: return "literal";
+            break;
+        case AST_EXPRESSION_TYPE_IDENTIFIER: return "identifier";
+            break;
+        case AST_EXPRESSION_TYPE_BINARY: return "binary";
+            break;
+        case AST_EXPRESSION_TYPE_UNARY: return "unary";
+            break;
+        case AST_EXPRESSION_TYPE_INDEX: return "index";
+            break;
+        case AST_EXPRESSION_TYPE_CALL: return "call";
+            break;
+    }
+    return "EXPRESSION_TYPE_UNKNOWN";
+}

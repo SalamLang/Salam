49c80241a76dce84a74afd750a9862dce83d0e0e
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 16:49:35 +0330

    Continue working on AST, ast_kind_union, ast_kind_struct, ast_kind enum, etc

---
+++ src/stage/ast/asts/ast_block/ast_block_direct_json/ast_block_direct_json.h
@@ -0,0 +1,19 @@
+#ifndef _STAGE_AST_ASTS_AST_BLOCK_AST_BLOCK_DIRECT_JSON_AST_BLOCK_DIRECT_JSON_H_
+#define _STAGE_AST_ASTS_AST_BLOCK_AST_BLOCK_DIRECT_JSON_AST_BLOCK_DIRECT_JSON_H_
+
+// base
+#include <base.h>
+
+// buffer
+#include <utility/buffer/buffer_create/buffer_create.h>
+#include <utility/buffer/buffer_destroy/buffer_destroy.h>
+
+// string
+#include <utility/string/string_duplicate/string_duplicate.h>
+
+// ast
+#include <stage/ast/type.h>
+
+char* ast_block_direct_json(ast_block_t block);
+
+#endif // _STAGE_AST_ASTS_AST_BLOCK_AST_BLOCK_DIRECT_JSON_AST_BLOCK_DIRECT_JSON_H_
--- src/stage/ast/asts/ast_function_decl/ast_function_decl_json/ast_function_decl_json.c
+++ src/stage/ast/asts/ast_function_decl/ast_function_decl_json/ast_function_decl_json.c
@@ -4,7 +4,47 @@ char* ast_function_decl_json(ast_function_decl_t* function_decl)
 {
     buffer_t* temp = buffer_create(24);
 
-    // TODO
+    if (function_decl == NULL)
+    {
+        buffer_append_str(temp, "null");
+    }
+    else {
+        buffer_append_char(temp, '{');
+
+        // name
+        buffer_append_str(temp, "\"name\":");
+        if (function_decl->name == NULL) {
+            buffer_append_str(temp, "null");
+        } else {
+            buffer_append_str(temp, "\"");
+            char* name = string_escaping(function_decl->name);
+            buffer_append_str(temp, name);
+            memory_destroy(name);
+            buffer_append_str(temp, "\"");
+        }
+
+        // parameters
+        buffer_append_str(temp, ", \"parameters\":");
+        if (function_decl->parameters == NULL) {
+            buffer_append_str(temp, "null");
+        } else {
+            char* name = ast_json(function_decl->parameters);
+            buffer_append_str(temp, name);
+            memory_destroy(name);
+        }
+
+        // block
+        buffer_append_str(temp, ", \"block\":");
+        if (function_decl->block == NULL) {
+            buffer_append_str(temp, "null");
+        } else {
+            char* name = ast_json(function_decl->block);
+            buffer_append_str(temp, name);
+            memory_destroy(name);
+        }
+
+        buffer_append_char(temp, '}');
+    }
 
     char* result = string_duplicate(temp->data);
     buffer_destroy(temp);

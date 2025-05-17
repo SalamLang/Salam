10c72f3684f00d58d6b348925b58ab75bb5afa70
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-17 09:48:22 +0330

    update ast_argument, ast_parameter

--- src/stage/ast/asts/ast_parameter/ast_parameter_item/ast_parameter_item_direct_destroy/ast_parameter_item_direct_destroy.h
+++ src/stage/ast/asts/ast_parameter/ast_parameter_item/ast_parameter_item_direct_destroy/ast_parameter_item_direct_destroy.h
@@ -1,6 +1,16 @@
 #ifndef _STAGE_AST_ASTS_AST_PARAMETER_AST_PARAMETER_ITEM_AST_PARAMETER_DIRECT_DESTROY_AST_PARAMETER_DIRECT_DESTROY_H_
 #define _STAGE_AST_ASTS_AST_PARAMETER_AST_PARAMETER_ITEM_AST_PARAMETER_DIRECT_DESTROY_AST_PARAMETER_DIRECT_DESTROY_H_
 
+// base
+#include <base.h>
 
+// memory
+#include <utility/memory/memory_destroy/memory_destroy.h>
+
+// array
+#include <utility/array/type.h>
+#include <utility/array/array_destroy/array_destroy.h>
+
+void ast_parameter_item_direct_destroy(ast_parameter_item_t parameter_item);
 
 #endif // _STAGE_AST_ASTS_AST_PARAMETER_AST_PARAMETER_ITEM_AST_PARAMETER_DIRECT_DESTROY_AST_PARAMETER_DIRECT_DESTROY_H_

9bd91ff1099f59044d06e5c69272e69c2770b4bb
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-17 09:33:37 +0330

    Fix some compiler issues

--- src/stage/ast/asts/ast_argument/ast_arguments/ast_arguments_create/ast_arguments_create.c
+++ src/stage/ast/asts/ast_argument/ast_arguments/ast_arguments_create/ast_arguments_create.c
@@ -1,3 +1,10 @@
-#include "ast_arguments_create.h"
+#include <stage/ast/asts/ast_argument/ast_arguments/ast_arguments_create/ast_arguments_create.h>
 
-// Implementation here
+ast_t* ast_arguments_create(array_t* values, size_t value_count)
+{
+    DEBUG_ME;
+    ast_t* arguments = ast_alloc(AST_TYPE_ARGUMENTS);
+    arguments->raw.arguments_value.values = values;
+    arguments->raw.arguments_value.value_count = value_count;
+    return arguments;
+}
---
+++ src/stage/ast/asts/ast_argument/ast_arguments/ast_arguments_direct_destroy/ast_arguments_direct_destroy.c
@@ -0,0 +1,10 @@
+
+#include <stage/ast/asts/ast_argument/ast_arguments/ast_arguments_destroy/ast_arguments_destroy.h>
+
+void ast_arguments_direct_destroy(ast_arguments_t arguments)
+{
+    DEBUG_ME;
+    if (arguments.values != NULL) {
+        array_destroy(arguments.values);
+    }
+}

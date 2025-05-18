488dcd1056c3672834f3d5e791e0edd50740d70a
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-18 22:58:02 +0330

    Add logs for ast_expr_literal_Direct_json and ast_expr_item_direct_json

--- src/stage/ast/asts/ast_expression/ast_expression_item/ast_expression_item_direct_json/ast_expression_item_direct_json.c
+++ src/stage/ast/asts/ast_expression/ast_expression_item/ast_expression_item_direct_json/ast_expression_item_direct_json.c
@@ -17,49 +17,63 @@ char* ast_expression_item_direct_json(ast_expression_item_t expression_item)
     switch (expression_item.type)
     {
         case AST_EXPRESSION_TYPE_LITERAL:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_LITERAL");
             buffer_append_str(temp, "\"literal\": ");
             value = ast_expression_literal_direct_json(expression_item.raw.literal);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_LITERAL");
             break;
 
         case AST_EXPRESSION_TYPE_IDENTIFIER:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_IDENTIFIER");
             buffer_append_str(temp, "\"identifier\": ");
             value = ast_expression_identifier_direct_json(expression_item.raw.identifier);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_IDENTIFIER");
             break;
 
         case AST_EXPRESSION_TYPE_BINARY:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_BINARY");
             buffer_append_str(temp, "\"binary\": ");
             value = ast_expression_binary_direct_json(expression_item.raw.binary);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_BINARY");
             break;
 
         case AST_EXPRESSION_TYPE_UNARY:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_UNARY");
             buffer_append_str(temp, "\"unary\": ");
             value = ast_expression_unary_direct_json(expression_item.raw.unary);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_UNARY");
             break;
 
         case AST_EXPRESSION_TYPE_INDEX:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_INDEX");
             buffer_append_str(temp, "\"index\": ");
             value = ast_expression_index_direct_json(expression_item.raw.index);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_INDEX");
             break;
 
         case AST_EXPRESSION_TYPE_CALL:
+            log_info("ast_expression_item_direct_json - Entering case AST_EXPRESSION_TYPE_CALL");
             buffer_append_str(temp, "\"call\": ");
             value = ast_expression_call_direct_json(expression_item.raw.call);
             buffer_append_str(temp, value);
             memory_destroy(value);
+            log_info("ast_expression_item_direct_json - Exiting case AST_EXPRESSION_TYPE_CALL");
             break;
 
         default:
+            log_info("Entering default case (unknown type)");
             buffer_append_str(temp, "\"unknown\": null");
+            log_info("Exiting default case");
             break;
     }
 

1d8fd19128c993a8b2e791041fec7c454503d271
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-19 13:38:33 +0330

    Check ast.json, tokens.json, add base to all ast, and structures to make to easy to track - looking for a bug

--- src/stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.c
+++ src/stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.c
@@ -1,9 +1,9 @@
 #include <stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.h>
 
-ast_t* ast_function_decl_create(const char* name, ast_t* parameters, ast_t* block)
+ast_t* ast_function_decl_create(const char* name, ast_t* parameters, ast_t* block, ast_type_t* return_type)
 {
     ast_t* ast = ast_alloc(AST_TYPE_FUNCTION_DECL);
-    ast->raw.function_decl_value.return_type = NULL; // TODO: set the return type
+    ast->raw.function_decl_value.return_type = return_type;
     ast->raw.function_decl_value.name = string_duplicate(name);
     ast->raw.function_decl_value.parameters = parameters;
     ast->raw.function_decl_value.block = block;
--- src/stage/parser/parser_parse/parser_parse_function_decl/parser_parse_function_decl.c
+++ src/stage/parser/parser_parse/parser_parse_function_decl/parser_parse_function_decl.c
@@ -3,11 +3,18 @@
 ast_t* parser_parse_function_decl(parser_t* parser)
 {
     DEBUG_ME;
+    if (!parser) return NULL;
+
     parser_expect(parser, TOKEN_TYPE_KEYWORD_FN);
+
     token_t* name = parser_expect_get(parser, TOKEN_TYPE_IDENTIFIER);
+
     ast_t* parameters = parser_parse_parameters(parser);
+
+    ast_type_t* return_type = NULL; // TODO
+
     ast_t* block = parser_parse_block(parser);
 
-    ast_t* ast = ast_function_decl_create(name->source, parameters, block);
+    ast_t* ast = ast_function_decl_create(name->source, parameters, block, return_type);
     return ast;
 }

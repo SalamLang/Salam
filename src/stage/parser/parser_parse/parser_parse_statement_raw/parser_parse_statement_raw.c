c95722b443b575b70b634f5a134841a8df79436f
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-19 16:15:04 +0330

    Adding parser_parse_statement_raw

---
+++ src/stage/parser/parser_parse/parser_parse_statement_raw/parser_parse_statement_raw.c
@@ -0,0 +1,13 @@
+#include "parser_parse_statement_raw.h"
+
+ast_t* parser_parse_statement_raw(parser_t* parser)
+{
+    DEBUG_ME;
+    if (!parser) return NULL;
+
+    token_t* token = PARSER_CURRENT;
+
+    parser_expect(parser, TOKEN_TYPE_KEYWORD_RAW);
+
+    return ast_statement_raw_create(token);
+}

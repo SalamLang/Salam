0bfc903e1e7d968f96a7b9ea34b7cfff3fb5e877
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-19 16:16:37 +0330

    Fix mistake in header file, change macro names

--- src/stage/parser/parser_parse/parser_parse_statement_raw/parser_parse_statement_raw.h
+++ src/stage/parser/parser_parse/parser_parse_statement_raw/parser_parse_statement_raw.h
@@ -4,6 +4,14 @@
 // base
 #include <base.h>
 
+// parser
+#include <stage/parser/type.h>
+#include <stage/parser/parser_expect/parser_expect.h>
+
+// ast
+#include <stage/ast/type.h>
+#include <stage/ast/asts/ast_statement/ast_statement_raw/ast_statement_raw_create/ast_statement_raw_create.h>
+
 ast_t* parser_parse_statement_raw(parser_t* parser);
 
 #endif // _STAGE_PARSER_PARSER_PARSE_PARSER_PARSE_STATEMENT_PARSER_PARSE_STATEMENT_RAW_PARSER_PARSE_STATEMENT_RAW_H_

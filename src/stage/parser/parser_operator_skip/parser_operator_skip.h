5da2098b29936495a2b438742c8281850c3c7ef5
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-19 15:23:18 +0330

    Add parser_operator_skip, handling multiple values in parse_expressions

---
+++ src/stage/parser/parser_operator_skip/parser_operator_skip.h
@@ -0,0 +1,20 @@
+#ifndef _STAGE_PARSER_PARSER_SKIP_PARSER_SKIP_H_
+#define _STAGE_PARSER_PARSER_SKIP_PARSER_SKIP_H_
+
+#include <stdbool.h> // for bool, true, false
+
+// base
+#include <base.h>
+
+// log
+#include <utility/log/log_fatal/log_fatal.h>
+
+// parser
+#include <stage/parser/type.h>
+#include <stage/parser/parser_next/parser_next.h>
+// token
+#include <stage/token/type.h>
+
+bool parser_operator_skip(parser_t* parser, token_operator_type_t operator_type);
+
+#endif // _STAGE_PARSER_PARSER_SKIP_PARSER_SKIP_H_

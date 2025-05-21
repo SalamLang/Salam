abde47b77c02507065a85f2903c8468bfd5f6957
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-21 20:05:35 +0330

    Add ast_expression_identifier_create, handling identifier in expr literal

--- src/stage/ast/asts/ast_expression/ast_expression_identifier/ast_expression_identifier_create/ast_expression_identifier_create.c
+++ src/stage/ast/asts/ast_expression/ast_expression_identifier/ast_expression_identifier_create/ast_expression_identifier_create.c
@@ -1,3 +1,12 @@
 #include "ast_expression_identifier_create.h"
 
-// Implementation here
+ast_t* ast_expression_identifier_create(char* name, ast_type_t* runtime_type)
+{
+    DEBUG_ME;
+    ast_t* ast = ast_alloc(AST_TYPE_EXPRESSION_IDENTIFIER);
+    // ast->raw.expression_literal_value = memory_allocation(sizeof(ast_expression_literal_t));
+    ast->raw.expression_identifier_value.name = string_duplicate(name);
+    ast->raw.expression_identifier_value.runtime_type = runtime_type;
+
+    return ast;
+}
--- src/stage/ast/asts/ast_expression/ast_expression_identifier/ast_expression_identifier_create/ast_expression_identifier_create.h
+++ src/stage/ast/asts/ast_expression/ast_expression_identifier/ast_expression_identifier_create/ast_expression_identifier_create.h
@@ -1,6 +1,16 @@
 #ifndef _STAGE_AST_ASTS_AST_EXPRESSION_AST_EXPRESSION_IDENTIFIER_AST_EXPRESSION_IDENTIFIER_CREATE_AST_EXPRESSION_IDENTIFIER_CREATE_H_
 #define _STAGE_AST_ASTS_AST_EXPRESSION_AST_EXPRESSION_IDENTIFIER_AST_EXPRESSION_IDENTIFIER_CREATE_AST_EXPRESSION_IDENTIFIER_CREATE_H_
 
+// base
+#include <base.h>
 
+// string
+#include <utility/string/string_duplicate/string_duplicate.h>
+
+// ast
+#include <stage/ast/type.h>
+#include <stage/ast/ast_alloc/ast_alloc.h>
+
+ast_t* ast_expression_identifier_create(char* name, ast_type_t* runtime_type);
 
 #endif // _STAGE_AST_ASTS_AST_EXPRESSION_AST_EXPRESSION_IDENTIFIER_AST_EXPRESSION_IDENTIFIER_CREATE_AST_EXPRESSION_IDENTIFIER_CREATE_H_

65adda38b3d70e0f6925364644dac07e3ff7b28d
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-22 22:12:59 +0330

    Adding AST_TYPE_TYPE in generator_c_node

--- src/stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.c
+++ src/stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.c
@@ -1,6 +1,6 @@
 #include <stage/ast/asts/ast_function_decl/ast_function_decl_create/ast_function_decl_create.h>
 
-ast_t* ast_function_decl_create(const char* name, ast_type_t* return_type, ast_t* parameters, ast_t* block)
+ast_t* ast_function_decl_create(const char* name, ast_t* return_type, ast_t* parameters, ast_t* block)
 {
     ast_t* ast = ast_alloc(AST_TYPE_FUNCTION_DECL);
     ast->raw.function_decl_value.return_type = return_type;

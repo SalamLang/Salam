e6b7db6b419e7cdb78f8bc843aa7f4fcefd231c9
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-23 14:48:26 +0330

    complete ast_type_json, and ast_type_name

---
+++ src/stage/ast/ast_type/ast_type_name/ast_type_name.h
@@ -0,0 +1,12 @@
+#ifndef _STAGE_AST_AST_TYPE_AST_TYPE_NAME_AST_TYPE_NAME_H_
+#define _STAGE_AST_AST_TYPE_AST_TYPE_NAME_AST_TYPE_NAME_H_
+
+// base
+#include <base.h>
+
+// ast
+#include <stage/ast/type.h>
+
+char* ast_type_name(ast_type_type_t type);
+
+#endif // _STAGE_AST_AST_TYPE_AST_TYPE_NAME_AST_TYPE_NAME_H_

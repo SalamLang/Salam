68c5a36da5fd9ee9c319453cfb828bf92b48bb78
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2025-05-23 19:23:34 +0330

    Complete expressions in generator phase

--- src/stage/generator/generator_c/generator_c_expression/generator_c_expression_identifier/generator_c_expression_identifier.c
+++ src/stage/generator/generator_c/generator_c_expression/generator_c_expression_identifier/generator_c_expression_identifier.c
@@ -4,11 +4,12 @@ buffer_t* generator_c_expression_identifier(generator_t* generator, ast_t* ast)
 {
     DEBUG_ME;
     ast_expression_item_t expression_item = ast->raw.expression_item_value;
-    ast_expression_identifier_t expression_identifier = expression_item->raw.identifier;
+    ast_expression_identifier_t expression_identifier = expression_item.raw.identifier;
 
     buffer_t* temp = buffer_create(12);
 
-    buffer_append_str(temp, expression_identifier->name);
+    // TODO: validate if the variable already defiend or not.
+    buffer_append_str(temp, expression_identifier.name);
 
     return temp;
 }
--- src/stage/generator/generator_c/generator_c_expression/generator_c_expression_literal/generator_c_expression_literal.c
+++ src/stage/generator/generator_c/generator_c_expression/generator_c_expression_literal/generator_c_expression_literal.c
@@ -4,11 +4,44 @@ buffer_t* generator_c_expression_literal(generator_t* generator, ast_t* ast)
 {
     DEBUG_ME;
     ast_expression_item_t expression_item = ast->raw.expression_item_value;
-    ast_expression_literal_t expression_literal = expression_item->raw.literal;
+    ast_expression_literal_t expression_literal = expression_item.raw.literal;
 
     buffer_t* temp = buffer_create(12);
 
-    buffer_append_str(temp, "\"value\"");
+    char* value_str = value_json(expression_literal.value);
+
+    switch (expression_literal.value->type) {
+        case VALUE_TYPE_STRING:
+            buffer_append_char(temp, '"');
+            buffer_append_str(temp, expression_literal.value->raw.string_value);
+            buffer_append_char(temp, '"');
+            break;
+
+        case VALUE_TYPE_NUMBER_INT:
+            buffer_append_str(temp, convert_int2string(expression_literal.value->raw.int_value));
+            break;
+
+        case VALUE_TYPE_NUMBER_FLOAT:
+            buffer_append_str(temp, convert_float2string(expression_literal.value->raw.float_value));
+            break;
+
+        case VALUE_TYPE_BOOL:
+            buffer_append_str(temp, expression_literal.value->raw.bool_value ? "true" : "false");
+            break;
+
+        case VALUE_TYPE_SIZE:
+            buffer_append_str(temp, convert_size2string(expression_literal.value->raw.size_value));
+            break;
+
+        case VALUE_TYPE_NULL:
+            buffer_append_str(temp, "NULL");
+            break;
+
+        default:
+            log_fatal("Error: unknown value type!");
+            break;
+    }
+    memory_destroy(value_str);
 
     return temp;
 }

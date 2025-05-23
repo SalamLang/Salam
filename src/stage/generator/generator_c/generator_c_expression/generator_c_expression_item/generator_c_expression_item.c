44f107a8e2b539477a6adc540ef5d4912710732a
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-23 19:23:34 +0330

    Complete expressions in generator phase

--- src/stage/generator/generator_c/generator_c_expression/generator_c_expression_item/generator_c_expression_item.c
+++ src/stage/generator/generator_c/generator_c_expression/generator_c_expression_item/generator_c_expression_item.c
@@ -6,40 +6,41 @@ buffer_t* generator_c_expression_item(generator_t* generator, ast_t* ast)
     ast_expression_item_t expression_item = ast->raw.expression_item_value;
 
     buffer_t* temp = buffer_create(12);
+    buffer_t* buffer;
 
     switch (expression_item.type) {
-        case AST_KIND_EXPRESSION_LITERAL:
-            buffer_t* buffer = generator_c_expression_literal(generator, ast);
+        case AST_EXPRESSION_TYPE_LITERAL:
+            buffer = generator_c_expression_literal(generator, ast);
             buffer_append(temp, buffer);
             buffer_destroy(buffer);
             break;
 
-        case AST_KIND_EXPRESSION_IDENTIFIER:
-            buffer_t* buffer = generator_c_expression_identifier(generator, ast);
+        case AST_EXPRESSION_TYPE_IDENTIFIER:
+            buffer = generator_c_expression_identifier(generator, ast);
             buffer_append(temp, buffer);
             buffer_destroy(buffer);
             break;
 
-        case AST_KIND_EXPRESSION_BINARY:
-            // buffer_t* buffer = generator_c_expression_binary(generator, ast);
+        case AST_EXPRESSION_TYPE_BINARY:
+            // buffer = generator_c_expression_binary(generator, ast);
             // buffer_append(temp, buffer);
             // buffer_destroy(buffer);
             break;
 
-        case AST_KIND_EXPRESSION_UNARY:
-            // buffer_t* buffer = generator_c_expression_unary(generator, ast);
+        case AST_EXPRESSION_TYPE_UNARY:
+            // buffer = generator_c_expression_unary(generator, ast);
             // buffer_append(temp, buffer);
             // buffer_destroy(buffer);
             break;
 
-        case AST_KIND_EXPRESSION_INDEX:
-            // buffer_t* buffer = generator_c_expression_index(generator, ast);
+        case AST_EXPRESSION_TYPE_INDEX:
+            // buffer = generator_c_expression_index(generator, ast);
             // buffer_append(temp, buffer);
             // buffer_destroy(buffer);
             break;
 
-        case AST_KIND_EXPRESSION_CALL:
-            // buffer_t* buffer = generator_c_expression_call(generator, ast);
+        case AST_EXPRESSION_TYPE_CALL:
+            // buffer = generator_c_expression_call(generator, ast);
             // buffer_append(temp, buffer);
             // buffer_destroy(buffer);
             break;

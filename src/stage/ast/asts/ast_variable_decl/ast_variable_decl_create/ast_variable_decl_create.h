8b3372615a59c8df998f6a04d665dd1140f319a7
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-23 20:15:48 +0330

    start parser_parse_variable_decl

--- src/stage/ast/asts/ast_variable_decl/ast_variable_decl_create/ast_variable_decl_create.h
+++ src/stage/ast/asts/ast_variable_decl/ast_variable_decl_create/ast_variable_decl_create.h
@@ -16,6 +16,6 @@
 #include <stage/ast/ast_destroy/ast_destroy.h>
 #include <stage/ast/asts/ast_variable_decl/ast_variable_decl_direct_destroy/ast_variable_decl_direct_destroy.h>
 
-ast_t* ast_variable_decl_create(const char* name, ast_t* value);
+ast_t* ast_variable_decl_create(ast_t* type, const char* name, ast_t* value);
 
 #endif // _STAGE_AST_ASTS_AST_VARIABLE_DECL_AST_VAR_DECL_CREATE_AST_VAR_DECL_CREATE_H_

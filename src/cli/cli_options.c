1b1aec9e5d43047aa318feb07930c4aa082fc955
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2026-06-26 04:16:57 +0330

    Adding signature into project

--- src/cli/cli_options.c
+++ src/cli/cli_options.c
@@ -1,3 +1,17 @@
+/*
+ * Salam Programming Language (2024–2026)
+ *
+ *   +-------------------+
+ *   |     S A L A M     |
+ *   +-------------------+
+ *
+ * Designed by Seyyed Ali Mohammadiyeh and the Salam Team
+ * Born from a decade of language design experience (since 2018)
+ *
+ * Repository: https://github.com/SalamLang/Salam
+ *
+ */
+
 #include "core/prelude.h"
 #include "cli/cli_internal.h"
 #include "i18n/i18n.h"
--- src/logger/logger_internal.h
+++ src/logger/logger_internal.h
@@ -1,3 +1,17 @@
+/*
+ * Salam Programming Language (2024–2026)
+ *
+ *   +-------------------+
+ *   |     S A L A M     |
+ *   +-------------------+
+ *
+ * Designed by Seyyed Ali Mohammadiyeh and the Salam Team
+ * Born from a decade of language design experience (since 2018)
+ *
+ * Repository: https://github.com/SalamLang/Salam
+ *
+ */
+
 #ifndef SALAM_LOGGER_INTERNAL_H
 #define SALAM_LOGGER_INTERNAL_H
 

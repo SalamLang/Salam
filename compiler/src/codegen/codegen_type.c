182b2ee90148e705f1b179d74e81304a617702cf
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2026-06-28 20:03:13 +0300

    Max updates noon3 (#1248)
    * showing git commit hash in salam version
    
    * Adding  -static -static-libgcc -static-libstdc++, add version number in release zip file - filename
    
    * Update build-release github action to download linux and macos zip file and include the files inside release
    
    * Fully design slice for array in Salam, update books, add some examples and tests
    
    * Handling errors in i18n gettext
    
    * Fix some issues and update whatever AI mentioned in PR
    
    * remove useless comments

--- compiler/src/codegen/codegen_type.c
+++ compiler/src/codegen/codegen_type.c
@@ -166,7 +166,7 @@ bool cg_is_slice_ts(const char *ts)
 void cg_slice_elem(const char *ts, char *ebuf, size_t cap)
 {
     if (!ts) { if (cap) ebuf[0] = 0; return; }
-    cg_vec_elem(ts, ebuf, cap);   /* extracts the type between '<' and '>' */
+    cg_vec_elem(ts, ebuf, cap);
 }
 
 const char *cg_ctype(cg_t *cg, const char *ts)

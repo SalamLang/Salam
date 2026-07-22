0c67133e133823cdafdb44a6fd51fcc61c8ee1bf
Seyyed Ali Mohammadiyeh <MaxBaseCode@gmail.com>
2026-07-22 14:04:59 +0200

    update wsl test run

--- compiler/_wsl_test_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd /mnt/c/Users/Ali/Projects/SalamLang/Salam/compiler
-make -j"$(nproc)" 2>&1 | tail -40
-echo "=== BUILD DONE ==="
-SALAM="$(pwd)/../salam" bash tools/bash/run-tests.sh 2>&1 | tail -300

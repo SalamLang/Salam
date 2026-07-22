2776d94f6caae406164c87915690362e178738f1
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2026-07-23 01:48:39 +0200

    update and fix issues reported by benchmark error and linter errors

--- benchmark/programs/36_array_rotation_reversal/prog.py
+++ benchmark/programs/36_array_rotation_reversal/prog.py
@@ -21,9 +21,7 @@ def main():
     reverse_range(arr, 0, n - 1)
     checksum = 0
     for i in range(n):
-        checksum = (
-            checksum + (arr[i] % 1000000007) * ((i + 1) % 1000000007)
-        ) % 1000000007
+        checksum = (checksum + (arr[i] % 1000000007) * ((i + 1) % 1000000007)) % 1000000007
     print(arr[0])
     print(arr[n - 1])
     print(checksum)
